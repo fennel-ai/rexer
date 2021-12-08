c2b7b3dd858638324bf7d8a71f7d900d5a25596a
Abhay Bothra <bothra90@users.noreply.github.com>
2021-12-07 22:22:34 -0800

    build: cleanup build file

--- kengine/build.gradle.kts
+++ kengine/build.gradle.kts
@@ -8,6 +8,9 @@ plugins {
 group = "me.abhay"
 version = "1.0-SNAPSHOT"
 
+val ktor_version = "1.6.6"
+val logback_version = "1.2.5"
+
 repositories {
     mavenCentral()
 }
@@ -15,7 +18,16 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test"))
+    testImplementation "io.ktor:ktor-server-test-host:$ktor_version"
+    testImplementation "org.jetbrains.kotlin:kotlin-test"
+
     implementation(kotlin("stdlib"))
+
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation "io.ktor:ktor-serialization:$ktor_version"
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 }
 
 tasks.test {
@@ -28,11 +40,4 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("ai.fennel.engine.StarqlServerKt")
-}
-
-// Support for ktor
-dependencies {
-    implementation("io.ktor:ktor-server-core:1.6.6")
-    implementation("io.ktor:ktor-server-netty:1.6.6")
-    implementation("ch.qos.logback:logback-classic:1.2.5")
 }
