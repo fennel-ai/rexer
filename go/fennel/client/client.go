c78e36bd98bf7e10a0c9757e3de6d723ec02fcc8
Nikhil Garg <nikhil@fennel.ai>
2022-01-15 13:38:29 -0800

    go/client: refactor and simplify code

--- go/fennel/client/client.go
+++ go/fennel/client/client.go
@@ -42,6 +42,27 @@ func (c Client) setProfileURL() string {
 	return fmt.Sprintf("%s:%d/set", c.url, lib.PORT)
 }
 
+func post(protoMessage proto.Message, url string) ([]byte, error) {
+	// serialize the request to be sent on wire
+	ser, err := proto.Marshal(protoMessage)
+	if err != nil {
+		return nil, fmt.Errorf("marshal error on client: %v", err)
+	}
+
+	reqBody := bytes.NewBuffer(ser)
+	response, err := http.Post(url, "application/json", reqBody)
+	if err != nil {
+		return nil, fmt.Errorf("server error: %v", err)
+	}
+	// verify server sent no error
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not read server response: %v", err)
+	}
+	return body, nil
+}
+
 // GetProfile if no matching value is found, a nil pointer is returned with no error
 // If a matching value is found, a valid Value pointer is returned with no error
 // If an error occurs, a nil pointer is returned with a non-nil error
@@ -54,29 +75,19 @@ func (c *Client) GetProfile(request *profileLib.ProfileItem) (*value.Value, erro
 	if err != nil {
 		return nil, fmt.Errorf("invalid request: %v", err)
 	}
-	postBody, err := proto.Marshal(&protoReq)
+
+	response, err := post(&protoReq, c.getProfileURL())
 	if err != nil {
-		return nil, fmt.Errorf("marshal error on client: %v", err)
+		return nil, err
 	}
-	reqBody := bytes.NewBuffer(postBody)
-	// TODO: should these be in body (which means POST) or in headers with GET method?
-	response, err := http.Post(c.getProfileURL(), "application/json", reqBody)
-	if err != nil {
-		return nil, fmt.Errorf("server error: %v", err)
-	}
-	//Read the response body
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, fmt.Errorf("could not read server response: %v", err)
-	}
-	if len(body) == 0 {
+	// so server sent some response without error, so let's decode that response
+	if len(response) == 0 {
 		// i.e. no valid value is found, so we return nil pointer
 		return nil, nil
 	}
-	// now try to read body as a serialized ProtoValue
+	// now try to read response as a serialized ProtoValue
 	var pv value.PValue
-	if err = proto.Unmarshal(body, &pv); err != nil {
+	if err = proto.Unmarshal(response, &pv); err != nil {
 		return nil, fmt.Errorf("could not unmarshal server response: %v", err)
 	}
 	// now convert proto value to real value
@@ -98,45 +109,21 @@ func (c *Client) SetProfile(req *profileLib.ProfileItem) error {
 		return fmt.Errorf("could not convert request to proto: %v", err)
 	}
 	// serialize the request to be sent on wire
-	ser, err := proto.Marshal(&protoReq)
-	if err != nil {
-		return fmt.Errorf("marshal error on client: %v", err)
-	}
-
-	reqBody := bytes.NewBuffer(ser)
-	response, err := http.Post(c.setProfileURL(), "application/json", reqBody)
-	if err != nil {
-		return fmt.Errorf("server error: %v", err)
-	}
-	// verify server sent no error
-	defer response.Body.Close()
-	if _, err = ioutil.ReadAll(response.Body); err != nil {
-		return fmt.Errorf("could not read server response: %v", err)
+	if _, err = post(&protoReq, c.setProfileURL()); err != nil {
+		return err
 	}
 	return nil
 }
 
 func (c *Client) FetchActions(request lib.ActionFetchRequest) ([]lib.Action, error) {
 	protoRequest := lib.ToProtoActionFetchRequest(request)
-	ser, err := proto.Marshal(&protoRequest)
+	response, err := post(&protoRequest, c.fetchURL())
 	if err != nil {
 		return nil, err
 	}
-
-	reqBody := bytes.NewBuffer(ser)
-	response, err := http.Post(c.fetchURL(), "application/json", reqBody)
-	if err != nil {
-		return nil, fmt.Errorf("http error: %v", err)
-	}
-	// verify server sent no error
-	defer response.Body.Close()
-	ser, err = ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, fmt.Errorf("server error: %v", err)
-	}
 	// now read all actions
 	var actionList lib.ProtoActionList
-	err = proto.Unmarshal(ser, &actionList)
+	err = proto.Unmarshal(response, &actionList)
 	if err != nil {
 		return nil, fmt.Errorf("unmarshal error: %v", err)
 	}
@@ -151,19 +138,7 @@ func (c *Client) LogAction(action lib.Action) error {
 		return fmt.Errorf("can not log invalid action: %v", err)
 	}
 	pa := lib.ToProtoAction(action)
-	ser, err := proto.Marshal(&pa)
-	if err != nil {
-		return err
-	}
-	reqBody := bytes.NewBuffer(ser)
-	response, err := http.Post(c.logURL(), "application/json", reqBody)
-	if err != nil {
-		return fmt.Errorf("server error: %v", err)
-	}
-	// verify server sent no error
-	defer response.Body.Close()
-	_, err = ioutil.ReadAll(response.Body)
-	if err != nil {
+	if _, err = post(&pa, c.logURL()); err != nil {
 		return err
 	}
 	return nil
@@ -175,26 +150,13 @@ func (c *Client) GetCount(request lib.GetCountRequest) (uint64, error) {
 		return 0, fmt.Errorf("invalid request: %v", err)
 	}
 
-	// now convert to proto and marshal
-	pgcr := lib.ToProtoGetCountRequest(&request)
-	ser, err := proto.Marshal(&pgcr)
-	if err != nil {
-		return 0, fmt.Errorf("could not marshal request: %v", err)
-	}
-
-	reqBody := bytes.NewBuffer(ser)
-	response, err := http.Post(c.countURL(), "application/json", reqBody)
-	if err != nil {
-		return 0, fmt.Errorf("http error: %v", err)
-	}
-	// verify server sent no error
-	defer response.Body.Close()
-	ser, err = ioutil.ReadAll(response.Body)
+	protoRequest := lib.ToProtoGetCountRequest(&request)
+	response, err := post(&protoRequest, c.countURL())
 	if err != nil {
 		return 0, err
 	}
 	var count uint64
-	err = json.Unmarshal(ser, &count)
+	err = json.Unmarshal(response, &count)
 	if err != nil {
 		return 0, fmt.Errorf("server unmarshall error %v", err)
 	}
@@ -203,33 +165,20 @@ func (c *Client) GetCount(request lib.GetCountRequest) (uint64, error) {
 
 // GetRate returns the normalized ratio of two counters in the same window
 // if lower is true, the lower bound is returned and if false upper bound is returned
-// TODO: ideally we should just move this logic to server instead of client?
 func (c *Client) GetRate(request lib.GetRateRequest) (float64, error) {
 	err := request.Validate()
 	if err != nil {
 		return 0, fmt.Errorf("invalid request: %v", err)
 	}
+	// convert to proto and send to server
+	protoRequest := lib.ToProtoGetRateRequest(&request)
+	response, err := post(&protoRequest, c.rateURL())
+	if err != nil {
 
-	// now convert to proto and marshal
-	pgrr := lib.ToProtoGetRateRequest(&request)
-	ser, err := proto.Marshal(&pgrr)
-	if err != nil {
-		return 0, fmt.Errorf("could not marshal request: %v", err)
-	}
-
-	reqBody := bytes.NewBuffer(ser)
-	response, err := http.Post(c.rateURL(), "application/json", reqBody)
-	if err != nil {
-		return 0, fmt.Errorf("http error: %v", err)
-	}
-	// verify server sent no error
-	defer response.Body.Close()
-	ser, err = ioutil.ReadAll(response.Body)
-	if err != nil {
 		return 0, err
 	}
 	var rate float64
-	err = json.Unmarshal(ser, &rate)
+	err = json.Unmarshal(response, &rate)
 	if err != nil {
 		return 0, fmt.Errorf("server unmarshall error %v", err)
 	}
