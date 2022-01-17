600df4e8ee8ca2e23dbe13282c2b9b16b8204ff7
Abhay Bothra <bothra90@users.noreply.github.com>
2022-01-17 13:51:39 +0530

    cloud: install ambassador edge stack to expose services

--- cloud/pulumi/ts/eks/index.ts
+++ cloud/pulumi/ts/eks/index.ts
@@ -1,11 +1,13 @@
 import * as eks from "@pulumi/eks";
 import * as k8s from "@pulumi/kubernetes"
+import * as pulumi from "@pulumi/pulumi";
 import { local } from "@pulumi/command";
 
 function setupLinkerd(cluster: k8s.Provider) {
     // Setup root and issuer CA as per https://linkerd.io/2.11/tasks/generate-certificates/.
     const rootCA = new local.Command("root-ca", {
-        create: "step certificate create root.linkerd.cluster.local ca.crt ca.key --profile root-ca --no-password --insecure"
+        create: "step certificate create root.linkerd.cluster.local ca.crt ca.key --profile root-ca --no-password --insecure",
+        delete: "rm -f ca.crt ca.key"
     })
 
     const readCaCrt = new local.Command("ca-crt", {
@@ -14,6 +16,7 @@ function setupLinkerd(cluster: k8s.Provider) {
 
     const issuerCA = new local.Command("issuer-ca", {
         create: "step certificate create identity.linkerd.cluster.local issuer.crt issuer.key --profile intermediate-ca --not-after 8760h --no-password --insecure --ca ca.crt --ca-key ca.key",
+        delete: "rm -f issuer.crt issuer.key"
     }, { dependsOn: rootCA })
 
     const readIssuerCrt = new local.Command("issuer-crt", {
@@ -51,11 +54,74 @@ function setupLinkerd(cluster: k8s.Provider) {
     }, { dependsOn: linkerd.ready })
 }
 
+function setupAmbassadorIngress(cluster: k8s.Provider) {
+
+    // Create namespace.
+    const ns = new k8s.core.v1.Namespace("aes-ns", {
+        metadata: {
+            name: "ambassador"
+        }
+    })
+
+    // Create CRDs.
+    const aesCrds = new k8s.yaml.ConfigFile("aes-cerds", {
+        file: "aes-crds.yaml"
+    }, { provider: cluster, dependsOn: ns })
+
+    // Configure default Module to add linkerd headers.
+    const l5dmapping = new k8s.apiextensions.CustomResource("l5d-mapping", {
+        "apiVersion": "getambassador.io/v3alpha1",
+        "kind": "Module",
+        "metadata": {
+            "name": "ambassador"
+        },
+        "spec": {
+            "config": {
+                "add_linkerd_headers": true
+            }
+        }
+    }, { provider: cluster, dependsOn: aesCrds })
+
+    const config = new pulumi.Config();
+
+    // Install ambassador via helm.
+    const ambassador = new k8s.helm.v3.Chart("aes", {
+        fetchOpts: {
+            repo: "https://app.getambassador.io"
+        },
+        chart: "edge-stack",
+        version: "7.2.1",
+        namespace: ns.id,
+        values: {
+            "emissary-ingress": {
+                "createDefaultListeners": true,
+                "agent": {
+                    "cloudConnectToken": config.requireSecret("aes-token")
+                }
+            }
+        },
+        transformations: [
+            (obj: any, opts: pulumi.CustomResourceOptions) => {
+                if (obj.kind === "Deployment" && obj.metadata.name === "aes-edge-stack") {
+                    const metadata = obj.spec.template.metadata
+                    if (metadata.annotations == null) {
+                        metadata.annotations = {}
+                    }
+                    metadata.annotations["linkerd.io/inject"] = "enabled"
+                    metadata.annotations["config.linkerd.io/skip-inbound-ports"] = "80,443"
+                }
+            },
+        ]
+    }, { provider: cluster, dependsOn: [aesCrds, l5dmapping] })
+}
+
 
 // Create an EKS cluster with the default configuration.
 const cluster = new eks.Cluster("eks-cluster", {
     nodeGroupOptions: {
-        desiredCapacity: 1,
+        desiredCapacity: 3,
+        minSize: 3,
+        maxSize: 3,
         amiId: "ami-047a7967ea0436232"
     },
     providerCredentialOpts: {
@@ -68,3 +134,7 @@ export const kubeconfig = cluster.kubeconfig;
 
 // Setup linkerd service mesh.
 setupLinkerd(cluster.provider)
+
+
+// Install Ambassador
+setupAmbassadorIngress(cluster.provider)
