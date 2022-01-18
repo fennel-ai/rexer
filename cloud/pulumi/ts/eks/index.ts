ca178f9911d36b245adf3da315fccdb601aa67c2
Abhay Bothra <bothra90@users.noreply.github.com>
2022-01-19 00:49:16 +0530

    cloud: configure aws-load-balancer-controller for eks cluster

--- cloud/pulumi/ts/eks/index.ts
+++ cloud/pulumi/ts/eks/index.ts
@@ -2,6 +2,9 @@ import * as eks from "@pulumi/eks";
 import * as k8s from "@pulumi/kubernetes"
 import * as pulumi from "@pulumi/pulumi";
 import { local } from "@pulumi/command";
+import * as aws from "@pulumi/aws";
+import * as fs from 'fs';
+import { CustomResource } from "@pulumi/pulumi";
 
 function setupLinkerd(cluster: k8s.Provider) {
     // Setup root and issuer CA as per https://linkerd.io/2.11/tasks/generate-certificates/.
@@ -55,7 +58,6 @@ function setupLinkerd(cluster: k8s.Provider) {
 }
 
 function setupAmbassadorIngress(cluster: k8s.Provider) {
-
     // Create namespace.
     const ns = new k8s.core.v1.Namespace("aes-ns", {
         metadata: {
@@ -81,7 +83,7 @@ function setupAmbassadorIngress(cluster: k8s.Provider) {
                 "add_linkerd_headers": true
             }
         }
-    }, { provider: cluster, dependsOn: aesCrds })
+    }, { provider: cluster, dependsOn: aesCrds.ready })
 
     const config = new pulumi.Config();
 
@@ -116,32 +118,147 @@ function setupAmbassadorIngress(cluster: k8s.Provider) {
                     metadata.annotations["config.linkerd.io/skip-inbound-ports"] = "80,443"
                 }
             },
+            (obj: any, opts: pulumi.CustomResourceOptions) => {
+                if (obj.kind === "Service" && obj.spec.type === "LoadBalancer") {
+                    const metadata = obj.metadata
+                    if (metadata.annotations == null) {
+                        metadata.annotations = {}
+                    }
+                    // Set load-balancer type as external to bypass k8s in-tree
+                    // load-balancer controller and use AWS Load Balancer Controller
+                    // instead.
+                    // https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.3/guide/service/nlb/#configuration
+                    metadata.annotations["service.beta.kubernetes.io/aws-load-balancer-type"] = "external"
+                    // Use NLB in instance mode since we don't currently setup the VPC CNI plugin.
+                    metadata.annotations["service.beta.kubernetes.io/aws-load-balancer-nlb-target-type"] = "instance"
+                    // Deploy an internal load-balancer instead of an internet-facing one.
+                    metadata.annotations["service.beta.kubernetes.io/aws-load-balancer-scheme"] = "internal"
+                }
+            },
         ]
-    }, { provider: cluster, dependsOn: [aesCrds, l5dmapping] })
+    }, { provider: cluster, dependsOn: l5dmapping })
 }
 
+async function setupIamRoleForServiceAccount(namespace: string, serviceAccountName: string, cluster: eks.Cluster) {
+    // Account id
+    const current = await aws.getCallerIdentity({});
+    const accountId = current.accountId
 
-// Create an EKS cluster with the default configuration.
-const cluster = new eks.Cluster("eks-cluster", {
-    nodeGroupOptions: {
-        desiredCapacity: 3,
-        minSize: 3,
-        maxSize: 3,
-        amiId: "ami-047a7967ea0436232"
-    },
-    providerCredentialOpts: {
-        profileName: "admin"
-    },
-    createOidcProvider: true
-});
+    // Create k8s service account and IAM role for LoadBalanacerController, and
+    // associate the above policy with the account.
+    const role = cluster.core.oidcProvider!.url.apply(oidcUrl => {
+        return new aws.iam.Role("lbc-role", {
+            namePrefix: serviceAccountName,
+            description: "IAM role for AWS load-balancer-controller",
+            assumeRolePolicy: `{
+             "Version": "2012-10-17",
+             "Statement": [
+               {
+                 "Effect": "Allow",
+                 "Principal": {
+                   "Federated": "arn:aws:iam::${accountId}:oidc-provider/${oidcUrl}"
+                 },
+                 "Action": "sts:AssumeRoleWithWebIdentity",
+                 "Condition": {
+                   "StringEquals": {
+                     "${oidcUrl}:sub": "system:serviceaccount:${namespace}:${serviceAccountName}"
+                   }
+                 }
+               }
+             ]
+           }`
+        })
+    })
 
-// Export the cluster's kubeconfig.
-export const kubeconfig = cluster.kubeconfig;
+    const acc = role.apply(role => {
+        new k8s.core.v1.ServiceAccount("lbc-ac", {
+            automountServiceAccountToken: true,
+            metadata: {
+                name: serviceAccountName,
+                namespace: namespace,
+                annotations: {
+                    "eks.amazonaws.com/role-arn": role.arn
+                }
+            }
+        }, { provider: cluster.provider })
+    })
 
-export const oidcUrl = cluster.core.oidcProvider?.url
+    return { "role": role, "serviceAccount": acc }
+}
 
-// Setup linkerd service mesh.
-setupLinkerd(cluster.provider)
+async function setupLoadBalancerController(cluster: eks.Cluster) {
 
-// Install Ambassador
-setupAmbassadorIngress(cluster.provider)
+    const serviceAccountName = "aws-load-balancer-controller"
+
+    // Create k8s service account and IAM role for LoadBalanacerController, and
+    // associate the above policy with the account.
+    const { role } = await setupIamRoleForServiceAccount("kube-system", serviceAccountName, cluster)
+
+    // Create policy for lb-controller.
+    try {
+        var policyJson = fs.readFileSync('iam-policy.json', 'utf8')
+    } catch (err) {
+        console.error(err)
+        process.exit()
+    }
+    const iamPolicy = new aws.iam.Policy("lbc-policy", {
+        namePrefix: "AWSLoadBalancerControllerIAMPolicy",
+        policy: policyJson,
+    })
+
+    const attachPolicy = new aws.iam.RolePolicyAttachment("attach-lbc-policy", {
+        role: role.id,
+        policyArn: iamPolicy.arn,
+    })
+
+    const lbc = new k8s.helm.v3.Chart("aws-lbc", {
+        fetchOpts: {
+            repo: "https://aws.github.io/eks-charts"
+        },
+        chart: "aws-load-balancer-controller",
+        namespace: "kube-system",
+        values: {
+            "clusterName": cluster.core.cluster.name,
+            "serviceAccount": {
+                "create": false,
+                "name": serviceAccountName,
+            }
+        }
+    }, { provider: cluster.provider, dependsOn: attachPolicy })
+
+    return lbc
+}
+
+export = async () => {
+    // Create an EKS cluster with the default configuration.
+    const cluster = new eks.Cluster("eks-cluster", {
+        nodeGroupOptions: {
+            desiredCapacity: 3,
+            minSize: 3,
+            maxSize: 3,
+            amiId: "ami-047a7967ea0436232"
+        },
+        providerCredentialOpts: {
+            profileName: "admin"
+        },
+        createOidcProvider: true
+    });
+
+    // Export the cluster's kubeconfig.
+    const kubeconfig = cluster.kubeconfig;
+
+    const oidcUrl = cluster.core.oidcProvider?.url
+
+    // Setup linkerd service mesh.
+    setupLinkerd(cluster.provider)
+
+    // Setup AWS load balancer controller.
+    const lbc = await setupLoadBalancerController(cluster)
+
+    // Install Ambassador after load-balancer controller.
+    lbc.ready.apply((_) => {
+        setupAmbassadorIngress(cluster.provider)
+    })
+
+    return { kubeconfig, oidcUrl }
+}
