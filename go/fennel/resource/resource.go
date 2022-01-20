d32da4854c7ca68f8b373828e338839abe61a22a
Nikhil Garg <nikhil@fennel.ai>
2022-01-20 00:52:39 -0800

    db: remove table abstraction (because now we have full schema management)

--- go/fennel/resource/resource.go
+++ go/fennel/resource/resource.go
@@ -10,20 +10,13 @@ implements Config interface. Using that config, materialize the
 resource. Any initialization/setup should be done during this
 materialization.
 
-In addition to this explicit way, it is recommended to create
-methods that provide a default resource using a default config.
-In such cases, it's best if different default resources are provided
-depending on whatever instance (test/prod/dev) the control is in.
-
 */
 
 const (
 	DBConnection  Type = 1
-	DBTable            = 2
-	RedisClient        = 3
-	RedisCluster       = 4
-	KafkaConsumer      = 5
-	KafkaProducer      = 6
+	RedisClient        = 2
+	KafkaConsumer      = 3
+	KafkaProducer      = 4
 )
 
 type Config interface {
