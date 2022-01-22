5db2be29880ba6411ade5f3962f2b87839fba3cb
Abhay Bothra <bothra90@users.noreply.github.com>
2022-01-23 02:10:39 +0530

    nix: add more dependencies

--- shell.nix
+++ shell.nix
@@ -4,11 +4,47 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.which
+
+    pkgs.direnv
+
+    # Protobuf
+    pkgs.protobuf3_9
+
+    # Packages for go development.
+    pkgs.go_1_17
+    pkgs.protoc-gen-go
+
+    # Packages to build kafka go client
+    pkgs.rdkafka
+    pkgs.openssl
+    pkgs.pkg-config
+
+    # Packages for python development
+    pkgs.python310
+    pkgs.poetry
+
+    # Packages for javascript development
+    pkgs.nodejs
+
+    # Packages for deployment
+    pkgs.pulumi-bin
+    pkgs.docker
+    pkgs.kubernetes
+    pkgs.kubernetes-helm
+    pkgs.linkerd
+    pkgs.step-cli
+
+    # Tools for connecting to dbs
+    pkgs.confluent-platform
+    pkgs.redis
+    pkgs.pscale
+    pkgs.mysql-client
+
+    # Some nice-to-have tools
+    pkgs.jq
+    pkgs.fzf
+    pkgs.fzf-zsh
     pkgs.htop
     pkgs.zlib
-    pkgs.protobuf3_9
-    pkgs.protoc-gen-go
-    pkgs.darwin.apple_sdk.frameworks.Security
   ];
 }
