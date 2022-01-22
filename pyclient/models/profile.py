d608808f9f400776d15c04280cb96a5e0b4b9911
Araya <araya@fennel.ai>
2022-01-22 08:16:11 -0800

    pyclient: add protobuf conversion functions for profile list

--- pyclient/models/profile.py
+++ pyclient/models/profile.py
@@ -1,4 +1,7 @@
+import client
 from gen.profile_pb2 import ProtoProfileItem as ProfileItem
+from gen.profile_pb2 import ProtoProfileList as ProfileList
+from gen.profile_pb2 import ProtoProfileFetchRequest as ProfileFetchRequest
 
 
 def validate(pi: ProfileItem):
@@ -11,3 +14,22 @@ def validate(pi: ProfileItem):
         errors.append("key can not be empty")
 
     return errors
+
+def from_proto_profile_list(pl):
+    """Takes a valid profile list and returns a list of profiles"""
+    ret = []
+    for p in pl.profiles:
+        ret.append(p)
+    return ret
+
+def to_proto_profile_list(profiles):
+    """Takes a list of profiles and returns a profilelist"""
+    if not isinstance(profiles, list):
+        raise client.InvalidInput('profiles not a list but instead: %s' % str(profiles))
+    pl = ProfileList()
+    for p in profiles:
+        if not isinstance(p, ProfileItem):
+            raise client.InvalidInput('members of profiles not profile but instead: %s' % str(p))
+        
+        pl.profiles.append(p)
+    return pl
