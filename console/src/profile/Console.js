64fef8c2d58d0b3362306703588b03b905a36a6e
ljq2022 <lukeqin1@gmail.com>
2022-01-29 18:49:43 -0500

    removed changes to profile main

--- console/src/profile/Console.js
+++ console/src/profile/Console.js
@@ -1,52 +1,45 @@
-import * as React from "react";
-import { API } from "aws-amplify";
-import axios from "axios";
-import { ConsoleForm } from "./ConsoleForm";
-import { ConsoleResult } from "./ConsoleResult";
-import "./../style.css";
+import * as React from 'react';
+import { API } from 'aws-amplify';
+import { ConsoleForm } from './ConsoleForm';
+import { ConsoleResult } from './ConsoleResult';
+import './../style.css';
 
-const API_ENDPOINT = "/actions/profiles";
+const API_ENDPOINT = '/actions/profiles';
 
 const getQuery = (form) => {
   const params = {};
 
-  if (form.filterOType.value !== "ANY") {
+  if (form.filterOType.value !== 'ANY') {
     params.otype = form.filterOType.value;
   }
-  if (form.filterOId.value !== "") {
+  if (form.filterOId.value !== '') {
     params.oid = form.filterOId.value;
   }
-  if (form.filterKey.value !== "") {
+  if (form.filterKey.value !== '') {
     params.key = form.filterKey.value;
   }
-  if (form.filterVersion.value !== "") {
+  if (form.filterVersion.value !== '') {
     params.version = form.filterVersion.value;
   }
 
-  return { queryStringParameters: params };
+  return { 'queryStringParameters' : params }
 };
 
 const Console = () => {
-  const [results, setResults] = React.useState([]);
+  const [ results, setResults ] = React.useState([]);
 
   const handleQuery = (event) => {
     const form = event.target;
-
+    
     const query = getQuery(form);
 
-    axios
-      .get(`http://localhost:3001${API_ENDPOINT}`, {
-        params: {
-          query,
-        },
-      })
-      .then((response) => setResults(response.data.data))
-      .catch((error) => {
-        console.log("Failed to load metadata.", error);
-      });
+    API
+      .get('bff', API_ENDPOINT, query)
+      .then(response => setResults(response.data))
+      .catch(error => console.log(error));
 
     event.preventDefault();
-  };
+  }
 
   return (
     <div className="consoleBody">
