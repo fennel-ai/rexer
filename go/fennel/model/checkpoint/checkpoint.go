9fbf5993f0dbdb67e1982706b60fc45fe1d3b10d
Nikhil Garg <nikhil@fennel.ai>
2022-01-29 16:38:08 -0800

    model/checkpoint: remove older unused checkpoint model

--- go/fennel/model/checkpoint/checkpoint.go
+++ go/fennel/model/checkpoint/checkpoint.go
@@ -3,41 +3,9 @@ package checkpoint
 import (
 	"database/sql"
 	"fennel/instance"
-	"fennel/lib/counter"
 	"fennel/lib/ftypes"
 )
 
-func GetCheckpoint(this instance.Instance, ct counter.CounterType) (ftypes.OidType, error) {
-	// TODO: Use appropriate custid instead
-	row := this.DB.QueryRow(`
-		SELECT checkpoint
-		FROM checkpoint
-		WHERE cust_id = ?
-		AND counter_type = ?;
-	`, this.CustID, ct)
-	var checkpoint uint64
-	err := row.Scan(&checkpoint)
-	if err != nil && err != sql.ErrNoRows {
-		return 0, err
-	} else if err == sql.ErrNoRows {
-		// this happens when no matching row was found. By default, checkpoint is zero
-		return 0, nil
-	} else {
-		return ftypes.OidType(checkpoint), nil
-	}
-}
-
-func SetCheckpoint(this instance.Instance, ct counter.CounterType, checkpoint ftypes.OidType) error {
-	// TODO: Use appropriate custid instead
-	_, err := this.DB.Exec(`
-		INSERT INTO checkpoint (cust_id, counter_type, checkpoint)
-        VALUES (?, ?, ?)
-		ON DUPLICATE KEY
-		UPDATE
-			checkpoint = ?
-		;`, this.CustID, ct, checkpoint, checkpoint)
-	return err
-}
 func GetCheckpoint2(this instance.Instance, aggtype ftypes.AggType, aggname ftypes.AggName) (ftypes.OidType, error) {
 	row := this.DB.QueryRow(`
 		SELECT checkpoint
