d40052a935b080d0a27a7f29c7e6570bc3dafbaa
Nikhil Garg <nikhil@fennel.ai>
2022-01-31 15:03:04 -0800

    pyconsole: move pyclient out, install it using github

--- pyclient/rql/__init__.py
+++
@@ -1,4 +0,0 @@
-from rql.expr import Expr
-from rql.expr import Int, Double, Bool, String
-from rql.expr import InvalidQueryException, List, Dict, Table, Var, Ops, at, Cond
-from rql.serializer import Serializer
