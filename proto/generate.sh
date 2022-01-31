12361570d6490daf3bd764ad240d35d2b421a5d3
Araya <1nikhil9@gmail.com>
2022-01-31 15:57:09 -0800

    proto: add AstWithDict protobuf and fix generate query

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate python bindings
-protoc --proto_path=./ --python_out=../../rexer-pyclient-alpha/gen *.proto
+protoc --proto_path=./ --python_out=../../rexer-pyclient-alpha/rexerclient/gen *.proto
 
 # Generate go bindings
 protoc -I=. --go_out=../go/ ./*.proto
