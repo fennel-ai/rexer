8ae886c3c24164cfaa727f24b0e660e033b623ee
ljq2022 <lukeqin1@gmail.com>
2022-02-01 01:30:14 -0500

    added hardcoded aws config to stop user pool from self deleting

--- console/src/index.js
+++ console/src/index.js
@@ -10,8 +10,33 @@ import ProfileApp from "./profile/ProfileApp";
 import { SignIn } from "./SignIn";
 import { SignUp } from "./SignUp";
 import { ConfirmSignUp } from "./ConfirmSignUp";
-
-Amplify.configure(awsconfig);
+const config = {
+  aws_project_region: "us-west-2",
+  aws_cognito_identity_pool_id: "us-west-2_fJKVSx1Dj",
+  aws_cognito_region: "us-west-2",
+  aws_user_pools_id: "us-west-2_fJKVSx1Dj",
+  aws_user_pools_web_client_id: "4g5cf9uncg1pfupqq37233ccth",
+  oauth: {},
+  aws_cognito_username_attributes: ["EMAIL"],
+  aws_cognito_social_providers: [],
+  aws_cognito_signup_attributes: ["EMAIL"],
+  aws_cognito_mfa_configuration: "OFF",
+  aws_cognito_mfa_types: ["SMS"],
+  aws_cognito_password_protection_settings: {
+    passwordPolicyMinLength: 8,
+    passwordPolicyCharacters: [],
+  },
+  aws_cognito_verification_mechanisms: ["EMAIL"],
+  aws_cloud_logic_custom: [
+    {
+      name: "consoleBff",
+      endpoint:
+        "https://nizekl4xd9.execute-api.us-west-2.amazonaws.com/westdev",
+      region: "us-west-2",
+    },
+  ],
+};
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
