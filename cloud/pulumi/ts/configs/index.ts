9f6f17586336a542c3116dc058f7a15092c61ec2
Abhay Bothra <bothra90@users.noreply.github.com>
2022-02-02 15:30:20 -0800

    pulumi: create project to install tier config into k8s secrets

---
+++ cloud/pulumi/ts/configs/index.ts
@@ -0,0 +1,28 @@
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+
+const config = new pulumi.Config();
+
+const rc = new k8s.core.v1.Secret("redis-config", {
+    stringData: config.requireSecretObject("redis-conf"),
+    metadata: {
+        namespace: "fennel",
+        name: "redis-conf",
+    },
+}, { deleteBeforeReplace: true })
+
+const kafkaCreds = new k8s.core.v1.Secret("kafka-config", {
+    stringData: config.requireSecretObject("kafka-conf"),
+    metadata: {
+        name: "kafka-conf",
+        namespace: "fennel",
+    }
+}, { deleteBeforeReplace: true })
+
+const pscreds = new k8s.core.v1.Secret("planetscale-config", {
+    stringData: config.requireSecretObject("planetscale-conf"),
+    metadata: {
+        namespace: "fennel",
+        name: "mysql-conf",
+    }
+}, { deleteBeforeReplace: true })
