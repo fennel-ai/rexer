f17259c7cdba6b2909ad76d75838a0f52529805b
Araya <1nikhil9@gmail.com>
2022-02-04 12:36:45 -0800

    testall: uncomment go tests

--- testall.py
+++ testall.py
@@ -7,13 +7,13 @@ import platform
 
 root = os.getcwd()
 godir = os.path.join(root, 'go/fennel')
-'''print('Running go tests...')
+print('Running go tests...')
 print('-' * 50)
 if platform.processor() in [ 'arm', 'arm64']:
     p1 = subprocess.Popen(['go test -tags dynamic ./...'], shell=True, cwd=godir)
 else:
     p1 = subprocess.Popen(['go test ./...'], shell=True, cwd=godir)
-p1.wait()'''
+p1.wait()
 
 pydir = os.path.join(root, 'pyconsole')
 print('Running python tests in pyconsole...')
@@ -30,6 +30,6 @@ p3.wait()
 print('=' * 50)
 print('Summary:')
 print('=' * 50)
-#print('Go tests %s' % ('PASS' if p1.returncode == 0 else 'FAIL'))
+print('Go tests %s' % ('PASS' if p1.returncode == 0 else 'FAIL'))
 print('Pyconsole tests %s' % ('PASS' if p2.returncode == 0 else 'FAIL'))
 print('Pyclient tests %s' % ('PASS' if p3.returncode == 0 else 'FAIL'))
