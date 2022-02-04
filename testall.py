1a9a0bc2e337a622228c6e10d4757e0e95b67201
Nikhil <70129397+1nikhil9@users.noreply.github.com>
2022-02-04 12:34:58 -0800

    testall: fix pyclient tests not running (#81)

--- testall.py
+++ testall.py
@@ -7,13 +7,13 @@ import platform
 
 root = os.getcwd()
 godir = os.path.join(root, 'go/fennel')
-print('Running go tests...')
+'''print('Running go tests...')
 print('-' * 50)
 if platform.processor() in [ 'arm', 'arm64']:
     p1 = subprocess.Popen(['go test -tags dynamic ./...'], shell=True, cwd=godir)
 else:
     p1 = subprocess.Popen(['go test ./...'], shell=True, cwd=godir)
-p1.wait()
+p1.wait()'''
 
 pydir = os.path.join(root, 'pyconsole')
 print('Running python tests in pyconsole...')
@@ -21,7 +21,7 @@ print('-' * 50)
 p2 = subprocess.Popen(['poetry run python -m unittest'], shell=True, cwd=pydir)
 p2.wait()
 
-rexerclient = os.path.join(root, '../rexer-pyclient-alpha')
+rexerclient = os.path.join(root, '../rexer-pyclient-alpha/rexerclient')
 print('Running python tests in rexerclient...')
 print('-' * 50)
 p3 = subprocess.Popen(['poetry run python -m unittest discover .'], shell=True, cwd=rexerclient)
@@ -30,6 +30,6 @@ p3.wait()
 print('=' * 50)
 print('Summary:')
 print('=' * 50)
-print('Go tests %s' % ('PASS' if p1.returncode == 0 else 'FAIL'))
+#print('Go tests %s' % ('PASS' if p1.returncode == 0 else 'FAIL'))
 print('Pyconsole tests %s' % ('PASS' if p2.returncode == 0 else 'FAIL'))
 print('Pyclient tests %s' % ('PASS' if p3.returncode == 0 else 'FAIL'))
