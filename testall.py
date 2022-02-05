96ad3754c0ffc30cb647597909f4a51f0612b620
Nikhil Garg <nikhilgarg28@users.noreply.github.com>
2022-02-04 23:17:25 -0800

    testall: parallelize tests by degree of 2
    This doubles execution time and doesn't cross the connection limit either

--- testall.py
+++ testall.py
@@ -10,9 +10,9 @@ godir = os.path.join(root, 'go/fennel')
 print('Running go tests...')
 print('-' * 50)
 if platform.processor() in [ 'arm', 'arm64']:
-    p1 = subprocess.Popen(['go test -p 1 -tags dynamic ./...'], shell=True, cwd=godir)
+    p1 = subprocess.Popen(['go test -p 2 -tags dynamic ./...'], shell=True, cwd=godir)
 else:
-    p1 = subprocess.Popen(['go test -p 1 ./...'], shell=True, cwd=godir)
+    p1 = subprocess.Popen(['go test -p 2 ./...'], shell=True, cwd=godir)
 p1.wait()
 
 pydir = os.path.join(root, 'pyconsole')
