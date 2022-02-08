282a19858831c93641096bfda23ff261b000790f
ljq2022 <lukeqin1@gmail.com>
2022-02-08 18:17:53 -0500

    added user fetch and sending in axios requests

--- console/src/profile/Console.js
+++ console/src/profile/Console.js
@@ -1,45 +1,52 @@
-import * as React from 'react';
-import { API } from 'aws-amplify';
-import { ConsoleForm } from './ConsoleForm';
-import { ConsoleResult } from './ConsoleResult';
-import './../style.css';
+import * as React from "react";
+import { API } from "aws-amplify";
+import { loadLoggedInPage } from "../AuthFunctions";
+import { ConsoleForm } from "./ConsoleForm";
+import { ConsoleResult } from "./ConsoleResult";
+import { useNavigate } from "react-router-dom";
+import "./../style.css";
 
-const API_ENDPOINT = '/actions/profiles';
+const API_ENDPOINT = "/actions/profiles";
 
 const getQuery = (form) => {
   const params = {};
 
-  if (form.filterOType.value !== 'ANY') {
+  if (form.filterOType.value !== "ANY") {
     params.otype = form.filterOType.value;
   }
-  if (form.filterOId.value !== '') {
+  if (form.filterOId.value !== "") {
     params.oid = form.filterOId.value;
   }
-  if (form.filterKey.value !== '') {
+  if (form.filterKey.value !== "") {
     params.key = form.filterKey.value;
   }
-  if (form.filterVersion.value !== '') {
+  if (form.filterVersion.value !== "") {
     params.version = form.filterVersion.value;
   }
 
-  return { 'queryStringParameters' : params }
+  return { queryStringParameters: params };
 };
 
 const Console = () => {
-  const [ results, setResults ] = React.useState([]);
+  const [results, setResults] = React.useState([]);
+  const [username, setUsername] = React.useState("");
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    loadLoggedInPage(setUsername, navigate);
+  }, []);
 
   const handleQuery = (event) => {
     const form = event.target;
 
     const query = getQuery(form);
-
-    API
-      .get('consoleBff', API_ENDPOINT, query)
-      .then(response => setResults(response.data))
-      .catch(error => console.log(error));
+    query["username"] = username;
+    API.get("consoleBff", API_ENDPOINT, query)
+      .then((response) => setResults(response.data))
+      .catch((error) => console.log(error));
 
     event.preventDefault();
-  }
+  };
 
   return (
     <div className="consoleBody">
