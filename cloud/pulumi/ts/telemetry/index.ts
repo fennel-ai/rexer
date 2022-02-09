34c9d7d1258a44fbbd3d0ee51041332046a1692b
Abhay Bothra <bothra90@users.noreply.github.com>
2022-02-09 15:08:00 -0800

    telemetry: pulumi project to setup Aws open-telemetry collector

---
+++ cloud/pulumi/ts/telemetry/index.ts
@@ -0,0 +1,104 @@
+import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
+
+export = async () => {
+    setupOtelPolicy();
+    await setupAdotCollector();
+    return { MonitoredDeployment, MonitoredReplicaSet }
+}
+
+class MonitoredDeployment extends k8s.apps.v1.Deployment {
+    constructor(name: string,
+        args: k8s.apps.v1.Deployment,
+        opts?: pulumi.CustomResourceOptions) {
+        const metadata = args.spec.template.metadata
+        metadata.annotations = metadata.annotations || {};
+        metadata.annotations.apply((annotations) => {
+            annotations["prometheus.io/scrape"] = "true"
+        })
+        super(name, args, opts);
+    }
+}
+
+class MonitoredReplicaSet extends k8s.apps.v1.ReplicaSet {
+    constructor(name: string,
+        args: k8s.apps.v1.ReplicaSet,
+        opts?: pulumi.CustomResourceOptions) {
+        const metadata = args.spec.template.metadata
+        metadata.annotations = metadata.annotations || {};
+        metadata.annotations.apply((annotations) => {
+            annotations["prometheus.io/scrape"] = "true"
+        })
+        super(name, args, opts);
+    }
+}
+
+function setupOtelPolicy() {
+    const rawPolicyStr = `{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "logs:PutLogEvents",
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:DescribeLogStreams",
+                    "logs:DescribeLogGroups",
+                    "cloudwatch:PutMetricData",
+                    "xray:PutTraceSegments",
+                    "xray:PutTelemetryRecords",
+                    "xray:GetSamplingRules",
+                    "xray:GetSamplingTargets",
+                    "xray:GetSamplingStatisticSummaries",
+                    "ssm:GetParameters"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }
+    `
+
+    const policy = new aws.iam.Policy("otel-collector-policy", {
+        namePrefix: "AWSDistroOpenTelemetryPolicy-",
+        policy: rawPolicyStr,
+    });
+
+
+    const config = new pulumi.Config();
+    const attachOtelPolicy = new aws.iam.RolePolicyAttachment("otel-instance", {
+        policyArn: policy.arn,
+        role: config.require("instanceRole"),
+    });
+
+}
+
+// Setup the ADOT (AWS Distro for OpenTelemetry) Collector to collect metrics
+// and traces and forward them to cloudwatch.
+async function setupAdotCollector() {
+    const region = (await aws.getRegion()).name;
+    const config = new pulumi.Config();
+    const eksClusterName = config.require("eksClusterName");
+    const collector = new k8s.yaml.ConfigFile("adot-collector", {
+        file: "otel-deployment.yaml",
+        transformations: [
+            (obj: any, opts: pulumi.CustomResourceOptions) => {
+                if (obj.kind === "DaemonSet") {
+                    let containers = obj.spec.template.spec.containers;
+                    obj.spec.template.spec.containers = containers.map((container: any) => {
+                        container.env.push({
+                            name: "AWS_REGION",
+                            value: region,
+                        } as k8s.types.output.core.v1.EnvVar)
+                        container.env.push({
+                            name: "OTEL_RESOURCE_ATTRIBUTES",
+                            value: `ClusterName=${eksClusterName}`,
+                        } as k8s.types.output.core.v1.EnvVar)
+                        return container
+                    })
+                }
+            },
+        ],
+    })
+}
