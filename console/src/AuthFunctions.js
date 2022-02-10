5d176cbaba56df81f424acf79387de9072d7b14c
ljq2022 <42047492+ljq2022@users.noreply.github.com>
2022-02-09 22:41:52 -0500

    console: sign-in and sign-up pages redirect to /actions if the user logged in recently (#117)

--- console/src/AuthFunctions.js
+++ console/src/AuthFunctions.js
@@ -5,3 +5,8 @@ export const loadLoggedInPage = async (setUsername, navigate) => {
   if (user) setUsername(user.username);
   else navigate("/");
 };
+
+export const loadAuthPage = async (navigate) => {
+  const user = await Auth.currentAuthenticatedUser();
+  if (user) navigate("/actions");
+};
