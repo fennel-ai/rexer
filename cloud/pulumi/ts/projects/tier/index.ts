5f2a9542f0b0185e3f4f9f2d8cdc84dae8041fb4
ljq2022 <42047492+ljq2022@users.noreply.github.com>
2022-02-14 23:31:04 -0500

    pulumi: library directory created for project functions (#154)
    * pulumi: library directory created for project functions
    
    * imported foo into tier project for example

--- cloud/pulumi/ts/projects/tier/index.ts
+++ cloud/pulumi/ts/projects/tier/index.ts
@@ -1,9 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import { eks } from "../../library/tier/eksLib";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+export const foo = eks.foo();
