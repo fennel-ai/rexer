7d1847f3363c639e920dc6f96116a3e3abc33668
Abhay Bothra <bothra90@users.noreply.github.com>
2022-02-17 14:46:05 -0800

    pulumi: create template directory for new pulumi projects

---
+++ cloud/pulumi/ts/template/index.ts
@@ -0,0 +1,44 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as process from "process";
+
+// TODO: use version from common library.
+// operator for type-safety for string key access:
+// https://schneidenbach.gitbooks.io/typescript-cookbook/content/nameof-operator.html
+export const nameof = <T>(name: keyof T) => name;
+
+// TODO: move to common library module.
+export const fennelStdTags = {
+    "managed-by": "fennel.ai",
+}
+
+export const plugins = {}
+
+export type inputType = {}
+
+export type outputType = {}
+
+const parseConfig = (): inputType => {
+    const config = new pulumi.Config();
+    return {}
+}
+
+export const setup = async (input: inputType) => {
+    const output: outputType = {}
+    return output
+}
+
+async function run() {
+    let output: outputType | undefined;
+    // Run the main function only if this program is run through the pulumi CLI.
+    // Unfortunately, in that case the argv0 itself is not "pulumi", but the full
+    // path of node: e.g. /nix/store/7q04aq0sq6im9a0k09gzfa1xfncc0xgm-nodejs-14.18.1/bin/node
+    if (process.argv0 !== 'node') {
+        pulumi.log.info("Running...")
+        const input: inputType = parseConfig();
+        output = await setup(input)
+    }
+    return output
+}
+
+
+export const output = await run();
