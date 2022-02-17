c8396fbb52ecba8373f8e3a09bad92af9c997aa4
Abhay Bothra <bothra90@users.noreply.github.com>
2022-02-17 13:23:12 -0800

    pulumi: update confluent cluster configs
    The previous cluster got deleted when we ran "pulumi destroy" in the
    fennel/confluentenv/dev stack.

--- cloud/pulumi/ts/arkay/index.ts
+++ cloud/pulumi/ts/arkay/index.ts
@@ -5,8 +5,8 @@ setupTier(
         tierId: 1,
         kafkaCluster: {
             bootstrapServers: "SASL_SSL://pkc-pgq85.us-west-2.aws.confluent.cloud:9092",
-            environmentId: "env-rpjx9",
-            id: "lkc-v7p7yz",
+            environmentId: "env-k36j2",
+            id: "lkc-12r9x3",
         },
         topicNames: ["my-favorite-topic"],
     },
