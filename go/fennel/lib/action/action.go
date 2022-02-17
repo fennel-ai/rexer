0acca4b24f27a591a7ce6134cf54c45988e8705c
Araya <1nikhil9@gmail.com>
2022-02-16 21:45:22 -0800

    go: move unused action, profile proto code to their own files

--- go/fennel/lib/action/action.go
+++ go/fennel/lib/action/action.go
@@ -25,42 +25,6 @@ type Action struct {
 	Metadata   value.Value       `db:"metadata"`
 }
 
-func FromProtoAction(pa *ProtoAction) (Action, error) {
-	v, err := value.FromProtoValue(pa.GetMetadata())
-	if err != nil {
-		return Action{}, err
-	}
-	return Action{
-		ActionID:   ftypes.OidType(pa.GetActionID()),
-		ActorID:    ftypes.OidType(pa.GetActorID()),
-		ActorType:  ftypes.OType(pa.GetActorType()),
-		TargetID:   ftypes.OidType(pa.GetTargetID()),
-		TargetType: ftypes.OType(pa.GetTargetType()),
-		ActionType: ftypes.ActionType(pa.GetActionType()),
-		Timestamp:  ftypes.Timestamp(pa.GetTimestamp()),
-		RequestID:  ftypes.RequestID(pa.RequestID),
-		Metadata:   v,
-	}, nil
-}
-
-func ToProtoAction(a Action) (ProtoAction, error) {
-	pv, err := value.ToProtoValue(a.Metadata)
-	if err != nil {
-		return ProtoAction{}, err
-	}
-	return ProtoAction{
-		ActionID:   uint64(a.ActionID),
-		ActorID:    uint64(a.ActorID),
-		ActorType:  string(a.ActorType),
-		TargetID:   uint64(a.TargetID),
-		TargetType: string(a.TargetType),
-		ActionType: string(a.ActionType),
-		Timestamp:  uint64(a.Timestamp),
-		RequestID:  uint64(a.RequestID),
-		Metadata:   &pv,
-	}, nil
-}
-
 type ActionSer struct {
 	ActionID   ftypes.OidType    `db:"action_id"`
 	ActorID    ftypes.OidType    `db:"actor_id"`
@@ -147,63 +111,6 @@ type ActionFetchRequest struct {
 	MinTimestamp ftypes.Timestamp  `db:"min_timestamp" json:"MinTimestamp"`
 	MaxTimestamp ftypes.Timestamp  `db:"max_timestamp" json:"MaxTimestamp"`
 	RequestID    ftypes.RequestID  `db:"request_id" json:"RequestID"`
-}
-
-func FromProtoActionFetchRequest(pa *ProtoActionFetchRequest) ActionFetchRequest {
-	return ActionFetchRequest{
-
-		MinActionID:  ftypes.OidType(pa.GetMinActionID()),
-		MaxActionID:  ftypes.OidType(pa.GetMaxActionID()),
-		ActorID:      ftypes.OidType(pa.GetActorID()),
-		ActorType:    ftypes.OType(pa.GetActorType()),
-		TargetID:     ftypes.OidType(pa.GetTargetID()),
-		TargetType:   ftypes.OType(pa.GetTargetType()),
-		ActionType:   ftypes.ActionType(pa.GetActionType()),
-		MinTimestamp: ftypes.Timestamp(pa.GetMinTimestamp()),
-		MaxTimestamp: ftypes.Timestamp(pa.GetMaxTimestamp()),
-		RequestID:    ftypes.RequestID(pa.GetRequestID()),
-	}
-}
-
-func ToProtoActionFetchRequest(a ActionFetchRequest) ProtoActionFetchRequest {
-	return ProtoActionFetchRequest{
-
-		MinActionID:  uint64(a.MinActionID),
-		MaxActionID:  uint64(a.MaxActionID),
-		ActorID:      uint64(a.ActorID),
-		ActorType:    string(a.ActorType),
-		TargetID:     uint64(a.TargetID),
-		TargetType:   string(a.TargetType),
-		ActionType:   string(a.ActionType),
-		MinTimestamp: uint64(a.MinTimestamp),
-		MaxTimestamp: uint64(a.MaxTimestamp),
-		RequestID:    uint64(a.RequestID),
-	}
-}
-
-func FromProtoActionList(actionList *ProtoActionList) ([]Action, error) {
-	actions := make([]Action, len(actionList.Actions))
-	var err error
-	for i, pa := range actionList.Actions {
-		actions[i], err = FromProtoAction(pa)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return actions, nil
-}
-
-func ToProtoActionList(actions []Action) (*ProtoActionList, error) {
-	ret := &ProtoActionList{}
-	ret.Actions = make([]*ProtoAction, len(actions))
-	for i, action := range actions {
-		pa, err := ToProtoAction(action)
-		if err != nil {
-			return nil, err
-		}
-		ret.Actions[i] = &pa
-	}
-	return ret, nil
 }
 
 // Validate validates that all fields (except action ID) are appropriately specified
---
+++ go/fennel/lib/profile/proto.go
@@ -0,0 +1,76 @@
+package profile
+
+import (
+	"fennel/lib/ftypes"
+	"fennel/lib/value"
+)
+
+func FromProtoProfileItem(ppr *ProtoProfileItem) (ProfileItem, error) {
+	v, err := value.FromProtoValue(ppr.Value)
+	if err != nil {
+		return ProfileItem{}, err
+	}
+	return ProfileItem{
+		ftypes.OType(ppr.OType),
+		ppr.Oid,
+		ppr.Key,
+		ppr.Version,
+		v,
+	}, nil
+}
+func ToProtoProfileItem(pi *ProfileItem) (ProtoProfileItem, error) {
+	pv, err := value.ToProtoValue(pi.Value)
+	if err != nil {
+		return ProtoProfileItem{}, err
+	}
+	return ProtoProfileItem{
+		OType:   string(pi.OType),
+		Oid:     pi.Oid,
+		Key:     pi.Key,
+		Version: pi.Version,
+		Value:   &pv,
+	}, nil
+}
+
+func FromProtoProfileFetchRequest(ppfr *ProtoProfileFetchRequest) ProfileFetchRequest {
+	return ProfileFetchRequest{
+		ftypes.OType(ppfr.OType),
+		ppfr.Oid,
+		ppfr.Key,
+		ppfr.Version,
+	}
+}
+
+func ToProtoProfileFetchRequest(pfr *ProfileFetchRequest) ProtoProfileFetchRequest {
+	return ProtoProfileFetchRequest{
+		OType:   string(pfr.OType),
+		Oid:     pfr.Oid,
+		Key:     pfr.Key,
+		Version: pfr.Version,
+	}
+}
+
+func FromProtoProfileList(profileList *ProtoProfileList) ([]ProfileItem, error) {
+	profiles := make([]ProfileItem, len(profileList.Profiles))
+	for i, ppr := range profileList.Profiles {
+		var err error
+		profiles[i], err = FromProtoProfileItem(ppr)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return profiles, nil
+}
+
+func ToProtoProfileList(profiles []ProfileItem) (*ProtoProfileList, error) {
+	ret := &ProtoProfileList{}
+	ret.Profiles = make([]*ProtoProfileItem, len(profiles))
+	for i, profile := range profiles {
+		ppr, err := ToProtoProfileItem(&profile)
+		if err != nil {
+			return nil, err
+		}
+		ret.Profiles[i] = &ppr
+	}
+	return ret, nil
+}
