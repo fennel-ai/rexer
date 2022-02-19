86e678e03eac533f4575daba4dbc6da4038ce98c
Abhay Bothra <bothra90@users.noreply.github.com>
2022-02-18 16:13:32 -0800

    vpc: setup route-tables and nacls
    With this change, we also move NAT gateway setup from the tailscale
    project to the VPC project since we need to associate the NAT gateway
    with private route tables.

--- cloud/pulumi/ts/tailscale/index.ts
+++ cloud/pulumi/ts/tailscale/index.ts
@@ -2,20 +2,10 @@ import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi"
 import * as fs from "fs";
 
-const eip = new aws.ec2.Eip("tailscale-eip");
-
 const config = new pulumi.Config();
 
 // TODO: Do not hardcode subnet, and use a private subnet.
 const subnet = config.require("subnet")  // "subnet-0ac6f13fe8fcd49ef";
-
-// Create NAT gateway which needed for internet connectivity from private subnets.
-const ng = new aws.ec2.NatGateway("ng", {
-    allocationId: eip.allocationId,
-    subnetId: subnet,
-})
-
-export const gatewayid = ng.id
 
 try {
     var startupScript = fs.readFileSync('startup-script.sh', 'utf8')
--- cloud/pulumi/ts/vpc/index.ts
+++ cloud/pulumi/ts/vpc/index.ts
@@ -27,6 +27,10 @@ export type outputType = {
     vpcId: pulumi.Output<string>
     publicSubnets: pulumi.Output<string>[]
     privateSubnets: pulumi.Output<string>[]
+    privateRouteTable: pulumi.Output<string>
+    publicRouteTable: pulumi.Output<string>
+    publicNacl: pulumi.Output<string>
+    privateNacl: pulumi.Output<string>
 }
 
 const parseConfig = (): inputType => {
@@ -38,21 +42,73 @@ const parseConfig = (): inputType => {
     }
 }
 
-function createPrivateRouteTable(vpc: aws.ec2.Vpc): pulumi.Output<string> {
-    const routeTable = new aws.ec2.RouteTable("private-rt", {
-        vpcId: vpc.id,
-    })
-    return routeTable.id
+// TODO: Tighten rules for more security.
+function createPublicNacl(vpcId: pulumi.Output<string>, subnets: pulumi.Output<string>[], provider: aws.Provider): pulumi.Output<string> {
+    const privateNacl = new aws.ec2.NetworkAcl("public-nacl", {
+        vpcId: vpcId,
+        subnetIds: subnets,
+        egress: [
+            // Allow all egress TCP traffic.
+            {
+                ruleNo: 100,
+                action: "ALLOW",
+                cidrBlock: "0.0.0.0/0",
+                fromPort: 0,
+                toPort: 65535,
+                protocol: "tcp",
+            },
+        ],
+        ingress: [
+            // Allow all ingress TCP traffic.
+            {
+                ruleNo: 100,
+                action: "ALLOW",
+                cidrBlock: "0.0.0.0/0",
+                fromPort: 0,
+                toPort: 65535,
+                protocol: "tcp",
+            },
+        ],
+        tags: { ...fennelStdTags }
+    }, { provider })
+
+    return privateNacl.id
 }
 
-function createPublicRouteTable(vpc: aws.ec2.Vpc): pulumi.Output<string> {
-    const routeTable = new aws.ec2.RouteTable("public-rt", {
-        vpcId: vpc.id,
-    })
-    return routeTable.id
+// TODO: Tighten rules for more security.
+function createPrivateNacl(vpcId: pulumi.Output<string>, subnets: pulumi.Output<string>[], provider: aws.Provider): pulumi.Output<string> {
+    const privateNacl = new aws.ec2.NetworkAcl("private-nacl", {
+        vpcId: vpcId,
+        subnetIds: subnets,
+        egress: [
+            // Allow all egress TCP traffic.
+            {
+                ruleNo: 100,
+                action: "ALLOW",
+                cidrBlock: "0.0.0.0/0",
+                fromPort: 0,
+                toPort: 65535,
+                protocol: "tcp",
+            },
+        ],
+        ingress: [
+            // Allow all ingress TCP traffic.
+            {
+                ruleNo: 100,
+                action: "ALLOW",
+                cidrBlock: "0.0.0.0/0",
+                fromPort: 0,
+                toPort: 65535,
+                protocol: "tcp",
+            },
+        ],
+        tags: { ...fennelStdTags }
+    }, { provider })
+
+    return privateNacl.id
 }
 
-function createSubnet(name: string, vpcId: pulumi.Input<string>, subnet: string, az: string, provider: aws.Provider) {
+function createSubnet(name: string, vpcId: pulumi.Output<string>, subnet: string, az: string, provider: aws.Provider) {
     return new aws.ec2.Subnet(name, {
         vpcId: vpcId,
         cidrBlock: subnet,
@@ -62,6 +118,67 @@ function createSubnet(name: string, vpcId: pulumi.Input<string>, subnet: string,
             ...fennelStdTags,
         }
     }, { provider })
+}
+
+function setupPrivateRouteTable(vpcId: pulumi.Output<string>, subnets: pulumi.Output<string>[], publicSubnet: pulumi.Output<string>, provider: aws.Provider): pulumi.Output<string> {
+    const eip = new aws.ec2.Eip("eip", {
+        tags: { ...fennelStdTags }
+    }, { provider })
+
+    const natGateway = new aws.ec2.NatGateway("nat-gateway", {
+        allocationId: eip.allocationId,
+        subnetId: publicSubnet,
+        tags: { ...fennelStdTags }
+    }, { provider })
+
+    const privateRt = new aws.ec2.RouteTable("private-rt", {
+        vpcId: vpcId,
+        tags: { ...fennelStdTags }
+    }, { provider })
+
+    // Create routes outside route-table so we can add routes in other projects as well.
+    const allowAll = new aws.ec2.Route("allow-all-private-rt", {
+        routeTableId: privateRt.id,
+        destinationCidrBlock: "0.0.0.0/0",
+        gatewayId: natGateway.id,
+    }, { provider })
+
+    subnets.map((subnetId, idx) => {
+        return new aws.ec2.RouteTableAssociation(`rt-assoc-private-${idx}`, {
+            subnetId: subnetId,
+            routeTableId: privateRt.id,
+        }, { provider })
+    })
+
+    return privateRt.id
+}
+
+function setupPublicRouteTable(vpcId: pulumi.Output<string>, subnets: pulumi.Output<string>[], provider: aws.Provider): pulumi.Output<string> {
+    const igw = new aws.ec2.InternetGateway("internet-gateway", {
+        vpcId: vpcId,
+        tags: { ...fennelStdTags }
+    }, { provider })
+
+    const publicRt = new aws.ec2.RouteTable("public-rt", {
+        vpcId: vpcId,
+        tags: { ...fennelStdTags }
+    }, { provider })
+
+    // Create routes outside route-table so we can add routes in other projects as well.
+    const allowAll = new aws.ec2.Route("allow-all-public-rt", {
+        routeTableId: publicRt.id,
+        destinationCidrBlock: "0.0.0.0/0",
+        gatewayId: igw.id,
+    }, { provider })
+
+    subnets.map((subnetId, idx) => {
+        return new aws.ec2.RouteTableAssociation(`rt-assoc-public-${idx}`, {
+            subnetId: subnetId,
+            routeTableId: publicRt.id,
+        }, { provider })
+    })
+
+    return publicRt.id
 }
 
 export const setup = async (input: inputType) => {
@@ -75,13 +192,17 @@ export const setup = async (input: inputType) => {
         }
     })
 
+    const vpcCidr = input.cidr
+
     const vpc = new aws.ec2.Vpc("vpc", {
-        cidrBlock: input.cidr,
+        cidrBlock: vpcCidr,
         tags: {
             "Name": "fennel-vpc",
             ...fennelStdTags
         }
     }, { provider })
+
+    const vpcId = vpc.id;
 
     // Divide the vpc into 4 subnets: 2 private and 2 public.
     const azs = await aws.getAvailabilityZones({}, { provider })
@@ -94,21 +215,34 @@ export const setup = async (input: inputType) => {
     const subnetMask = Number(mask) + 2
 
     let subnet = new netmask.Netmask(`${ip}/${subnetMask}`)
-    const primaryPublicSubnet = createSubnet("fennel-primary-public-subnet", vpc.id, subnet.toString(), primaryAz, provider)
+    const primaryPublicSubnet = createSubnet("fennel-primary-public-subnet", vpcId, subnet.toString(), primaryAz, provider)
 
     subnet = subnet.next()
-    const secondaryPublicSubnet = createSubnet("fennel-secondary-public-subnet", vpc.id, subnet.toString(), secondaryAz, provider)
+    const secondaryPublicSubnet = createSubnet("fennel-secondary-public-subnet", vpcId, subnet.toString(), secondaryAz, provider)
 
     subnet = subnet.next()
-    const primaryPrivateSubnet = createSubnet("fennel-primary-private-subnet", vpc.id, subnet.toString(), primaryAz, provider)
+    const primaryPrivateSubnet = createSubnet("fennel-primary-private-subnet", vpcId, subnet.toString(), primaryAz, provider)
 
     subnet = subnet.next()
-    const secondaryPrivateSubnet = createSubnet("fennel-secondary-private-subnet", vpc.id, subnet.toString(), secondaryAz, provider)
+    const secondaryPrivateSubnet = createSubnet("fennel-secondary-private-subnet", vpcId, subnet.toString(), secondaryAz, provider)
+
+    const privateSubnets = [primaryPrivateSubnet.id, secondaryPrivateSubnet.id];
+    const publicSubnets = [primaryPublicSubnet.id, secondaryPublicSubnet.id];
+
+    const privateNacl = createPrivateNacl(vpcId, privateSubnets, provider)
+    const publicNacl = createPublicNacl(vpcId, publicSubnets, provider)
+
+    const publicRouteTable = setupPublicRouteTable(vpcId, publicSubnets, provider)
+    const privateRouteTable = setupPrivateRouteTable(vpcId, privateSubnets, primaryPublicSubnet.id, provider)
 
     const output: outputType = {
-        vpcId: vpc.id,
-        publicSubnets: [primaryPublicSubnet.id, secondaryPublicSubnet.id],
-        privateSubnets: [primaryPrivateSubnet.id, secondaryPrivateSubnet.id],
+        vpcId,
+        publicSubnets,
+        privateSubnets,
+        privateNacl,
+        publicNacl,
+        privateRouteTable,
+        publicRouteTable,
     }
 
     return output
