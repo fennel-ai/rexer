8138e06f615b77eb454a547eacaccb3a3e51fbc4
Araya <1nikhil9@gmail.com>
2022-02-23 08:22:01 -0800

    go: replace tierid with scope in sql connections

--- go/fennel/db/db.go
+++ go/fennel/db/db.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"fennel/lib/ftypes"
 	"fennel/resource"
 	"fmt"
 	"os"
@@ -14,12 +13,8 @@ import (
 
 type Connection struct {
 	config resource.Config
-	tierID ftypes.TierID
+	scope  resource.Scope
 	*sqlx.DB
-}
-
-func (c Connection) TierID() ftypes.TierID {
-	return c.tierID
 }
 
 func (c Connection) Close() error {
@@ -42,10 +37,6 @@ type SQLiteConfig struct {
 }
 
 func (conf SQLiteConfig) Materialize(scope resource.Scope) (resource.Resource, error) {
-	if scope.GetTierID() == 0 {
-		return nil, fmt.Errorf("tier ID not valid")
-	}
-
 	os.Remove(conf.dbname)
 
 	file, err := os.Create(conf.dbname)
@@ -58,7 +49,7 @@ func (conf SQLiteConfig) Materialize(scope resource.Scope) (resource.Resource, e
 	if err != nil {
 		return nil, err
 	}
-	conn := Connection{config: conf, DB: DB, tierID: scope.GetTierID()}
+	conn := Connection{config: conf, DB: DB, scope: scope}
 	if err = syncSchema(conn.DB, conf.schema); err != nil {
 		return nil, err
 	}
@@ -82,9 +73,6 @@ type MySQLConfig struct {
 var _ resource.Config = MySQLConfig{}
 
 func (conf MySQLConfig) Materialize(scope resource.Scope) (resource.Resource, error) {
-	if scope.GetTierID() == 0 {
-		return nil, fmt.Errorf("tier ID not valid")
-	}
 	connectStr := fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true", conf.Username, conf.Password, conf.Host, conf.DBname)
 	DB, err := sqlx.Connect("mysql", connectStr)
 	if err != nil {
@@ -100,7 +88,7 @@ func (conf MySQLConfig) Materialize(scope resource.Scope) (resource.Resource, er
 	// cause hard-to-debug errors much later.
 	DB.SetConnMaxLifetime(1 * time.Hour)
 
-	conn := Connection{config: conf, DB: DB, tierID: scope.GetTierID()}
+	conn := Connection{config: conf, DB: DB, scope: scope}
 	if err := syncSchema(conn.DB, conf.Schema); err != nil {
 		return nil, err
 	}
