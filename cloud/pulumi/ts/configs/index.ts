c0550ddc8e225ad74aaca1d5c14a66124fd815b7
Abhay Bothra <bothra90@users.noreply.github.com>
2022-02-24 13:12:37 -0800

    configs: refactor project and create for trell tier t-102 (#245)

--- cloud/pulumi/ts/configs/index.ts
+++ cloud/pulumi/ts/configs/index.ts
@@ -1,44 +1,109 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
+import * as process from "process";
 
-const config = new pulumi.Config();
+// TODO: use version from common library.
+// operator for type-safety for string key access:
+// https://schneidenbach.gitbooks.io/typescript-cookbook/content/nameof-operator.html
+export const nameof = <T>(name: keyof T) => name;
 
-const rc = new k8s.core.v1.Secret("redis-config", {
-    stringData: config.requireSecretObject("redis-conf"),
-    metadata: {
-        namespace: "fennel",
-        name: "redis-conf",
-    },
-}, { deleteBeforeReplace: true })
+// TODO: move to common library module.
+export const fennelStdTags = {
+    "managed-by": "fennel.ai",
+}
 
-const cacheConf = new k8s.core.v1.Secret("cache-config", {
-    stringData: config.requireSecretObject("cache-conf"),
-    metadata: {
-        namespace: "fennel",
-        name: "cache-conf"
+export const plugins = {
+    "kubernetes": "v3.15.0"
+}
+
+
+interface map {
+    [key: string]: string;
+}
+
+export type inputType = {
+    kubeconfig: string,
+    tierConfig: map,
+    redisConfig: pulumi.Output<map>,
+    cacheConfig: pulumi.Output<map>,
+    dbConfig: pulumi.Output<map>,
+    kafkaConfig: pulumi.Output<map>,
+}
+
+export type outputType = {}
+
+const parseConfig = (): inputType => {
+    const config = new pulumi.Config();
+    return {
+        tierConfig: config.requireObject(nameof<inputType>("tierConfig")),
+        kubeconfig: config.require(nameof<inputType>("kubeconfig")),
+        redisConfig: config.requireSecretObject(nameof<inputType>("redisConfig")),
+        cacheConfig: config.requireSecretObject(nameof<inputType>("cacheConfig")),
+        dbConfig: config.requireSecretObject(nameof<inputType>("dbConfig")),
+        kafkaConfig: config.requireSecretObject(nameof<inputType>("kafkaConfig")),
     }
-}, { deleteBeforeReplace: true })
+}
 
-const kafkaCreds = new k8s.core.v1.Secret("kafka-config", {
-    stringData: config.requireSecretObject("kafka-conf"),
-    metadata: {
-        name: "kafka-conf",
-        namespace: "fennel",
-    }
-}, { deleteBeforeReplace: true })
+export const setup = async (input: inputType) => {
+    const provider = new k8s.Provider("k8s-provider", {
+        kubeconfig: input.kubeconfig,
+    })
 
-const pscreds = new k8s.core.v1.Secret("db-config", {
-    stringData: config.requireSecretObject("db-conf"),
-    metadata: {
-        namespace: "fennel",
-        name: "mysql-conf",
-    }
-}, { deleteBeforeReplace: true })
+    const rc = new k8s.core.v1.Secret("redis-config", {
+        stringData: input.redisConfig,
+        metadata: {
+            namespace: "fennel",
+            name: "redis-conf",
+        },
+    }, { provider, deleteBeforeReplace: true })
 
-const tierid = new k8s.core.v1.ConfigMap("tier-conf", {
-    data: config.requireObject("tier-conf"),
-    metadata: {
-        namespace: "fennel",
-        name: "tier-conf",
+    const cacheConf = new k8s.core.v1.Secret("cache-config", {
+        stringData: input.cacheConfig,
+        metadata: {
+            namespace: "fennel",
+            name: "cache-conf"
+        }
+    }, { provider, deleteBeforeReplace: true })
+
+    const kafkaCreds = new k8s.core.v1.Secret("kafka-config", {
+        stringData: input.kafkaConfig,
+        metadata: {
+            name: "kafka-conf",
+            namespace: "fennel",
+        }
+    }, { provider, deleteBeforeReplace: true })
+
+    const dbCreds = new k8s.core.v1.Secret("db-config", {
+        stringData: input.dbConfig,
+        metadata: {
+            namespace: "fennel",
+            name: "mysql-conf",
+        }
+    }, { provider, deleteBeforeReplace: true })
+
+    const tierConf = new k8s.core.v1.ConfigMap("tier-conf", {
+        data: input.tierConfig,
+        metadata: {
+            namespace: "fennel",
+            name: "tier-conf",
+        }
+    }, { provider, deleteBeforeReplace: true })
+    const output: outputType = {}
+    return output
+}
+
+async function run() {
+    let output: outputType | undefined;
+    // Run the main function only if this program is run through the pulumi CLI.
+    // Unfortunately, in that case the argv0 itself is not "pulumi", but the full
+    // path of node: e.g. /nix/store/7q04aq0sq6im9a0k09gzfa1xfncc0xgm-nodejs-14.18.1/bin/node
+    if (process.argv0 !== 'node') {
+        pulumi.log.info("Running...")
+        const input: inputType = parseConfig();
+        output = await setup(input)
     }
-}, { deleteBeforeReplace: true })
+    return output
+}
+
+
+export const output = await run();
