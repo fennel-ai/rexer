189ae1a544395ec46692c8fff678b1f141f7dc16
Abhay Bothra <bothra90@users.noreply.github.com>
2022-02-24 16:37:05 -0800

    telemetry: migrate to form callable from automation api

--- cloud/pulumi/ts/telemetry/index.ts
+++ cloud/pulumi/ts/telemetry/index.ts
@@ -1,14 +1,46 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
+import * as process from "process";
+import { input } from "@pulumi/aws/types";
 
-export = async () => {
-    setupOtelPolicy();
-    await setupAdotCollector();
-    await setupFluentBit();
-    return { MonitoredDeployment, MonitoredReplicaSet }
+// TODO: use version from common library.
+// operator for type-safety for string key access:
+// https://schneidenbach.gitbooks.io/typescript-cookbook/content/nameof-operator.html
+const nameof = <T>(name: keyof T) => name;
+
+// TODO: move to common library module.
+const fennelStdTags = {
+    "managed-by": "fennel.ai",
 }
 
+export const plugins = {
+    "aws": "v4.0.0",
+    "kubernetes": "v3.15.0",
+}
+
+export type inputType = {
+    roleArn: string,
+    region: string,
+    kubeconfig: string,
+    eksClusterName: string,
+    nodeInstanceRole: string,
+}
+
+export type outputType = {}
+
+const parseConfig = (): inputType => {
+    const config = new pulumi.Config();
+    return {
+        roleArn: config.require(nameof<inputType>("roleArn")),
+        region: config.require(nameof<inputType>("region")),
+        kubeconfig: config.require(nameof<inputType>("kubeconfig")),
+        eksClusterName: config.require(nameof<inputType>("eksClusterName")),
+        nodeInstanceRole: config.require(nameof<inputType>("nodeInstanceRole")),
+    }
+}
+
+// TODO: move to library.
 class MonitoredDeployment extends k8s.apps.v1.Deployment {
     constructor(name: string,
         args: k8s.apps.v1.Deployment,
@@ -22,6 +54,7 @@ class MonitoredDeployment extends k8s.apps.v1.Deployment {
     }
 }
 
+// TODO: move to library.
 class MonitoredReplicaSet extends k8s.apps.v1.ReplicaSet {
     constructor(name: string,
         args: k8s.apps.v1.ReplicaSet,
@@ -35,7 +68,7 @@ class MonitoredReplicaSet extends k8s.apps.v1.ReplicaSet {
     }
 }
 
-function setupOtelPolicy() {
+function setupOtelPolicy(input: inputType, awsProvider: aws.Provider) {
     const rawPolicyStr = `{
         "Version": "2012-10-17",
         "Statement": [
@@ -64,23 +97,19 @@ function setupOtelPolicy() {
     const policy = new aws.iam.Policy("otel-collector-policy", {
         namePrefix: "AWSDistroOpenTelemetryPolicy-",
         policy: rawPolicyStr,
-    });
+    }, { provider: awsProvider });
 
 
-    const config = new pulumi.Config();
     const attachOtelPolicy = new aws.iam.RolePolicyAttachment("otel-instance", {
         policyArn: policy.arn,
-        role: config.require("instanceRole"),
-    });
+        role: input.nodeInstanceRole,
+    }, { provider: awsProvider });
 
 }
 
 // Setup the ADOT (AWS Distro for OpenTelemetry) Collector to collect metrics
 // and traces and forward them to cloudwatch.
-async function setupAdotCollector() {
-    const region = (await aws.getRegion()).name;
-    const config = new pulumi.Config();
-    const eksClusterName = config.require("eksClusterName");
+async function setupAdotCollector(input: inputType, k8sProvider: k8s.Provider) {
     const collector = new k8s.yaml.ConfigFile("adot-collector", {
         file: "otel-deployment.yaml",
         transformations: [
@@ -90,23 +119,23 @@ async function setupAdotCollector() {
                     obj.spec.template.spec.containers = containers.map((container: any) => {
                         container.env.push({
                             name: "AWS_REGION",
-                            value: region,
+                            value: input.region,
                         } as k8s.types.output.core.v1.EnvVar)
                         container.env.push({
                             name: "OTEL_RESOURCE_ATTRIBUTES",
-                            value: `ClusterName=${eksClusterName}`,
+                            value: `ClusterName=${input.eksClusterName}`,
                         } as k8s.types.output.core.v1.EnvVar)
                         return container
                     })
                 }
             },
         ],
-    })
+    }, { provider: k8sProvider })
 }
 
 // Setup fluentbit as a daemon-set following instructions at:
 // https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Container-Insights-setup-logs-FluentBit.html
-async function setupFluentBit() {
+async function setupFluentBit(input: inputType, k8sProvider: k8s.Provider) {
     const ns = new k8s.core.v1.Namespace("cloudwatch-ns", {
         metadata: {
             name: "amazon-cloudwatch",
@@ -114,28 +143,65 @@ async function setupFluentBit() {
                 "name": "amazon-cloudwatch",
             },
         },
-    })
+    }, { provider: k8sProvider })
 
-    const region = (await aws.getRegion()).name;
-    const config = new pulumi.Config();
-    const eksClusterName = config.require("eksClusterName");
     const cm = new k8s.core.v1.ConfigMap("cluster-info-configmap", {
         data: {
-            "cluster.name": eksClusterName,
+            "cluster.name": input.eksClusterName,
             "read.head": "Off",
             "http.server": "On",
             "http.port": "2020",
             "read.tail": "On",
-            "logs.region": region,
+            "logs.region": input.region,
         },
         metadata: {
             name: "fluent-bit-cluster-info",
             namespace: ns.id,
         }
-    })
+    }, { provider: k8sProvider })
 
     const deployment = new k8s.yaml.ConfigFile("fluent-bit-config", {
         file: "fluent-bit.yaml",
-    }, { dependsOn: cm })
+    }, {
+        provider: k8sProvider,
+        dependsOn: cm
+    })
 
 }
+
+const setup = async (input: inputType) => {
+    const awsProvider = new aws.Provider("aws-provider", {
+        region: <aws.Region>input.region,
+        assumeRole: {
+            roleArn: input.roleArn,
+            // TODO: Also populate the externalId field to prevent "confused deputy"
+            // attacks: https://docs.aws.amazon.com/IAM/latest/UserGuide/confused-deputy.html
+        }
+    })
+
+    const k8sProvider = new k8s.Provider("k8s-provider", {
+        kubeconfig: input.kubeconfig,
+    })
+
+    setupOtelPolicy(input, awsProvider);
+    await setupAdotCollector(input, k8sProvider);
+    await setupFluentBit(input, k8sProvider);
+    const output: outputType = {}
+    return output
+}
+
+async function run() {
+    let output: outputType | undefined;
+    // Run the main function only if this program is run through the pulumi CLI.
+    // Unfortunately, in that case the argv0 itself is not "pulumi", but the full
+    // path of node: e.g. /nix/store/7q04aq0sq6im9a0k09gzfa1xfncc0xgm-nodejs-14.18.1/bin/node
+    if (process.argv0 !== 'node') {
+        pulumi.log.info("Running...")
+        const input: inputType = parseConfig();
+        output = await setup(input)
+    }
+    return output
+}
+
+
+export const output = await run();
