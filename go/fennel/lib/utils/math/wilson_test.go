37abb5d18d2da6ac8ee5a287c3466d188cb6ff04
Nikhil Garg <nikhilgarg28@gmail.com>
2022-02-28 14:09:37 -0800

    utils/math: export wilson function

--- go/fennel/lib/utils/math/wilson_test.go
+++ go/fennel/lib/utils/math/wilson_test.go
@@ -1,21 +1,22 @@
 package math
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestWilson(t *testing.T) {
-	s, err := wilson(35, 200, true)
+	s, err := Wilson(35, 200, true)
 	assert.NoError(t, err)
 	assert.Equal(t, 0.12860441174608936, s)
-	s, err = wilson(35, 200, false)
+	s, err = Wilson(35, 200, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 0.23364549210081922, s)
 
 	// and if num is < den, we also get error
-	_, err = wilson(201, 200, true)
+	_, err = Wilson(201, 200, true)
 	assert.Error(t, err)
-	_, err = wilson(201, 200, false)
+	_, err = Wilson(201, 200, false)
 	assert.Error(t, err)
 }
