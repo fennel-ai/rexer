2c111c447f0555a55823b40192481010c8bb64a1
Araya <1nikhil9@gmail.com>
2022-02-28 21:03:45 -0800

    go: add tests

---
+++ go/fennel/mothership/controller/launchrequest/launchrequest_test.go
@@ -0,0 +1,30 @@
+package launchrequest
+
+import (
+	"testing"
+
+	"fennel/mothership"
+	"fennel/mothership/model/launchrequest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessCompletedRequests(t *testing.T) {
+	m, err := mothership.Create()
+	assert.NoError(t, err)
+
+	reqID, err := launchrequest.InsertRequest(m, []byte(`{}`), []byte(`{"state":"COMPLETED"}`))
+	assert.NoError(t, err)
+	_, err = launchrequest.InsertRequest(m, []byte(`{}`), []byte(`{"state":"PENDING"}`))
+	assert.NoError(t, err)
+
+	completed, err := launchrequest.GetCompletedRequestIDs(m)
+	assert.Len(t, completed, 1)
+	assert.Equal(t, reqID, completed[0])
+
+	// now process the completed request and check there are no completed requests in table
+	err = ProcessCompletedRequests(m)
+	assert.NoError(t, err)
+	completed, err = launchrequest.GetCompletedRequestIDs(m)
+	assert.NoError(t, err)
+	assert.Len(t, completed, 0)
+}
