99479688e89d8daeab239baa0680dbef35eab289
Araya <1nikhil9@gmail.com>
2022-02-28 23:33:09 -0800

    go: use flags for controlserver

--- go/fennel/service/controlserver/main.go
+++ go/fennel/service/controlserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"time"
@@ -10,8 +11,6 @@ import (
 )
 
 const (
-	serverAddress       = ":2475"
-	endpoint            = "http://localhost:2425"
 	requestPollingDelay = time.Minute
 )
 
@@ -27,12 +26,16 @@ func pollLaunchRequests(m mothership.Mothership) {
 }
 
 func main() {
+	endpoint := flag.String("CONTROL_SERVER_ENDPOINT", "http://localhost:2425", "server address to connect to")
+	serverAddress := flag.String("CONTROL_SERVER_ADDRESS", ":2475", "address of the control server")
+	flag.Parse()
+
 	m, err := mothership.Create()
 	if err != nil {
 		log.Fatalf("Error creating mothership: %v", err)
 	}
 
-	server := createServer(serverAddress, endpoint)
+	server := createServer(*serverAddress, *endpoint)
 	go pollLaunchRequests(m)
 
 	log.Printf("starting http service on '%s'\n", server.address)
