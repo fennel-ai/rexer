dcdf311c6bf02a04dea073432f4b4a62a68773ed
Araya <1nikhil9@gmail.com>
2022-03-07 15:35:00 -0800

    go: rename mothership controller/model files

--- go/fennel/mothership/controller/launchrequest/defs_test.go
+++ go/fennel/mothership/controller/launchrequest/defs_test.go
@@ -1,6 +1,7 @@
 package launchrequest
 
 import (
+	"log"
 	"testing"
 
 	"fennel/mothership"
@@ -9,19 +10,20 @@ import (
 )
 
 func TestProcessCompletedRequests(t *testing.T) {
+	log.Print("w")
 	m, err := mothership.NewTestMothership()
 	assert.NoError(t, err)
 	defer mothership.Teardown(m)
-
+	log.Print("x")
 	reqID, err := launchrequest.InsertRequest(m, []byte(`{}`), []byte(`{"state":"COMPLETED"}`))
 	assert.NoError(t, err)
 	_, err = launchrequest.InsertRequest(m, []byte(`{}`), []byte(`{"state":"PENDING"}`))
 	assert.NoError(t, err)
-
+	log.Print("y")
 	completed, err := launchrequest.GetCompletedRequestIDs(m)
 	assert.Len(t, completed, 1)
 	assert.Equal(t, reqID, completed[0])
-
+	log.Print("z")
 	// now process the completed request and check there are no completed requests in table
 	err = ProcessCompletedRequests(m)
 	assert.NoError(t, err)
