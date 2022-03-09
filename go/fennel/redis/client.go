2d70dd5f8f675829c7c6f229ee4ca8ea5106e8eb
Araya <1nikhil9@gmail.com>
2022-03-09 01:57:48 -0800

    redis: add SetNX and SetNXPipelined functions

--- go/fennel/redis/client.go
+++ go/fennel/redis/client.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"fmt"
+	"log"
 	"time"
 
 	"fennel/lib/timer"
@@ -14,6 +15,12 @@ func (c Client) Set(ctx context.Context, k string, v interface{}, ttl time.Durat
 	defer timer.Start(ctx, c.ID(), "redis.set").Stop()
 	k = c.tieredKey(k)
 	return c.client.Set(ctx, k, v, ttl).Err()
+}
+
+func (c Client) SetNX(ctx context.Context, key string, v interface{}, ttl time.Duration) (bool, error) {
+	defer timer.Start(ctx, c.ID(), "redis.setnx").Stop()
+	key = c.tieredKey(key)
+	return c.client.SetNX(ctx, key, v, ttl).Result()
 }
 
 func (c Client) Del(ctx context.Context, k ...string) error {
@@ -80,6 +87,38 @@ func (c Client) MSet(ctx context.Context, keys []string, values []interface{}, t
 	}
 	_, err := pipe.Exec(ctx)
 	return err
+}
+
+// SetNXPipelined pipelines and executes multiple SetNX commands and returns their result as a list of bools
+// Returns any error before execution but ignores errors that happen during execution
+func (c Client) SetNXPipelined(ctx context.Context, keys []string, values []interface{}, ttls []time.Duration) (
+	ok []bool, err error) {
+	defer timer.Start(ctx, c.ID(), "redis.setnx_pipelined").Stop()
+	// nothing to write if there are no keys.
+	if len(keys) == 0 {
+		return nil, nil
+	}
+	if len(keys) != len(values) || len(keys) != len(ttls) {
+		return nil, fmt.Errorf("keys, values, and ttls should all be slices of the same length")
+	}
+	pipe := c.client.Pipeline()
+	for i, key := range keys {
+		err = pipe.SetNX(ctx, c.tieredKey(key), values[i], ttls[i]).Err()
+		// Return errors that happen before execution
+		if err != nil {
+			return nil, err
+		}
+	}
+	cmds, _ := pipe.Exec(ctx)
+	ok = make([]bool, len(keys))
+	for i, cmd := range cmds {
+		ok[i], err = cmd.(*redis.BoolCmd).Result()
+		// Log errors that happened during execution
+		if err != nil {
+			log.Printf("Redis Error: SetNXPipelined(): %v", err)
+		}
+	}
+	return ok, nil
 }
 
 func (c Client) tieredKey(k string) string {
--- go/fennel/redis/client_integration_test.go
+++ go/fennel/redis/client_integration_test.go
@@ -7,6 +7,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"math/rand"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -52,7 +53,7 @@ func TestMultiSetTTL(t *testing.T) {
 		assert.Equal(t, redis.Nil, err)
 	}
 	ttl1 := time.Second
-	ttl2 := time.Second + ttl1
+	ttl2 := 5*time.Second + ttl1
 	assert.NoError(t, c.MSet(ctx, []string{k1, k2, k3}, vals, []time.Duration{ttl1, ttl2, ttl1}))
 	for i, k := range []string{k1, k2, k3} {
 		found, err := c.Get(ctx, k)
@@ -70,7 +71,7 @@ func TestMultiSetTTL(t *testing.T) {
 	assert.Equal(t, vals[1], found)
 
 	// sleep some more and even this should be gone
-	time.Sleep(ttl1)
+	time.Sleep(ttl2 - ttl1)
 	_, err = c.Get(ctx, k2)
 	assert.Equal(t, redis.Nil, err)
 }
@@ -138,4 +139,103 @@ func TestClientConfig_Materialize_Invalid(t *testing.T) {
 			assert.Error(t, err)
 		})
 	}
+}
+
+func TestSetNX(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	tierID := ftypes.RealmID(rand.Uint32())
+	scope := resource.NewTierScope(tierID)
+	conf := ClientConfig{Addr: addr, TLSConfig: &tls.Config{}, Scope: scope}
+	rdb, err := conf.Materialize()
+	assert.NoError(t, err)
+
+	ctx := context.Background()
+	c := rdb.(Client)
+	ttl := 5 * time.Second
+
+	// set key works initially
+	ok, err := c.SetNX(ctx, "key", 1, ttl)
+	assert.NoError(t, err)
+	assert.Equal(t, true, ok)
+
+	// setting again should not work
+	ok, err = c.SetNX(ctx, "key", 1, ttl)
+	assert.NoError(t, err)
+	assert.Equal(t, false, ok)
+
+	// sleep and set again; works because key expired
+	time.Sleep(ttl)
+	ok, err = c.SetNX(ctx, "key", 1, ttl)
+	assert.NoError(t, err)
+	assert.Equal(t, true, ok)
+}
+
+func TestSetNXPipelined(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	tierID := ftypes.RealmID(rand.Uint32())
+	scope := resource.NewTierScope(tierID)
+	conf := ClientConfig{Addr: addr, TLSConfig: &tls.Config{}, Scope: scope}
+	rdb, err := conf.Materialize()
+	assert.NoError(t, err)
+
+	ctx := context.Background()
+	c := rdb.(Client)
+	n := 5
+	ttl := 5 * time.Second
+	ttls := make([]time.Duration, n)
+	keys := make([]string, n)
+	values := make([]interface{}, n)
+	exp := make([]bool, n)
+
+	// try setting with no elements
+	_, err = c.SetNXPipelined(ctx, nil, nil, nil)
+	assert.NoError(t, err)
+
+	// set 5 keys, all should succeed
+	for i := 0; i < n; i++ {
+		ttls[i] = ttl
+		keys[i] = strconv.Itoa(i)
+		values[i] = 1
+		exp[i] = true
+	}
+	ok, err := c.SetNXPipelined(ctx, keys, values, ttls)
+	assert.NoError(t, err)
+	assert.Equal(t, exp, ok)
+
+	// try setting the same 5 keys again, should fail because already set
+	for i := 0; i < n; i++ {
+		exp[i] = false
+	}
+	ok, err = c.SetNXPipelined(ctx, keys, values, ttls)
+	assert.NoError(t, err)
+	assert.Equal(t, exp, ok)
+
+	// sleep until keys expire and try again, should succeed
+	for i := 0; i < n; i++ {
+		exp[i] = true
+	}
+	time.Sleep(ttl)
+	ok, err = c.SetNXPipelined(ctx, keys, values, ttls)
+	assert.NoError(t, err)
+	assert.Equal(t, exp, ok)
+
+	// try setting some new keys, only new keys should set successfully
+	keys = []string{"3", "4", "5", "6", "7"}
+	exp = []bool{false, false, true, true, true}
+	ok, err = c.SetNXPipelined(ctx, keys, values, ttls)
+	assert.NoError(t, err)
+	assert.Equal(t, exp, ok)
+
+	// try pipelining with multiple instances of same keys
+	keys = []string{"a", "b", "a", "b", "a"}
+	ok, err = c.SetNXPipelined(ctx, keys, values, ttls)
+	assert.NoError(t, err)
+	count := 0
+	for i := range ok {
+		if ok[i] {
+			count++
+		}
+	}
+	// number of set keys should be the number of unique keys
+	assert.Equal(t, 2, count)
 }
