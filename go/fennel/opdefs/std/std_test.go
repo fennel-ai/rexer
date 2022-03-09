128edf250fcd8fde2dc34047c25e54b246a26716
Mohit Reddy <mohitreddy1996@gmail.com>
2022-03-08 20:31:13 -0800

    go: use default instance of tier.

--- go/fennel/opdefs/std/std_test.go
+++ go/fennel/opdefs/std/std_test.go
@@ -3,18 +3,12 @@ package std
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"fennel/lib/value"
-	"fennel/test"
 	"fennel/test/optest"
+	"fennel/tier"
 )
 
 func TestFilterOperator_Apply(t *testing.T) {
-	tier, err := test.Tier()
-	assert.NoError(t, err)
-	defer test.Teardown(tier)
-
 	intable := []value.Dict{
 		{"a.inner": value.Int(1), "b": value.String("hi")},
 		{"a.inner": value.Int(1), "b": value.String("bye")},
@@ -31,18 +25,15 @@ func TestFilterOperator_Apply(t *testing.T) {
 		{"a.inner": value.Int(7), "b": value.String("hello")},
 	}
 
-	optest.Assert(t, tier, &FilterOperator{}, whereTrue, intable, contextKwargTable, expected)
+	tr := tier.Tier{}
+	optest.Assert(t, tr, &FilterOperator{}, whereTrue, intable, contextKwargTable, expected)
 
 	// and when we filter everything, we should get empty table
 	contextKwargTable = []value.Dict{whereFalse, whereFalse, whereFalse}
-	optest.Assert(t, tier, &FilterOperator{}, whereTrue, intable, contextKwargTable, []value.Dict{})
+	optest.Assert(t, tr, &FilterOperator{}, whereTrue, intable, contextKwargTable, []value.Dict{})
 }
 
 func TestTakeOperator_Apply(t *testing.T) {
-	tier, err := test.Tier()
-	assert.NoError(t, err)
-	defer test.Teardown(tier)
-
 	intable := []value.Dict{
 		{"a.inner": value.Int(1), "b": value.String("hi")},
 		{"a.inner": value.Int(1), "b": value.String("bye")},
@@ -55,84 +46,68 @@ func TestTakeOperator_Apply(t *testing.T) {
 		{"a.inner": value.Int(1), "b": value.String("bye")},
 	}
 	contextKwargTable := []value.Dict{{}, {}, {}}
-	optest.Assert(t, tier, &TakeOperator{}, value.Dict{"limit": value.Int(2)}, intable, contextKwargTable, expected)
+	tr := tier.Tier{}
+	optest.Assert(t, tr, &TakeOperator{}, value.Dict{"limit": value.Int(2)}, intable, contextKwargTable, expected)
 
 	// and when the limit is very large, it only returns intable as it is
-	optest.Assert(t, tier, &TakeOperator{}, value.Dict{"limit": value.Int(10000)}, intable, contextKwargTable, intable)
+	optest.Assert(t, tr, &TakeOperator{}, value.Dict{"limit": value.Int(10000)}, intable, contextKwargTable, intable)
 }
 
 func TestExplodeOperator_KeyNotString(t *testing.T) {
-	tier, err := test.Tier()
-	assert.NoError(t, err)
-	defer test.Teardown(tier)
-
 	intable := []value.Dict{
 		{"a.list": value.List{value.Int(1), value.Int(5)}, "b": value.String("hi")},
 	}
 
+	tr := tier.Tier{}
 	skwargs := value.Dict{"keys": value.Int(2)}
-	optest.AssertError(t, tier, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
+	optest.AssertError(t, tr, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
 	skwargs = value.Dict{"keys": value.List{value.Int(2)}}
-	optest.AssertError(t, tier, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
+	optest.AssertError(t, tr, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
 	skwargs = value.Dict{"keys": value.List{value.String("a.list"), value.Int(2)}}
-	optest.AssertError(t, tier, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
+	optest.AssertError(t, tr, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
 }
 
 func TestExplodeOperator_KeyNotPresent(t *testing.T) {
-	tier, err := test.Tier()
-	assert.NoError(t, err)
-	defer test.Teardown(tier)
-
 	intable := []value.Dict{
 		{"a.list": value.List{value.Int(1), value.Int(5)}, "b": value.String("hi")},
 	}
 
+	tr := tier.Tier{}
 	skwargs := value.Dict{"keys": value.String("c")}
-	optest.AssertError(t, tier, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
+	optest.AssertError(t, tr, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
 }
 
 func TestExplodeOperator_ListScalarKeys(t *testing.T) {
-	tier, err := test.Tier()
-	assert.NoError(t, err)
-	defer test.Teardown(tier)
 	intable := []value.Dict{
 		{"a.list": value.List{value.Int(1), value.Int(5)}, "b": value.String("hi")},
 	}
 
+	tr := tier.Tier{}
 	skwargs := value.Dict{"keys": value.List{value.String("a.list"), value.String("b")}}
-	optest.AssertError(t, tier, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
+	optest.AssertError(t, tr, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
 }
 
 func TestExplodeOperator_ScalarKeys(t *testing.T) {
-	tier, err := test.Tier()
-	assert.NoError(t, err)
-	defer test.Teardown(tier)
 	intable := []value.Dict{
 		{"a": value.Int(1), "b": value.String("hi")},
 	}
 
+	tr := tier.Tier{}
 	skwargs := value.Dict{"keys": value.List{value.String("a"), value.String("b")}}
-	optest.Assert(t, tier, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}}, intable)
+	optest.Assert(t, tr, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}}, intable)
 }
 
 func TestExplodeOperator_NonMatchingRowWiseElements(t *testing.T) {
-	tier, err := test.Tier()
-	assert.NoError(t, err)
-	defer test.Teardown(tier)
-
 	intable := []value.Dict{
 		{"a.list": value.List{value.Int(1), value.Int(5)}, "b": value.List{value.String("hi")}},
 	}
 
+	tr := tier.Tier{}
 	skwargs := value.Dict{"keys": value.List{value.String("a.list"), value.String("b")}}
-	optest.AssertError(t, tier, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
+	optest.AssertError(t, tr, &ExplodeOperator{}, skwargs, intable, []value.Dict{{}})
 }
 
 func TestExplodeOperator_Apply(t *testing.T) {
-	tier, err := test.Tier()
-	assert.NoError(t, err)
-	defer test.Teardown(tier)
-
 	intable := []value.Dict{
 		{"a.list": value.List{value.Int(1), value.Int(5)}, "b": value.String("hi")},
 		{"a.list": value.List{value.Int(10), value.Int(15)}, "b": value.String("bye")},
@@ -150,17 +125,14 @@ func TestExplodeOperator_Apply(t *testing.T) {
 		{"a.list": value.Int(8), "b": value.String("hello")},
 	}
 
+	tr := tier.Tier{}
 	skwargs := value.Dict{"keys": value.String("a.list")}
-	optest.Assert(t, tier, &ExplodeOperator{}, skwargs, intable, contextKwargTable, expected)
+	optest.Assert(t, tr, &ExplodeOperator{}, skwargs, intable, contextKwargTable, expected)
 	skwargs = value.Dict{"keys": value.List{value.String("a.list")}}
-	optest.Assert(t, tier, &ExplodeOperator{}, skwargs, intable, contextKwargTable, expected)
+	optest.Assert(t, tr, &ExplodeOperator{}, skwargs, intable, contextKwargTable, expected)
 }
 
 func TestExplodeOperator_ApplyListKeys(t *testing.T) {
-	tier, err := test.Tier()
-	assert.NoError(t, err)
-	defer test.Teardown(tier)
-
 	intable := []value.Dict{
 		{"a.list": value.List{value.Int(1), value.Int(5)}, "b.list": value.List{value.String("hi"), value.String("hello")}},
 		{"a.list": value.List{value.Int(10)}, "b.list": value.List{value.String("hi")}},
@@ -176,7 +148,8 @@ func TestExplodeOperator_ApplyListKeys(t *testing.T) {
 		{"a.list": value.Nil, "b.list": value.Nil},
 	}
 
+	tr := tier.Tier{}
 	skwargs := value.Dict{"keys": value.List{value.String("a.list"), value.String("b.list")}}
 	// should work with a list of strings
-	optest.Assert(t, tier, &ExplodeOperator{}, skwargs, intable, contextKwargTable, expected)
+	optest.Assert(t, tr, &ExplodeOperator{}, skwargs, intable, contextKwargTable, expected)
 }
