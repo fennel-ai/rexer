9e9826b798af6da8eafc7a60d9d8cb37659c4931
Nikhil <70129397+1nikhil9@users.noreply.github.com>
2022-03-11 00:20:23 -0800

    go: refactor sort operator code (#383)

--- go/fennel/opdefs/std/sort.go
+++ go/fennel/opdefs/std/sort.go
@@ -12,31 +12,6 @@ type SortOperator struct{}
 
 var _ operators.Operator = SortOperator{}
 
-type sortableRows struct {
-	data value.List
-	keys []float64
-	desc value.Bool
-}
-
-func (s sortableRows) Len() int {
-	return len(s.data)
-}
-
-func (s sortableRows) Less(i, j int) bool {
-	if s.desc {
-		return s.keys[j] < s.keys[i]
-	} else {
-		return s.keys[i] < s.keys[j]
-	}
-}
-
-func (s sortableRows) Swap(i, j int) {
-	s.data[i], s.data[j] = s.data[j], s.data[i]
-	s.keys[i], s.keys[j] = s.keys[j], s.keys[i]
-}
-
-var _ sort.Interface = sortableRows{}
-
 func (op SortOperator) New(_ value.Dict, _ map[string]interface{}) (operators.Operator, error) {
 	return SortOperator{}, nil
 }
@@ -49,7 +24,11 @@ func (op SortOperator) Signature() *operators.Signature {
 }
 
 func (op SortOperator) Apply(staticKwargs value.Dict, in operators.InputIter, out *value.List) error {
-	rows := sortableRows{}
+	type sortableRow struct {
+		data value.Value
+		key  float64
+	}
+	var rows []sortableRow
 	for in.HasMore() {
 		row, contextKwargs, err := in.Next()
 		if err != nil {
@@ -66,13 +45,17 @@ func (op SortOperator) Apply(staticKwargs value.Dict, in operators.InputIter, ou
 			expType := op.Signature().ContextKwargs["by"].Type
 			return fmt.Errorf("value of context kwarg 'by' is not of type '%s': %s", expType, expType.Validate(key))
 		}
-		rows.data = append(rows.data, row)
-		rows.keys = append(rows.keys, v)
+		rows = append(rows, sortableRow{data: row, key: v})
 	}
-	rows.desc = staticKwargs["desc"].(value.Bool)
-	sort.Stable(rows)
-	for _, row := range rows.data {
-		if err := out.Append(row); err != nil {
+	sort.SliceStable(rows, func(i, j int) bool {
+		if !staticKwargs["desc"].(value.Bool) {
+			return rows[i].key < rows[j].key
+		} else {
+			return rows[i].key > rows[j].key
+		}
+	})
+	for _, row := range rows {
+		if err := out.Append(row.data); err != nil {
 			return err
 		}
 	}
