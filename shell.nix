91f5f49998a26e95f46193070bae414e0d82c921
Abhay Bothra <bothra90@users.noreply.github.com>
2022-03-17 16:28:45 -0700

    go: update to 1.18 on unstable channel

--- shell.nix
+++ shell.nix
@@ -15,7 +15,7 @@ with pkgs; mkShell {
     pkgs.protobuf3_17
 
     # Packages for go development.
-    pkgs.go_1_17
+    unstable.pkgs.go_1_18
     pkgs.protoc-gen-go
 
     # Packages to build kafka go client
