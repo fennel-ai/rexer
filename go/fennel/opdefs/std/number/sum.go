fb8cbab8fd18cf9ba9eb22ad26b78633113ce41a
Nikhil Garg <nikhilgarg28@gmail.com>
2022-03-24 16:41:20 -0700

    operators: add sum, mean, all, any

---
+++ go/fennel/opdefs/std/number/sum.go
@@ -0,0 +1,54 @@
+package number
+
+import (
+	"fennel/engine/operators"
+	"fennel/lib/value"
+)
+
+func init() {
+	operators.Register(adder{})
+}
+
+type adder struct{}
+
+func (a adder) New(args value.Dict, bootargs map[string]interface{}) (operators.Operator, error) {
+	return adder{}, nil
+}
+
+func (a adder) Apply(kwargs value.Dict, in operators.InputIter, out *value.List) error {
+	sum, float := extract(kwargs.GetUnsafe("start"))
+
+	for in.HasMore() {
+		heads, _, err := in.Next()
+		if err != nil {
+			return err
+		}
+		thisNum, thisFloat := extract(heads[0])
+		sum += thisNum
+		float = float || thisFloat
+	}
+	var ret value.Value = value.Double(sum)
+	if !float {
+		ret = value.Int(sum)
+	}
+	return out.Append(ret)
+}
+
+func extract(n value.Value) (float64, bool) {
+	switch t := n.(type) {
+	case value.Int:
+		return float64(t), false
+	case value.Double:
+		return float64(t), true
+	default:
+		panic("this should not happen")
+	}
+}
+
+func (a adder) Signature() *operators.Signature {
+	return operators.NewSignature("std", "sum").
+		Input([]value.Type{value.Types.Number}).
+		Param("start", value.Types.Number, true, true, value.Int(0))
+}
+
+var _ operators.Operator = adder{}
