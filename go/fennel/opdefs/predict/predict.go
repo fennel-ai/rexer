02b3b292d77b3d04dfcf7800d671e0ea3aea197d
Abhay Bothra <bothra90@users.noreply.github.com>
2022-03-29 00:44:20 -0700

    minor: rename inferenceOperator struct to predictOperator (#508)

--- go/fennel/opdefs/predict/predict.go
+++ go/fennel/opdefs/predict/predict.go
@@ -11,26 +11,26 @@ import (
 )
 
 func init() {
-	if err := operators.Register(inferenceOperator{}); err != nil {
+	if err := operators.Register(predictOperator{}); err != nil {
 		panic(err)
 	}
 }
 
-type inferenceOperator struct {
+type predictOperator struct {
 	tier tier.Tier
 }
 
-var _ operators.Operator = inferenceOperator{}
+var _ operators.Operator = predictOperator{}
 
-func (iop inferenceOperator) New(args value.Dict, bootargs map[string]interface{}) (operators.Operator, error) {
+func (pop predictOperator) New(args value.Dict, bootargs map[string]interface{}) (operators.Operator, error) {
 	tr, err := bootarg.GetTier(bootargs)
 	if err != nil {
 		return nil, err
 	}
-	return inferenceOperator{tr}, nil
+	return predictOperator{tr}, nil
 }
 
-func (iop inferenceOperator) Signature() *operators.Signature {
+func (pop predictOperator) Signature() *operators.Signature {
 	return operators.NewSignature("std", "predict").
 		Input([]value.Type{value.Types.Any}).
 		// TODO: change to Tuple when ready.
@@ -39,7 +39,7 @@ func (iop inferenceOperator) Signature() *operators.Signature {
 		Param("model_version", value.Types.String, true, false, value.Nil)
 }
 
-func (iop inferenceOperator) Apply(staticKwargs value.Dict, in operators.InputIter, out *value.List) error {
+func (pop predictOperator) Apply(staticKwargs value.Dict, in operators.InputIter, out *value.List) error {
 	var featureVecs []value.List
 	for in.HasMore() {
 		_, contextKwargs, err := in.Next()
@@ -53,7 +53,7 @@ func (iop inferenceOperator) Apply(staticKwargs value.Dict, in operators.InputIt
 	modelName := staticKwargs.GetUnsafe("model_name").(value.String)
 	modelVersion := staticKwargs.GetUnsafe("model_version").(value.String)
 	// TODO: Split into correctly sized requests instead of just 1.
-	res, err := iop.tier.SagemakerClient.Score(context.TODO(), &sagemaker.ScoreRequest{
+	res, err := pop.tier.SagemakerClient.Score(context.TODO(), &sagemaker.ScoreRequest{
 		ModelName:    string(modelName),
 		ModelVersion: string(modelVersion),
 		FeatureLists: featureVecs,
