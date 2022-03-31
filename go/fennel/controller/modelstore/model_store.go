50e324230edb390d9ccf4ec050e26fec723e9e62
Abhay Bothra <bothra90@users.noreply.github.com>
2022-03-31 00:11:05 -0700

    minor: add tier id to sagemaker model name

--- go/fennel/controller/modelstore/model_store.go
+++ go/fennel/controller/modelstore/model_store.go
@@ -25,7 +25,7 @@ func EnsureEndpointExists(ctx context.Context, tier tier.Tier, endpointName stri
 	}
 	var sagemakerModelName string
 	if len(coveringModels) == 0 {
-		sagemakerModelName = fmt.Sprintf("model-%s", time.Now().Format("20060102150405"))
+		sagemakerModelName = fmt.Sprintf("t-%d-model-%s", tier.ID, time.Now().Format("20060102150405"))
 		hostedModels := make([]lib.SagemakerHostedModel, len(activeModels))
 		for i, model := range activeModels {
 			hostedModels[i] = lib.SagemakerHostedModel{
