1704eafe52faee4881cc1d4f3572326a465dabac
Mohit Reddy <mohitreddy1996@gmail.com>
2022-04-05 13:35:02 -0700

    Add script to read json files of the previous day and write as parquet in the day directory

---
+++ tools/aws_glue_parquet_transform.py
@@ -0,0 +1,56 @@
+""" Script to crawl all the json files written by kafka s3 connector in a day and writing them as a single parquet file.
+
+The script is meant to be run on AWS GLUE.
+
+The script makes the following assumptions:
+    1. assumes that the source is structured using UTC timezone (which our kafka is currently configured as)
+    2. AND the script is run in the next day i.e. say the data has to be transformed for 04/05/2022, then the script is assumed to be run on 04/06/2022 
+    3. assumes that `PLANE_ID` and `TIER_ID` are passed as program arguments
+    4. writes parquet files in the `day` directory
+
+NOTE: This is potentially a temporary solution till the official support for writing messages using schema in confluent go client.
+see: https://coda.io/d/_dby7ZnsqEiy/S3-Connector-v-s-Self-hosted-service_su9yb
+"""
+from datetime import datetime, timedelta
+
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'PLANE_ID', 'TIER_ID'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+job.commit()
+
+# since we are interested in the files generated in the previous day,
+# subtract few hours from the current time and fetch the yyyy,mm,dd
+utc_past = datetime.utcnow() - timedelta(hours=23, minutes=59)
+
+year = utc_past.strftime("%Y")
+month = utc_past.strftime("%m")
+day = utc_past.strftime("%d")
+
+print(f'======== Reading data from date: year={year}/month={month}/day={day}\n')
+
+# the format of the s3 objects: `s3://p-{PLANE_ID}-training-data/daily/t_{TIER_ID}_featurelog/year=2022/month=04/day=05/hour=18/xyz.json`
+# read JSON files using wildcard to fetch all the files
+feature_logs_path = f's3://p-{args["PLANE_ID"]}-training-data/daily/t_{args["TIER_ID"]}_featurelog/year={year}/month={month}/day={day}/*/*.json'
+action_logs_path = f's3://p-{args["PLANE_ID"]}-training-data/daily/t_{args["TIER_ID"]}_actionlog/year={year}/month={month}/day={day}/*/*.json'
+
+features_df = spark.read.json(feature_logs_path)
+actions_df = spark.read.json(action_logs_path)
+
+feature_parquet_path = f's3://p-{args["PLANE_ID"]}-training-data/daily/t_{args["TIER_ID"]}_featurelog/year={year}/month={month}/day={day}/features.parquet'
+actions_parquet_path = f's3://p-{args["PLANE_ID"]}-training-data/daily/t_{args["TIER_ID"]}_actionlog/year={year}/month={month}/day={day}/actions.parquet'
+
+# write to parquet in the same day directory
+features_df.write.parquet()
+actions_df.write.parquet()
