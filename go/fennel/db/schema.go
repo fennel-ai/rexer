a9fa4b43f213f272496782b64060b11072ccaa90
Nikhil Garg <nikhilgarg28@gmail.com>
2022-04-05 14:07:13 -0700

    db/schema: use if block of stored procedures to run schema updates
    Earlier, 'alter table' was being run as part of a transaction. But DDL
    statements (including alter table) don't play nice with transactions.
    To fix that, we now run all statements conditionally (only when the
    version is correct) - this way, each statement is run exactly once and
    so we don't see any issues.

--- go/fennel/db/schema.go
+++ go/fennel/db/schema.go
@@ -1,8 +1,8 @@
 package db
 
 import (
-	"context"
 	"database/sql"
+	"fennel/lib/utils"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
@@ -10,35 +10,26 @@ import (
 
 type Schema map[uint32]string
 
+// initSchema initializes the schema for the database.
+// it does so by creating the schema table if it does not exist and populating it with the
+// default version of 0. This works even when multiple processes are trying to create the
+// schema table at the same time
 func initSchemaVersion(db *sqlx.DB) error {
 	_, err := db.Exec(`
 			create table if not exists schema_version (
-			version INT NOT NULL
-	)`)
+				 version INT NOT NULL
+			)`)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(`
+		  INSERT INTO schema_version (version) 
+		  SELECT 0 WHERE NOT EXISTS (SELECT * FROM schema_version);
+	  `)
 	return err
 }
 
-// schemaVersionTx returns version of the last schema updated on the database using given transaction `tx`
-//
-// returns error if the `schema_version` table does not exist
-func schemaVersionTx(tx *sql.Tx) (uint32, error) {
-	var total sql.NullInt32
-	if err := tx.QueryRow("SELECT version FROM schema_version").Scan(&total); err != nil {
-		// if there were no rows, do not fail
-		if err != sql.ErrNoRows {
-			return 0, err
-		}
-	}
-	if total.Valid {
-		return uint32(total.Int32), nil
-	} else {
-		// this likely means that there was no such row and hence the table has not been initialized
-		return 0, nil
-	}
-}
-
 // schemaVersion returns version of the last schema updated on the database
-//
 // returns error if the `schema_version` table does not exist
 func schemaVersion(db *sqlx.DB) (uint32, error) {
 	var total sql.NullInt32
@@ -56,73 +47,44 @@ func schemaVersion(db *sqlx.DB) (uint32, error) {
 	}
 }
 
-func incrementVersion(tx *sql.Tx, v uint32) error {
-	if v == 1 {
-		if _, err := tx.Exec("INSERT INTO schema_version VALUES (?);", 1); err != nil {
-			return err
-		}
-	} else {
-		if _, err := tx.Exec("UPDATE schema_version SET version = ?", v); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func execSchema(db *sqlx.DB, defv uint32, def string) error {
-	tx, err := db.BeginTx(context.Background(), &sql.TxOptions{Isolation: sql.LevelSerializable, ReadOnly: false})
-	if err != nil {
-		return err
-	}
-	// if tx commits successfully, rollback is a no-op. In any other failure scenario, rollback the changes
-	// in the txn
-	defer tx.Rollback()
-
-	v, err := schemaVersionTx(tx)
-	if err != nil {
-		return err
-	}
-	// if the schema version right now is synced to the version we are processing, execute the schema at our current version
-	// and increment the version
-	if v < defv {
-		// this is a sanity check to ensure that we have not accidentally skipped over any schema version
-		//
-		// since we are executing the schemas and updating the version atomically, this should never happen
-		if defv-v > 1 {
-			return fmt.Errorf("found latest schema version: %+v while trying to execute schema of version: %+v. Was one of the schemas skipped?", v, defv)
-		}
-		if _, err := tx.Exec(def); err != nil {
-			return err
-		}
-		if err = incrementVersion(tx, defv); err != nil {
-			return err
-		}
-	}
-	if err = tx.Commit(); err != nil {
-		return err
-	}
-	return nil
-}
-
 func syncSchema(db *sqlx.DB, defs Schema) error {
 	// initializing `schema_version` table is an idempotent operation
 	if err := initSchemaVersion(db); err != nil {
 		return err
 	}
-
-	// NOTE: since we are fetching the schema version outside of a txn, the value could change b/w this call
-	// and subsequent call below
-	//
-	// we could essentially start the schema version from '0' explicitly, but we fetch the schema version at the current
-	// time to avoid DB roundtrips (helps improve tier initialization time)
-	curr, err := schemaVersion(db)
+	start, err := schemaVersion(db)
 	if err != nil {
 		return err
 	}
 	len_ := uint32(len(defs))
-	for i := curr + 1; i <= len_; i++ {
-		if err := execSchema(db, i, defs[i]); err != nil {
+	// nothing to update
+	if start >= len_ {
+		return nil
+	}
+
+	name := fmt.Sprintf("update_schema_%s", utils.RandString(8))
+	defer db.Exec(fmt.Sprintf("DROP PROCEDURE IF EXISTS %s", name))
+
+	// starting from version next to 'start' so that we don't have to recreate the existing schema
+	for version := start + 1; version <= len_; version++ {
+		sql := defs[version]
+		if _, err := db.Exec(fmt.Sprintf("DROP PROCEDURE IF EXISTS %s", name)); err != nil {
 			return err
+		}
+		procedure := fmt.Sprintf(`
+		 CREATE PROCEDURE %s ()
+		 BEGIN
+			IF (SELECT version FROM schema_version) = %d - 1 
+			THEN
+				 UPDATE schema_version SET version = version + 1;
+				 %s
+			END IF;
+		 END;`, name, version, sql)
+		if _, err = db.Exec(procedure); err != nil {
+			return fmt.Errorf("could not define procedure: %v", err)
+		}
+		if _, err = db.Exec(fmt.Sprintf("CALL %s()", name)); err != nil {
+			return fmt.Errorf("could not execute procedure: %v", err)
 		}
 	}
 	return nil
