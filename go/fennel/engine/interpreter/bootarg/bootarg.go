e35dca50111a30aaf164dcad83837eeefc31a89c
Nikhil <70129397+1nikhil9@users.noreply.github.com>
2022-04-07 18:01:08 -0700

    go: cache profiles at query level (#582)

--- go/fennel/engine/interpreter/bootarg/bootarg.go
+++ go/fennel/engine/interpreter/bootarg/bootarg.go
@@ -3,12 +3,14 @@ package bootarg
 import (
 	"fmt"
 
+	"fennel/lib/profile"
 	"fennel/tier"
 )
 
 func Create(tier tier.Tier) map[string]interface{} {
 	return map[string]interface{}{
-		"__tier__": tier,
+		"__tier__":            tier,
+		"__cached_profiles__": &[]profile.ProfileItem{},
 	}
 }
 
@@ -20,6 +22,18 @@ func GetTier(bootargs map[string]interface{}) (tier.Tier, error) {
 	ret, ok := v.(tier.Tier)
 	if !ok {
 		return tier.Tier{}, fmt.Errorf("__tier__ magic property had: '%v', not a tier", v)
+	}
+	return ret, nil
+}
+
+func GetCachedProfiles(bootargs map[string]interface{}) (*[]profile.ProfileItem, error) {
+	v, ok := bootargs["__cached_profiles__"]
+	if !ok {
+		return nil, fmt.Errorf("cached profiles not found in bootargs")
+	}
+	ret, ok := v.(*[]profile.ProfileItem)
+	if !ok {
+		return nil, fmt.Errorf("expected __cached_profiles__ to be of type '*[]profile.ProfileItem' but found: %v", ret)
 	}
 	return ret, nil
 }
--- go/fennel/opdefs/std/profile/operator_test.go
+++ go/fennel/opdefs/std/profile/operator_test.go
@@ -53,9 +53,9 @@ func TestProfileOp(t *testing.T) {
 	defer test.Teardown(tier)
 	ctx := context.Background()
 
-	// mini-redis does not work well cache keys being in different slots
-	// we run a trimmed version of the test as unit test and add cases where the keys are stored in different
-	// slots in `_integration_test`
+	// mini-redis does not work well with cache keys being in different slots
+	// we run a trimmed version of the test with different versions of the same profile as unit test
+	// and add cases where the keys are stored in different slots in `_integration_test`
 	otype1, oid1, key1, val1, ver1 := ftypes.OType("user"), uint64(223), "age", value.Int(7), uint64(4)
 	req1a := profilelib.ProfileItem{OType: otype1, Oid: oid1, Key: key1, Version: ver1 - 1, Value: value.Int(1121)}
 	assert.NoError(t, profile.Set(ctx, tier, req1a))
@@ -78,12 +78,70 @@ func TestProfileOp(t *testing.T) {
 	}
 	i := interpreter.NewInterpreter(bootarg.Create(tier))
 	table := value.NewList()
-	table.Append(value.NewDict(map[string]value.Value{"otype": value.String(otype1), "oid": value.Int(oid1), "key": value.String(key1), "ver": value.Int(ver1)}))
+	table.Append(value.NewDict(map[string]value.Value{
+		"otype": value.String(otype1),
+		"oid":   value.Int(oid1),
+		"key":   value.String(key1),
+	}))
+	expected := value.NewDict(map[string]value.Value{
+		"otype":         value.String(otype1),
+		"oid":           value.Int(oid1),
+		"key":           value.String(key1),
+		"profile_value": val1,
+	})
+	verify(t, &i, query, table, expected)
+}
+
+func TestProfileOpCache(t *testing.T) {
+	tier, err := test.Tier()
+	assert.NoError(t, err)
+	defer test.Teardown(tier)
+	ctx := context.Background()
+
+	otype, oid, key, val, ver := ftypes.OType("user"), uint64(223), "age", value.Int(7), uint64(4)
+	query := ast.OpCall{
+		Operands:  []ast.Ast{ast.Var{Name: "actions"}},
+		Vars:      []string{"a"},
+		Namespace: "std",
+		Name:      "profile",
+		Kwargs: ast.Dict{Values: map[string]ast.Ast{
+			"otype":   ast.Lookup{On: ast.Var{Name: "a"}, Property: "otype"},
+			"oid":     ast.Lookup{On: ast.Var{Name: "a"}, Property: "oid"},
+			"key":     ast.Lookup{On: ast.Var{Name: "a"}, Property: "key"},
+			"version": ast.Lookup{On: ast.Var{Name: "a"}, Property: "version"},
+			"field":   ast.MakeString("profile_value"),
+		}},
+	}
+
+	i := interpreter.NewInterpreter(bootarg.Create(tier))
+	table := value.NewList()
+	table.Append(value.NewDict(map[string]value.Value{
+		"otype":   value.String(otype),
+		"oid":     value.Int(oid),
+		"key":     value.String(key),
+		"version": value.Int(ver),
+	}))
+	expected := value.NewDict(map[string]value.Value{
+		"otype":         value.String(otype),
+		"oid":           value.Int(oid),
+		"key":           value.String(key),
+		"version":       value.Int(ver),
+		"profile_value": value.Nil,
+	})
+	verify(t, &i, query, table, expected)
+
+	// test cache by setting a profile now
+	req1 := profilelib.ProfileItem{OType: otype, Oid: oid, Key: key, Version: ver, Value: val}
+	assert.NoError(t, profile.Set(ctx, tier, req1))
+	// we should still get back default value if it is cached properly
+	verify(t, &i, query, table, expected)
+}
+
+func verify(t *testing.T, i *interpreter.Interpreter, query ast.Ast, table value.List, expected value.Dict) {
 	out, err := i.Eval(query, value.NewDict(map[string]value.Value{"actions": table}))
 	assert.NoError(t, err)
 	rows := out.(value.List)
-	//assert.Len(t, rows, 1)
 	assert.Equal(t, 1, rows.Len())
-	r, _ := rows.At(0)
-	assert.Equal(t, value.NewDict(map[string]value.Value{"otype": value.String(otype1), "oid": value.Int(oid1), "key": value.String(key1), "ver": value.Int(ver1), "profile_value": val1}), r)
+	found, _ := rows.At(0)
+	assert.Equal(t, expected, found)
 }
