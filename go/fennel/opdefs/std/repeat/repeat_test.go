00a1971116023de81032c78f183c9293161db794
Nikhil Garg <nikhilgarg28@gmail.com>
2022-04-09 01:39:57 -0700

    operators: add repeat operator
    Sometimes user features are agnostic of candidates. In such cases, currently
    we have no choice but to compute the same feature again and again for each
    candidate. Adding repeat operator so that users have the ability to compute
    it once and repeat it many times cheaply

---
+++ go/fennel/opdefs/std/repeat/repeat_test.go
@@ -0,0 +1,52 @@
+package repeat
+
+import (
+	"fennel/lib/value"
+	"fennel/test/optest"
+	"fennel/tier"
+	"testing"
+)
+
+func TestRepeater_Apply(t *testing.T) {
+	scenarios := []struct {
+		input    []value.Value
+		context  []value.Dict
+		err      bool
+		expected []value.Value
+	}{
+		{
+			[]value.Value{value.Int(1), value.Int(2)},
+			[]value.Dict{value.NewDict(map[string]value.Value{"count": value.Int(3)}), value.NewDict(map[string]value.Value{"count": value.Int(2)})},
+			false,
+			[]value.Value{value.Int(1), value.Int(1), value.Int(1), value.Int(2), value.Int(2)},
+		},
+		{
+			[]value.Value{value.Int(1), value.Int(2)},
+			[]value.Dict{value.NewDict(map[string]value.Value{"count": value.Int(0)}), value.NewDict(map[string]value.Value{"count": value.Int(2)})},
+			false,
+			[]value.Value{value.Int(2), value.Int(2)},
+		},
+		{
+			[]value.Value{value.Int(1), value.Int(2)},
+			[]value.Dict{value.NewDict(map[string]value.Value{"count": value.Int(2)}), value.NewDict(map[string]value.Value{"count": value.Int(-1)})},
+			true, nil,
+		},
+		{
+			[]value.Value{value.Int(1), value.Int(2)},
+			[]value.Dict{value.NewDict(map[string]value.Value{"count": value.Double(2)}), value.NewDict(map[string]value.Value{"count": value.Int(1)})},
+			true, nil,
+		},
+		{
+			[]value.Value{value.Int(1), value.Int(2)},
+			[]value.Dict{value.NewDict(map[string]value.Value{"not_count": value.Double(2)}), value.NewDict(map[string]value.Value{"count": value.Int(1)})},
+			true, nil,
+		},
+	}
+	for _, s := range scenarios {
+		if s.err {
+			optest.AssertError(t, tier.Tier{}, repeater{}, value.NewDict(nil), [][]value.Value{s.input}, s.context)
+		} else {
+			optest.AssertEqual(t, tier.Tier{}, repeater{}, value.NewDict(nil), [][]value.Value{s.input}, s.context, s.expected)
+		}
+	}
+}
