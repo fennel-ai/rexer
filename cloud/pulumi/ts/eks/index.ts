b2bd9e4b968124e61a452e34aa8a6dd0a723de3c
Mohit Reddy <mohitreddy1996@gmail.com>
2022-04-10 11:29:50 -0700

    clarify few comments as per https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/upstream/circuit_breaking.html

--- cloud/pulumi/ts/eks/index.ts
+++ cloud/pulumi/ts/eks/index.ts
@@ -152,15 +152,18 @@ async function setupEmissaryIngressCrds(input: inputType, awsProvider: aws.Provi
             "spec": {
                 "config": {
                     "add_linkerd_headers": true,
-                    // https://www.getambassador.io/docs/edge-stack/latest/topics/using/circuit-breakers/
+                    // https://www.getambassador.io/docs/edge-stack/latest/topics/using/circuit-breakers/ and
                     //
-                    // NOTE: circuit breaker configured below is a GLOBAL circuit breaker.
+                    // https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/upstream/circuit_breaking.html
+                    //
+                    // NOTE: circuit breaker configured below is a GLOBAL circuit breaker. If there are more than one endpoint
+                    // configured in the future, consider making these limits at a `MAPPING` level.
                     "circuit_breakers": {
-                        // Specifies the maximum number of connections that Ambassador Edge Stack will make to the services.
+                        // Specifies the maximum number of connections that Ambassador Edge Stack will make to ALL hosts in the upstream cluster.
                         "max_connections": 3072,
                         // Specifies the maximum number of requests that will be queued while waiting for a connection.
                         "max_pending_requests": 1024,
-                        // Specifies the maximum number of parallel outstanding requests to an upstream service.
+                        // Specifies the maximum number of parallel outstanding requests to ALL hosts in a cluster at any given time.
                         "max_requests": 3072,
                         // default - "max_retries": 3,
                     }
