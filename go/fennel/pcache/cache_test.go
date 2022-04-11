0ebe9c37ddf6049433daf027dbeafa70f94b06df
Nikhil <70129397+1nikhil9@users.noreply.github.com>
2022-04-10 19:36:49 -0700

    go: add ttl test for aggregate value (#607)

--- go/fennel/pcache/cache_test.go
+++ go/fennel/pcache/cache_test.go
@@ -12,32 +12,40 @@ func TestPCache_Get(t *testing.T) {
 	cache, err := NewPCache(1<<10, 1<<4)
 	assert.NoError(t, err)
 
+	key1 := "some key"
+	key2 := "other key"
+
 	// initially, should get nothing as this key was not set
-	_, ok := cache.Get("some key")
+	_, ok := cache.Get(key1)
 	assert.False(t, ok)
 
 	// set it now
-	ok = cache.Set("some key", 27)
+	ok = cache.Set(key1, 27)
 	assert.True(t, ok)
 	time.Sleep(10 * time.Millisecond)
 
 	// should get it
-	v, ok := cache.Get("some key")
+	v, ok := cache.Get(key1)
 	assert.True(t, ok)
 	assert.Equal(t, 27, v)
 
 	// set a key with TTL
-	ok = cache.SetWithTTL("other key", 64, 5*time.Second)
+	ok = cache.SetWithTTL(key2, 64, 5*time.Second)
 	assert.True(t, ok)
 	time.Sleep(10 * time.Millisecond)
 
 	// should get it
-	v, ok = cache.Get("other key")
+	v, ok = cache.Get(key2)
 	assert.True(t, ok)
 	assert.Equal(t, 64, v)
 
+	// can get its ttl, should be less or equal to initial ttl
+	ttl, ok := cache.GetTTL(key2)
+	assert.True(t, ok)
+	assert.LessOrEqual(t, ttl, 5*time.Second)
+
 	// should expire after 5 seconds
 	time.Sleep(5 * time.Second)
-	v, ok = cache.Get("other key")
+	v, ok = cache.Get(key2)
 	assert.False(t, ok)
 }
