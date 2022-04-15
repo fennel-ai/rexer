ecf2d8e89c0ffb90de2c630379c9e6392cda93a4
Mohit Reddy <mohitreddy1996@users.noreply.github.com>
2022-04-15 16:09:27 -0700

    go: Fix panic error on context cancel errors from MGet (#643)

--- go/fennel/model/profile/cached.go
+++ go/fennel/model/profile/cached.go
@@ -213,6 +213,11 @@ func (c cachedProvider) getBatched(ctx context.Context, tier tier.Tier, reqs []p
 	txnLogic := func(tx cache.Txn, ks []string) error {
 		vals, err := tx.MGet(ctx, ks...)
 		if err != nil {
+			// It is possible that upon getting the error (e.g. context cancelled), vals == nil or the length
+			// does not match `ks`
+			if vals == nil || len(vals) != len(ks) {
+				vals = make([]interface{}, len(ks))
+			}
 			// if we got an error from cache, no need to panic - we just pretend nothing was found in cache
 			for i := range ks {
 				vals[i] = tier.Cache.Nil()
