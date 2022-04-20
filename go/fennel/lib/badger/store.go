2e7b61a281102f926f15429730b51740a275d405
Abhay Bothra <bothra90@users.noreply.github.com>
2022-04-19 17:28:46 -0700

    kv: add tablet id to kvstore set/get

--- go/fennel/lib/badger/store.go
+++ go/fennel/lib/badger/store.go
@@ -2,38 +2,44 @@ package badger
 
 import (
 	"context"
+	b64 "encoding/base64"
 
 	"fennel/lib/kvstore"
 	"fennel/tier"
 
 	"github.com/dgraph-io/badger/v3"
+	"go.uber.org/zap"
 )
 
 // BadgerTransactionalStore is a transactional KV store backed by BadgerDB.
 // Here, "transactional" means that all operations are performed within a
 // single BadgerDB transaction, and have "read-your-writes" semantics.
 type BadgerTransactionalStore struct {
-	tier   tier.Tier
-	tablet uint8
-	txn    *badger.Txn
+	tier tier.Tier
+	txn  *badger.Txn
 }
 
 var _ kvstore.Reader = (*BadgerTransactionalStore)(nil)
 var _ kvstore.Writer = (*BadgerTransactionalStore)(nil)
 
-func NewTransactionalStore(tier tier.Tier, tablet uint8, txn *badger.Txn) *BadgerTransactionalStore {
+func NewTransactionalStore(tier tier.Tier, txn *badger.Txn) *BadgerTransactionalStore {
 	return &BadgerTransactionalStore{
-		tier:   tier,
-		tablet: tablet,
-		txn:    txn,
+		tier: tier,
+		txn:  txn,
 	}
 }
 
-func (bs *BadgerTransactionalStore) Get(ctx context.Context, key []byte) (*kvstore.SerializedValue, error) {
+func (bs *BadgerTransactionalStore) Get(ctx context.Context, tablet kvstore.TabletType, key []byte) (*kvstore.SerializedValue, error) {
 	if len(key) == 0 {
 		return nil, kvstore.ErrEmptyKey
 	}
-	key = makeKey(bs.tablet, key)
+	bs.tier.Logger.Debug("BadgerTransactionalStore.Get",
+		zap.String("key", b64.StdEncoding.EncodeToString(key)),
+	)
+	key, err := makeKey(tablet, key)
+	if err != nil {
+		return nil, err
+	}
 	item, err := bs.txn.Get(key)
 	switch err {
 	case badger.ErrKeyNotFound:
@@ -54,19 +60,32 @@ func (bs *BadgerTransactionalStore) Get(ctx context.Context, key []byte) (*kvsto
 	}
 }
 
-func (bs *BadgerTransactionalStore) Set(ctx context.Context, key []byte, value kvstore.SerializedValue) error {
+func (bs *BadgerTransactionalStore) Set(ctx context.Context, tablet kvstore.TabletType, key []byte, value kvstore.SerializedValue) error {
 	if len(key) == 0 {
 		return kvstore.ErrEmptyKey
 	}
-	key = makeKey(bs.tablet, key)
+	bs.tier.Logger.Debug("BadgerTransactionalStore.Set",
+		zap.String("key", b64.StdEncoding.EncodeToString(key)),
+		zap.String("value", value.String()),
+	)
+	key, err := makeKey(tablet, key)
+	if err != nil {
+		return err
+	}
 	entry := badger.NewEntry(key, value.Raw).WithMeta(value.Codec)
 	return bs.txn.SetEntry(entry)
 }
 
-func makeKey(tablet uint8, key []byte) []byte {
-	// TODO(abhay): Can we avoid an extra allocation and copy here?
+func makeKey(tablet kvstore.TabletType, key []byte) ([]byte, error) {
+	// TODO(abhay): Can we avoid an extra allocation and/or copy here?
 	k := make([]byte, len(key)+1)
-	k[0] = tablet
+	n, err := tablet.Write(k)
+	if err != nil {
+		return nil, err
+	}
+	if n != 1 {
+		panic("size of tablet type is not 1 byte")
+	}
 	copy(k[1:], key)
-	return k
+	return k, nil
 }
