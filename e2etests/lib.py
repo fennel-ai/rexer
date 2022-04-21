9ef83aedd4598c390d6fb7a781c002d574814f52
Aditya Nambiar <aditya.nambiar007@gmail.com>
2022-04-21 14:04:07 -0700

    Script to run server locally (#679)
    * Done
    
    * nits
    
    * remove logs
    
    Co-authored-by: Aditya Nambiar <adityanambiar@Adityas-MacBook-Pro.local>

--- e2etests/lib.py
+++ e2etests/lib.py
@@ -7,10 +7,47 @@ import string
 import subprocess
 import sys
 import time
+import dataclasses
+from typing import Any, List
 
 
 ROOT = os.getenv("FENNEL_ROOT")
 GODIR = os.path.join(ROOT, 'go/fennel')
+
+class Process():
+    process: Any
+    flags: Any
+    path: str
+    binary: Any
+
+    def __init__(self, process, path, flags, binary):
+        self.path = path
+        self.flags = flags
+        self.binary = binary
+        self.process = process
+
+
+    def kill(self):
+        print('going to kill:', self.path, ' '.join(self.flags))
+        self.process.kill()
+        self.process.wait()
+        os.remove(self.binary)
+        print('done killing:', self.path, ' '.join(self.flags))
+
+
+def run(path, tags, env, flags=None, wait=False, sleep=0, cwd: str = GODIR) -> Process:
+    if flags is None:
+        flags = []
+
+    dir = randname(8)
+    binary = '/tmp/%s/%s' % (dir, path)
+    print('going to build:', path)
+    b = subprocess.Popen(['go', 'build', '--tags', tags, '-o', binary, path], cwd=cwd)
+    b.wait()
+    print('build: ', 'success' if b.returncode == 0 else 'fail')
+    print('going to run:', path, ' '.join(flags))
+    p = subprocess.Popen([binary] + flags, env=env, cwd=cwd) 
+    return Process(p, path, flags, binary)
 
 
 @contextlib.contextmanager
@@ -31,6 +68,7 @@ def gorun(path, tags, env, flags=None, wait=False, sleep=0, cwd: str = GODIR):
         p.wait()
     if sleep:
         time.sleep(sleep)
+
     try:
         yield
     finally:
---
+++ e2etests/run_local.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import dataclasses
+import functools
+import os
+import random
+import string
+import unittest
+from typing import Any
+import time
+from datetime import datetime, timezone, timedelta
+import lib
+import signal
+import sys
+from threading import Event
+
+# Url of the local server
+URL = 'http://localhost:2425'
+class LocalTier():
+    def __init__(self, flags=None):
+        self.http_process = lib.Process(None, None, None, None)
+        self.countaggr_process = lib.Process(None, None, None, None)
+        self.env = os.environ.copy()
+        signal.signal(signal.SIGINT, self.kill_process)
+
+    def run_local_server(self):
+        tier_id = random.randint(0, 1e8)
+        self.env['TIER_ID'] = str(tier_id)
+        with lib.gorun('fennel/test/cmds/tiergod', 'dynamic,integration', self.env, flags=['--mode', 'create'], wait=True):
+            pass
+        print("Tier id", tier_id)
+        self.env['METRICS_PORT'] = str(2436)
+        self.env['PPROF_PORT'] = str(2437)
+        self.env['BADGER_DIR'] = '/tmp/badger/' + lib.randname(5)
+        self.http_process = lib.run('fennel/service/http', 'dynamic,integration', self.env)
+        self.env['METRICS_PORT'] = str(2446)
+        self.env['PPROF_PORT'] = str(2467)
+        self.env['BADGER_DIR'] = '/tmp/badger/' + lib.randname(5)
+        self.countaggr_process = lib.run('fennel/service/countaggr', 'dynamic,integration', self.env)  
+        # Wait for the services to be up.
+        time.sleep(10)
+        print("Server is up")
+        Event().wait()
+
+    def kill_process(self, signum, frame):
+        print('You pressed Ctrl+C!')
+        self.countaggr_process.kill()
+        self.http_process.kill()
+        with lib.gorun('fennel/test/cmds/tiergod', 'dynamic,integration', self.env, flags=['--mode', 'destroy'],
+                       wait=True):
+            pass
+
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    local_tier = LocalTier()
+    local_tier.run_local_server()
