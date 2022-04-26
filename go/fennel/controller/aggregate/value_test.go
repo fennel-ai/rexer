10acc47de223dc7b3700011672933bd4da0a48c4
Nikhil <70129397+1nikhil9@users.noreply.github.com>
2022-04-25 23:24:49 -0700

    change ids to serialized value (#700)
    * change ids to serialized value
    
    * e2e: update e2e test

--- go/fennel/controller/aggregate/value_test.go
+++ go/fennel/controller/aggregate/value_test.go
@@ -171,8 +171,8 @@ func TestOfflineAggregates(t *testing.T) {
 	}()
 
 	expectedJsonTable := []string{
-		`{"action_id":2,"action_type":"like","actor_id":"3434","actor_type":"user","aggregate":"agg","groupkey":["3434"],"metadata":6,"request_id":"7","target_id":"3","target_type":"video","timestamp":1000}`,
-		`{"action_id":2,"action_type":"like","actor_id":"325235","actor_type":"user","aggregate":"agg","groupkey":["325235"],"metadata":6,"request_id":"7","target_id":"3","target_type":"video","timestamp":1000}`,
+		`{"action_id":2,"action_type":"like","actor_id":3434,"actor_type":"user","aggregate":"agg","groupkey":[3434],"metadata":6,"request_id":7,"target_id":3,"target_type":"video","timestamp":1000}`,
+		`{"action_id":2,"action_type":"like","actor_id":325235,"actor_type":"user","aggregate":"agg","groupkey":[325235],"metadata":6,"request_id":7,"target_id":3,"target_type":"video","timestamp":1000}`,
 	}
 	// test that actions were written as JSON as well
 	go func() {
@@ -321,7 +321,7 @@ func TestTransformActions(t *testing.T) {
 		row, ok := r.(value.Dict)
 		assert.True(t, ok)
 		assert.Equal(t, value.Int(i+1000), get(row, "timestamp"))
-		assert.Equal(t, value.NewList(value.String(uid)), get(row, "groupkey"))
+		assert.Equal(t, value.NewList(value.Int(41)), get(row, "groupkey"))
 	}
 }
 
--- go/fennel/opdefs/feature/operator.go
+++ go/fennel/opdefs/feature/operator.go
@@ -47,12 +47,12 @@ func (f featureLog) Apply(ctx context.Context, static value.Dict, in operators.I
 		}
 		msg := libfeature.Row{
 			ContextOType:    ftypes.OType(get(kwargs, "context_otype").(value.String)),
-			ContextOid:      ftypes.OidType(get(kwargs, "context_oid").(value.String)),
+			ContextOid:      ftypes.OidType(get(kwargs, "context_oid").String()),
 			CandidateOType:  ftypes.OType(get(kwargs, "candidate_otype").(value.String)),
-			CandidateOid:    ftypes.OidType(get(kwargs, "candidate_oid").(value.String)),
+			CandidateOid:    ftypes.OidType(get(kwargs, "candidate_oid").String()),
 			Features:        get(kwargs, "features").(value.Dict),
 			Workflow:        workflow,
-			RequestID:       ftypes.RequestID(get(kwargs, "request_id").(value.String)),
+			RequestID:       ftypes.RequestID(get(kwargs, "request_id").String()),
 			Timestamp:       ts,
 			ModelName:       modelName,
 			ModelVersion:    modelVersion,
@@ -61,7 +61,11 @@ func (f featureLog) Apply(ctx context.Context, static value.Dict, in operators.I
 		if err = feature.Log(ctx, f.tier, msg); err != nil {
 			return err
 		}
-		out.Append(msg.GetValue())
+		row, err := msg.GetValue()
+		if err != nil {
+			return err
+		}
+		out.Append(row)
 	}
 	return nil
 }
@@ -70,12 +74,12 @@ func (f featureLog) Signature() *operators.Signature {
 	return operators.NewSignature("feature", "log").
 		Input(nil).
 		Param("context_otype", value.Types.String, false, false, value.Nil).
-		Param("context_oid", value.Types.String, false, false, value.Nil).
+		Param("context_oid", value.Types.ID, false, false, value.Nil).
 		Param("candidate_otype", value.Types.String, false, false, value.Nil).
-		Param("candidate_oid", value.Types.String, false, false, value.Nil).
+		Param("candidate_oid", value.Types.ID, false, false, value.Nil).
 		Param("features", value.Types.Dict, false, false, value.Nil).
 		Param("workflow", value.Types.String, true, false, value.Nil).
-		Param("request_id", value.Types.String, false, false, value.Nil).
+		Param("request_id", value.Types.ID, false, false, value.Nil).
 		Param("model_name", value.Types.String, true, true, value.String("")).
 		Param("model_version", value.Types.String, true, true, value.String("")).
 		Param("model_prediction", value.Types.Double, false, true, value.Double(-1)).
