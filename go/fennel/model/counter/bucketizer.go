6105faa8d54e13d94d7009f9cff282e5ea8afeb6
Araya <1nikhil9@gmail.com>
2022-05-13 09:25:29 -0700

    go: add fixed split bucketizer

--- go/fennel/model/counter/bucketizer.go
+++ go/fennel/model/counter/bucketizer.go
@@ -1,6 +1,8 @@
 package counter
 
 import (
+	"fmt"
+
 	"fennel/lib/counter"
 	"fennel/lib/ftypes"
 	"fennel/lib/value"
@@ -63,4 +65,80 @@ func (f fixedWidthBucketizer) BucketizeDuration(key string, start, end ftypes.Ti
 	return ret
 }
 
+type fixedSplitBucketizer struct {
+	numBuckets []uint64
+	durations  []uint64
+	widths     []uint64
+}
+
+// numBuckets must always be > 0;
+// set duration = 0 for infinite bucket
+func newFixedSplitBucketizer(numBuckets []uint64, durations []uint64) (f fixedSplitBucketizer, err error) {
+	if len(numBuckets) != len(durations) {
+		return f, fmt.Errorf("error: length of arrays 'numBuckets' and 'durations' not equal")
+	}
+	f = fixedSplitBucketizer{
+		numBuckets: numBuckets,
+		durations:  durations,
+	}
+	f.widths = make([]uint64, len(durations))
+	for i := range numBuckets {
+		if numBuckets[i] == 0 {
+			return f, fmt.Errorf("error: numBuckets[%d] is 0; numBuckets must be a postive integer", i)
+		}
+		f.widths[i] = durations[i] / numBuckets[i]
+	}
+	return f, nil
+}
+
+func (f fixedSplitBucketizer) BucketizeMoment(key string, ts ftypes.Timestamp, v value.Value) []counter.Bucket {
+	buckets := make([]counter.Bucket, len(f.durations))
+	for i := range f.durations {
+		buckets[i] = counter.Bucket{
+			Key:    key,
+			Window: ftypes.Window_FOREVER,
+			Width:  f.widths[i],
+			Index:  f.getIndex(ts, f.widths[i]),
+			Value:  v,
+		}
+	}
+	return buckets
+}
+
+func (f fixedSplitBucketizer) BucketizeDuration(
+	key string, start, finish ftypes.Timestamp, v value.Value,
+) []counter.Bucket {
+	// Find which width, duration of the Bucketizer we are working with
+	d := finish - start
+	var w uint64
+	for i := range f.durations {
+		if f.durations[i] == uint64(d) {
+			w = f.widths[i]
+		}
+	}
+	// Get buckets
+	beg := f.getIndex(start, w)
+	end := f.getIndex(finish-1, w)
+	buckets := make([]counter.Bucket, end-beg+1)
+	for i := beg; i <= end; i++ {
+		buckets[i] = counter.Bucket{
+			Key:    key,
+			Window: ftypes.Window_FOREVER,
+			Width:  w,
+			Index:  i,
+			Value:  v,
+		}
+	}
+	return buckets
+}
+
+func (f fixedSplitBucketizer) getIndex(ts ftypes.Timestamp, w uint64) uint64 {
+	// If w is 0, width is infinite so we return 0
+	if w == 0 {
+		return 0
+	}
+	return uint64(ts) / w
+}
+
 var _ Bucketizer = fixedWidthBucketizer{}
+var _ Bucketizer = fixedSplitBucketizer{}
