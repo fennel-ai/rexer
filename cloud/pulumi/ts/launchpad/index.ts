9395ca23c0e24246d878d7bfa39bb18fc15089ab
Mohit Reddy <mohitreddy1996@gmail.com>
2022-05-30 23:54:37 -0700

    pulumi: create plane 6 and tier 109 for Lokal staging cluster

--- cloud/pulumi/ts/launchpad/index.ts
+++ cloud/pulumi/ts/launchpad/index.ts
@@ -45,7 +45,6 @@ const tierConfs: Record<number, TierConf> = {
         httpServerConf: {
             podConf: {
                 replicas: 1,
-                // each http-server should be in different nodes from each other
                 enforceReplicaIsolation: false,
             }
         },
@@ -56,7 +55,6 @@ const tierConfs: Record<number, TierConf> = {
         httpServerConf: {
             podConf: {
                 replicas: 1,
-                // each http-server should be in different nodes from each other
                 enforceReplicaIsolation: false,
             }
         },
@@ -103,7 +101,21 @@ const tierConfs: Record<number, TierConf> = {
         httpServerConf: {
             podConf: {
                 replicas: 1,
-                // each http-server should be in different nodes from each other
+                enforceReplicaIsolation: false,
+            },
+        },
+    },
+    // Lokal's staging tier
+    109: {
+        planeId: 6,
+        // use public subnets for ingress to allow traffic from outside the assigned vpc
+        ingressConf: {
+            usePublicSubnets: true,
+            loadBalancerScheme: PUBLIC_LB_SCHEME,
+        },
+        httpServerConf: {
+            podConf: {
+                replicas: 1,
                 enforceReplicaIsolation: false,
             },
         },
@@ -242,6 +254,39 @@ const planeConfs: Record<number, PlaneConf> = {
         prometheusConf: {
             useAMP: false
         }
+    },
+    // Lokal's staging data plane
+    6: {
+        planeId: 6,
+        region: "ap-south-1",
+        roleArn: "arn:aws:iam::030813887342:role/admin",
+        vpcConf: {
+            cidr: "10.106.0.0/16"
+        },
+        dbConf: {
+            minCapacity: 1,
+            maxCapacity: 4,
+            password: "password",
+            skipFinalSnapshot: true,
+        },
+        confluentConf: {
+            username: confluentUsername,
+            password: confluentPassword
+        },
+        controlPlaneConf: controlPlane,
+        redisConf: {
+            numShards: 1,
+            nodeType: "db.t4g.medium",
+            numReplicasPerShard: 0,
+        },
+        cacheConf: {
+            nodeType: "cache.t4g.medium",
+            numNodeGroups: 1,
+            replicasPerNodeGroup: 0,
+        },
+        prometheusConf: {
+            useAMP: false
+        },
     },
 }
 
