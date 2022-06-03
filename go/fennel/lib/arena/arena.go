615dc651be1990e487400e1185e488d72a69b4b3
Nikhil Garg <nikhilgarg28@gmail.com>
2022-06-03 01:56:35 -0700

    arena: export stats + start randomly dropping some Frees
    Earlier the arena was fairly static - once a few large slices come in,
    there isn't enough space left for smaller slices. And so we might get
    lots of misses for smaller sizes because of a few large slices.
    
    This commit repairs that to some degree by randomly dropping all free
    calls with some probability. This "eviction" mechanism should help
    bring the distribution of the arena more in sync with changing traffic.
    
    In addition, this commit also exposes counters about the performance of
    these arenas which will be useful in finetuning these over time.

--- go/fennel/lib/arena/arena.go
+++ go/fennel/lib/arena/arena.go
@@ -1,34 +1,63 @@
 package arena
 
 import (
+	"fmt"
 	"sync"
+	"time"
+	"unsafe"
+
+	"github.com/detailyang/fastrand-go"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+const (
+	// Drop one free call in every 16 calls. This needs to be a power of 2
+	dropRate = 16
 )
 
 /*
 	Arena is a memory allocator that can be used to allocate slices of arbitrary types.
-	Unlike the standard library's slice allocator, Arena reserves a configurable amount of
-	memory which isn't garbage collected until the arena is destroyed. This is useful for
-	allocating temporary slices which are only used for a short time and are then created
-	elsewhere -- in that sense, it acts like a buffer pool and saves on allocations + GC
-	overhead.
+	Unlike the standard library's slice allocator, slices freed by Arena aren't available
+	to GC right away - instead, they are (often) kept for reuse by someone else.
+	This is useful for allocating temporary slices which are only used for a short time
+	and are then created elsewhere -- in that sense, it acts like a buffer pool and saves
+	on allocations + GC overhead.
+
+	It has a few other similarities with sync.Pool:
+	- It only allocates memory on-demand, and not when arena is initialized.
+    - Free can also be called on a buffer that was not allocated from the arena.
+	- As memory is freed, arena's memory footprint goes down (unless ofcourse no one else
+	  is putting data back in) and can go down to zero. In other words, it doesn't OWN the
+      memory forever
+	- It randomly drops some free requests instead of putting them back in arena. This keeps
+	  the pool dynamic and also further ensures that this arena doesn't unnecessarily hog memory
 
 	It operates by maintaining many page lists consisting of buffers of a certain size. When
 	allocating a buffer, the arena will first check if there is a free page in the appropriate
 	page list. If there is, it will allocate a buffer from that page. If there isn't, it will
 	allocate a new page.
 
-	Note: Free can also be called on a buffer that was not allocated from the arena.
-
-	Arena is thread-safe and is currently implemented using an arena level lock. This isn't
-	super efficient, but it's simple, and it works. Someday we may want to implement either
-	a lock free version or at least have finer grained locking.
-
+	Arena is thread-safe and is currently implemented using arena level locks. It is simple and
+	reasonably fast for what we need. But someday, we may want to implement a lock free version
+	or at least have fine-grained locks.
 
 */
+var stats = promauto.NewGaugeVec(prometheus.GaugeOpts{
+	Name: "arena_stats",
+	Help: "Stats about performance of arena",
+}, []string{"metric", "name"})
+
 type Arena[T any] struct {
 	maxalloc  int
 	capacity  int
 	cursz     int
+	name      string
+	hits      int64
+	misses    int64
+	frees     int64
+	drops     int64
+	rejects   int64
 	pagelists []*pageList[T]
 	lock      sync.Mutex
 }
@@ -45,6 +74,8 @@ func New[T any](maxCap, totalCap int) *Arena[T] {
 		pl := pageList[T]{}
 		ret.pagelists = append([]*pageList[T](ret.pagelists), &pl)
 	}
+	// start a goroutine to periodically report stats about arena
+	go ret.report()
 	return ret
 }
 
@@ -70,6 +101,7 @@ func (a *Arena[T]) Alloc(len_, cap_ int) []T {
 
 	a.lock.Lock()
 	defer a.lock.Unlock()
+	a.hits += 1
 	if l := len([][]T(pl.pages)) - 1; l >= 0 {
 		ret = pl.pages[l]
 		pl.pages = pl.pages[:l]
@@ -78,6 +110,7 @@ func (a *Arena[T]) Alloc(len_, cap_ int) []T {
 
 	if cap(ret) < cap_ {
 		// page miss, or the found page is too small
+		a.misses += 1
 		ret = make([]T, cap_)
 	}
 
@@ -90,22 +123,59 @@ func (a *Arena[T]) Free(b []T) {
 	if cap(b) == 0 || cap(b) >= a.maxalloc {
 		return // ignore out-of-range slices
 	}
-	// cleanup the slice by putting zero for all elements upto its cap
-	var zero T
-	b = b[:cap(b)]
-	b[0] = zero
-	for i := 1; i < len(b); i *= 2 {
-		copy(b[i:], b[:i])
+	drop := false
+	if (fastrand.FastRand() & (dropRate - 1)) == 0 {
+		drop = true // we will drop this but first need to maintain state counters
+	}
+	if !drop {
+		// cleanup the slice by putting zero for all elements upto its cap
+		var zero T
+		b = b[:cap(b)]
+		b[0] = zero
+		for i := 1; i < len(b); i *= 2 {
+			copy(b[i:], b[:i])
+		}
 	}
 	a.lock.Lock()
 	defer a.lock.Unlock()
+	a.frees += 1
 	if a.cursz+cap(b) > a.capacity {
+		a.rejects += 1
+		return
+	}
+	// Randomly drop few requests to keep the pool dynamic. This has two benefits -
+	// 	1. Pool drains over time if traffic shifts to require a lower amount of space
+	// 	2. Few large capacity items can hog the pool capacity and hurt "hit rate" for
+	// 	   others. By constantly recycling some items, the "distribution" of arena
+	// 	   should adjust with traffic needs
+	if drop {
+		a.drops += 1
 		return
 	}
 	idx := idx(uint32(cap(b)))
 	pl := a.pagelists[idx]
 	pl.pages = append([][]T(pl.pages), b)
 	a.cursz += cap(b)
+}
+
+// report publishes stats about this arena every 1 min
+// this doesn't take lock, and so hits/misses etc may not be in sync
+// but that's okay -for any reasonably long-running server, the numbers
+// should be roughly correct
+func (a *Arena[T]) report() {
+	var zero T
+	sz := int(unsafe.Sizeof(zero))
+	name := fmt.Sprintf("%T_%d_%d", zero, a.maxalloc, a.capacity)
+	ticker := time.NewTicker(time.Minute)
+	defer ticker.Stop()
+	for ; true; <-ticker.C {
+		stats.WithLabelValues("hits", name).Set(float64(a.hits))
+		stats.WithLabelValues("misses", name).Set(float64(a.misses))
+		stats.WithLabelValues("frees", name).Set(float64(a.frees))
+		stats.WithLabelValues("drops", name).Set(float64(a.drops))
+		stats.WithLabelValues("rejects", name).Set(float64(a.rejects))
+		stats.WithLabelValues("size_bytes", name).Set(float64(sz * a.cursz))
+	}
 }
 
 // bit like lg2(n/8)
