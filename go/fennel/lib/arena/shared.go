8e0fed5f2a99fd565c5b5eaeb4dabad82084b172
Mohit Reddy <mohitreddy1996@users.noreply.github.com>
2022-06-06 22:30:54 -0700

    go: Double the capacity of Value and Buckets arena. (#918)

--- go/fennel/lib/arena/shared.go
+++ go/fennel/lib/arena/shared.go
@@ -10,12 +10,12 @@ import (
 
 var (
 	Bytes   = New[byte](1<<12, 1<<22)           // memory footprint <= 4MB
-	Values  = New[value.Value](1<<15, 1<<22)    // memory footprint <= 64MB
+	Values  = New[value.Value](1<<15, 1<<23)    // memory footprint <= 128MB
 	Strings = New[string](1<<12, 1<<22)         // memory footprint <= 96MB
 	Ints    = New[int](1<<12, 1<<22)            // memory footprint <= 16MB
 	Longs   = New[int64](1<<12, 1<<22)          // memory footprint <= 32MB
 	Bools   = New[bool](1<<12, 1<<22)           // memory footprint <= 4MB
 	Bytes2D = New[[]byte](1<<12, 1<<22)         // memory footprint <= 96MB
-	Buckets = New[counter.Bucket](1<<15, 1<<24) // memory footprint <= 1GB
+	Buckets = New[counter.Bucket](1<<15, 1<<25) // memory footprint <= 2GB
 	// NOTE: if you add more arena, please update the total size above in docstrings
 )
