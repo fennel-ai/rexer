4596d93eea9f8c14456cc7f99dc6640e76d554d4
Nikhil Garg <nikhilgarg28@gmail.com>
2022-06-08 15:33:40 -0700

    operator: create struct for storing operator kwargs

---
+++ go/fennel/engine/operators/kwarg.go
@@ -0,0 +1,63 @@
+package operators
+
+import (
+	"errors"
+	"fennel/lib/value"
+)
+
+type Kwargs struct {
+	sig    *Signature
+	static bool
+	vals   []value.Value
+}
+
+func NewKwargs(sig *Signature, vals []value.Value, static bool) (Kwargs, error) {
+	if static && len(sig.StaticKwargs) != len(vals) {
+		return Kwargs{}, errors.New("length of static kwargs doesn't match the signature")
+	} else if !static && len(sig.ContextKwargs) != len(vals) {
+		return Kwargs{}, errors.New("length of context kwargs doesn't match the signature")
+	}
+	return Kwargs{
+		sig:    sig,
+		vals:   vals,
+		static: static,
+	}, nil
+}
+
+func (k *Kwargs) Len() int {
+	return len(k.vals)
+}
+
+func (k *Kwargs) Get(key string) (value.Value, bool) {
+	var params []Param
+	if k.static {
+		params = k.sig.StaticKwargs
+	} else {
+		params = k.sig.ContextKwargs
+	}
+	for i, p := range params {
+		if p.Name == key {
+			if i < len(k.vals) {
+				return k.vals[i], true
+			} else {
+				return nil, false
+			}
+		}
+	}
+	return nil, false
+}
+
+func (k *Kwargs) GetUnsafe(key string) value.Value {
+	var params []Param
+	if k.static {
+		params = k.sig.StaticKwargs
+	} else {
+		params = k.sig.ContextKwargs
+	}
+	for i, p := range params {
+		if p.Name == key {
+			return k.vals[i]
+		}
+	}
+	return nil
+}
