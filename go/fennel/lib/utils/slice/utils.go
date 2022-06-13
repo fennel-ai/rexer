b2d73ac68cbc08e2e1721ac1a4c18cdec9b7fd9c
Abhay Bothra <bothra90@users.noreply.github.com>
2022-06-12 19:33:52 -0700

    sliceutils: add function to limit a subslice capacity to its length (#976)

--- go/fennel/lib/utils/slice/utils.go
+++ go/fennel/lib/utils/slice/utils.go
@@ -1,5 +1,6 @@
 package slice
 
+// Fill efficiently fills a slice to its length with the given value.
 func Fill[T any](slice []T, elem T) {
 	l := len(slice)
 	if l == 0 {
@@ -11,6 +12,7 @@ func Fill[T any](slice []T, elem T) {
 	}
 }
 
+// Grow ensures the slice has the capacity to fit an additional n elements.
 func Grow[T any](slice []T, n int) []T {
 	if cap(slice) >= len(slice)+n {
 		return slice
@@ -18,4 +20,14 @@ func Grow[T any](slice []T, n int) []T {
 	newSlice := make([]T, len(slice), len(slice)+n)
 	copy(newSlice, slice)
 	return newSlice
+}
+
+// Limit returns a full subslice of the given slice, but ensures that the
+// capacity of the subslice is the same as its length. This ensures that the
+// subslice is copied on append instead of modifying the underlying array.
+// This is useful in cases where a large slice is broken into many smaller
+// slices and the smaller slices are consecutively laid out in memory.
+func Limit[T any](slice []T) []T {
+	l := len(slice)
+	return slice[:l:l]
 }
