76248f0c2ee60ab2071325a5f3002a61f99d822a
Abhay Bothra <bothra90@users.noreply.github.com>
2022-06-23 23:43:14 -0700

    nitrous: add an interface and v1 impl for logical encoders

---
+++ go/fennel/nitrous/encoders/v1.go
@@ -0,0 +1,37 @@
+package encoders
+
+import (
+	"fmt"
+
+	"fennel/lib/aggregate"
+	"fennel/lib/ftypes"
+	"fennel/model/counter"
+	rpc "fennel/nitrous/rpc/v2"
+	"fennel/nitrous/server/store"
+	"fennel/nitrous/server/temporal"
+	"fennel/plane"
+
+	"github.com/raulk/clock"
+)
+
+// The v1 logical encoding has three salient components:
+// 1. It uses the FixedWidthBucketizer with 100 buckets for bucketizing time.
+// 2. Uses the counter.ToMergeReduce function to determine how intermediate or
+// partial counter values are represented and merged.
+// 3. Uses Closet store to store the aggregate values.
+// If any of these need to be changed, we need to create a different encoding.
+type V1Encoder struct{}
+
+func (e V1Encoder) NewStore(plane plane.Plane, tierId ftypes.RealmID, aggId ftypes.AggId, options aggregate.Options) (TailingStore, error) {
+	const numBuckets = 100
+	bucketizer := temporal.NewFixedWidthBucketizer(options, numBuckets, clock.New())
+	mr, err := counter.ToMergeReduce(aggId, options)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create merge reduce for aggId %d in tier %d: %w", aggId, tierId, err)
+	}
+	ags, err := store.NewCloset(plane, tierId, aggId, rpc.AggCodec_V1, mr, bucketizer)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize aggregate store for new aggregate (%d) in tier (%d): %w", aggId, tierId, err)
+	}
+	return ags, nil
+}
