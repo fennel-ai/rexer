6dbf11828dae79945208461336b6d540deb25912
Abhay Bothra <bothra90@users.noreply.github.com>
2022-06-23 23:42:24 -0700

    nitrous: add Closet as an aggregate value store built on hangar

---
+++ go/fennel/nitrous/server/store/closet.go
@@ -0,0 +1,271 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"unsafe"
+
+	"fennel/hangar"
+	"fennel/lib/ftypes"
+	"fennel/lib/utils/binary"
+	"fennel/lib/utils/slice"
+	"fennel/lib/value"
+	"fennel/model/counter"
+	rpc "fennel/nitrous/rpc/v2"
+	"fennel/nitrous/server"
+	"fennel/nitrous/server/tailer"
+	"fennel/nitrous/server/temporal"
+	"fennel/plane"
+
+	"github.com/samber/mo"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+// Closet stores aggregate data as fields in a hangar key that is created from
+// the codec, groupkey, and the bucket width and index determined by the given
+// bucketizer.
+// This ensures that all aggregate values for a groupkey in the same bucket
+// are colocated in the same hangar key, giving us high block-cache hit ratio,
+// since they are likely to be accessed together.
+type Closet struct {
+	plane plane.Plane
+
+	tierId     ftypes.RealmID
+	aggId      ftypes.AggId
+	codec      rpc.AggCodec
+	field      []byte // field is the hangar field for this aggregate. It is created from (tier id | agg id)
+	mr         counter.MergeReduce
+	bucketizer temporal.TimeBucketizer
+}
+
+var _ server.AggregateStore = Closet{}
+var _ tailer.EventProcessor = Closet{}
+
+type AggStoreOptions struct {
+	plane plane.Plane
+	tlr   *tailer.Tailer
+	s     *server.Server
+
+	tierId     ftypes.RealmID
+	aggId      ftypes.AggId
+	codec      rpc.AggCodec
+	mr         counter.MergeReduce
+	bucketizer temporal.TimeBucketizer
+}
+
+func NewCloset(
+	plane plane.Plane, tierId ftypes.RealmID, aggId ftypes.AggId, codec rpc.AggCodec,
+	mr counter.MergeReduce, bucketizer temporal.TimeBucketizer) (Closet, error) {
+	field, err := encodeField(tierId, aggId)
+	if err != nil {
+		return Closet{}, fmt.Errorf("failed to create field: %w", err)
+	}
+	ags := Closet{
+		plane,
+		tierId,
+		aggId,
+		codec,
+		field,
+		mr,
+		bucketizer,
+	}
+	return ags, nil
+}
+
+func encodeField(tierId ftypes.RealmID, aggId ftypes.AggId) ([]byte, error) {
+	buf := make([]byte, 20)
+	curr := 0
+	n, err := binary.PutUvarint(buf[curr:], uint64(tierId))
+	if err != nil {
+		return nil, fmt.Errorf("error encoding tierId (%d): %w", tierId, err)
+	}
+	curr += n
+	n, err = binary.PutUvarint(buf[curr:], uint64(aggId))
+	if err != nil {
+		return nil, fmt.Errorf("error encoding aggId (%d): %w", aggId, err)
+	}
+	curr += n
+	return buf[:curr], nil
+}
+
+func asString(s []byte) string {
+	return *(*string)(unsafe.Pointer(&s))
+}
+
+func (ms Closet) encodeKeys(groupkey string, buckets []temporal.TimeBucket) ([]hangar.KeyGroup, error) {
+	kgs := make([]hangar.KeyGroup, len(buckets))
+	// Allocate space for storing keys.
+	keylen := 10 + 10 + len(groupkey) + 10 + 10
+	keybuf := make([]byte, keylen*len(buckets))
+	for i, b := range buckets {
+		// Encode (codec | groupkey | width | index) as "prefix".
+		curr := 0
+		n, err := binary.PutVarint(keybuf[curr:], int64(ms.codec))
+		if err != nil {
+			return nil, fmt.Errorf("error encoding codec (%d): %w", ms.codec, err)
+		}
+		curr += n
+		n, err = binary.PutString(keybuf[curr:], groupkey)
+		if err != nil {
+			return nil, fmt.Errorf("error encoding groupkey (%s): %w", groupkey, err)
+		}
+		curr += n
+		n, err = binary.PutUvarint(keybuf[curr:], uint64(b.Width))
+		if err != nil {
+			return nil, fmt.Errorf("error encoding width (%d): %w", b.Width, err)
+		}
+		curr += n
+		n, err = binary.PutUvarint(keybuf[curr:], uint64(b.Index))
+		if err != nil {
+			return nil, fmt.Errorf("error encoding index (%d): %w", b.Index, err)
+		}
+		curr += n
+		kgs[i].Prefix.Data = keybuf[:curr:curr]
+		kgs[i].Fields = mo.Some(hangar.Fields{slice.Limit(ms.field)})
+		keybuf = keybuf[curr:]
+	}
+	return kgs, nil
+}
+
+func (ms Closet) Get(ctx context.Context, duration uint32, keys []string) ([]value.Value, error) {
+	buckets, err := ms.bucketizer.BucketizeDuration(duration)
+	if err != nil {
+		return nil, fmt.Errorf("error bucketizing: %w", err)
+	}
+	kgs := make([]hangar.KeyGroup, 0, len(keys)*len(buckets))
+	for _, key := range keys {
+		encoded, err := ms.encodeKeys(key, buckets)
+		if err != nil {
+			return nil, fmt.Errorf("error encoding: %w", err)
+		}
+		kgs = append(kgs, encoded...)
+	}
+	vgs, err := ms.plane.Store.GetMany(kgs)
+	if err != nil {
+		return nil, fmt.Errorf("error getting values: %w", err)
+	}
+	ret := make([]value.Value, len(keys))
+	vals := make([]value.Value, len(buckets))
+	for i := 0; i < len(keys); i++ {
+		offset := i * len(buckets)
+		for j := 0; j < len(buckets); j++ {
+			vg := vgs[offset+j]
+			if len(vg.Values) == 0 {
+				vals[j] = ms.mr.Zero()
+			} else {
+				vals[j], err = value.FromJSON(vg.Values[0])
+				if err != nil {
+					return nil, fmt.Errorf("error decoding value(%s): %w", string(vg.Values[0]), err)
+				}
+			}
+		}
+		ret[i], err = ms.mr.Reduce(vals)
+		if err != nil {
+			return nil, fmt.Errorf("error reducing: %w", err)
+		}
+	}
+	return ret, nil
+}
+
+func (ms Closet) Identity() string {
+	return fmt.Sprintf("agg:%d:%d", ms.tierId, ms.aggId)
+}
+
+func (ms Closet) Process(ctx context.Context, ops []*rpc.NitrousOp) ([]hangar.Key, []hangar.ValGroup, error) {
+	// TODO: Should we pre-allocate space?
+	var keys []string
+	var ts []uint32
+	var vals []value.Value
+	for _, op := range ops {
+		switch op.Type {
+		case rpc.OpType_AGG_EVENT:
+			event := op.GetAggEvent()
+			if ftypes.RealmID(op.TierId) != ms.tierId || ftypes.AggId(event.GetAggId()) != ms.aggId {
+				continue
+			}
+			keys = append(keys, event.GetGroupkey())
+			ts = append(ts, event.GetTimestamp())
+			pvalue := event.GetValue()
+			val, err := value.FromProtoValue(pvalue)
+			if err != nil {
+				s, err := protojson.Marshal(pvalue)
+				if err != nil {
+					return nil, nil, fmt.Errorf("error decoding and marshaling value: %w", err)
+				} else {
+					return nil, nil, fmt.Errorf("error decoding value %s: %w", s, err)
+				}
+			}
+			val, err = ms.mr.Transform(val)
+			if err != nil {
+				return nil, nil, fmt.Errorf("error transforming value: %w", err)
+			}
+			vals = append(vals, val)
+		}
+	}
+	return ms.Update(ctx, ts, keys, vals)
+}
+
+func (ms Closet) Update(ctx context.Context, ts []uint32, keys []string, val []value.Value) (
+	[]hangar.Key, []hangar.ValGroup, error) {
+	prefixes := make(map[string]int)
+	var hkgs []hangar.KeyGroup
+	var vals []value.Value
+	var expiry []int64
+	for i := 0; i < len(keys); i++ {
+		buckets, ttls, err := ms.bucketizer.BucketizeMoment(ts[i])
+		if err != nil {
+			return nil, nil, fmt.Errorf("error bucketizing ts (%d) for aggId (%d): %w", ts[i], ms.aggId, err)
+		}
+		kgs, err := ms.encodeKeys(keys[i], buckets)
+		if err != nil {
+			return nil, nil, fmt.Errorf("error encoding buckets: %w", err)
+		}
+		for j, kg := range kgs {
+			p := asString(kg.Prefix.Data)
+			if ptr, ok := prefixes[p]; !ok {
+				prefixes[p] = len(vals)
+				vals = append(vals, val[i])
+				expiry = append(expiry, ttls[j])
+				hkgs = append(hkgs, kg)
+			} else {
+				vals[ptr], err = ms.mr.Merge(vals[ptr], val[i])
+				if err != nil {
+					return nil, nil, fmt.Errorf("error merging: %w", err)
+				}
+			}
+		}
+	}
+	vgs, err := ms.plane.Store.GetMany(hkgs)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error reading from store: %w", err)
+	}
+	hkeys := make([]hangar.Key, len(hkgs))
+	for i := range vgs {
+		vg := &vgs[i]
+		hkeys[i] = hkgs[i].Prefix
+		// Select only the field specific to the current aggregate. This is to
+		// avoid data races with other concurrent go-routines trying to update
+		// another field in the same prefix.
+		vg.Select([][]byte{slice.Limit(ms.field)})
+		if len(vg.Fields) == 0 {
+			vg.Fields = append(vg.Fields, slice.Limit(ms.field))
+			vg.Values = append(vg.Values, []byte(vals[i].String()))
+			vg.Expiry = expiry[i]
+		} else {
+			if asString(vg.Fields[0]) != asString(ms.field) {
+				return nil, nil, fmt.Errorf("unexpected field mismatch after select. Expected: %s, Got: %s", asString(ms.field), asString(vg.Fields[0]))
+			}
+			currv, err := value.FromJSON(vg.Values[0])
+			if err != nil {
+				return nil, nil, fmt.Errorf("error decoding value: %w", err)
+			}
+			vals[i], err = ms.mr.Merge(vals[i], currv)
+			if err != nil {
+				return nil, nil, fmt.Errorf("error merging: %w", err)
+			}
+			vg.Values[0] = []byte(vals[i].String())
+			vg.Expiry = expiry[i]
+		}
+	}
+	return hkeys, vgs, nil
+}
---
+++ go/fennel/nitrous/server/store/closet_test.go
@@ -0,0 +1,172 @@
+package store
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"fennel/hangar/db"
+	"fennel/hangar/encoders"
+	"fennel/lib/aggregate"
+	"fennel/lib/ftypes"
+	"fennel/lib/value"
+	"fennel/model/counter"
+	rpc "fennel/nitrous/rpc/v2"
+	"fennel/nitrous/server/temporal"
+	"fennel/plane"
+
+	"github.com/raulk/clock"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+)
+
+func TestAggregateStore(t *testing.T) {
+	planeId := ftypes.RealmID(5)
+	// TODO: Create api to create a test plane.
+	db, err := db.NewHangar(planeId, t.TempDir(), 1<<10, encoders.Default())
+	assert.NoError(t, err)
+	p := plane.Plane{
+		ID:     planeId,
+		Logger: zap.NewNop(),
+		Store:  db,
+	}
+	opts := aggregate.Options{
+		AggType:   "sum",
+		Durations: []uint32{24 * 3600},
+	}
+	aggId := ftypes.AggId(1)
+	mr, err := counter.ToMergeReduce(aggId, opts)
+	assert.NoError(t, err)
+	b := temporal.NewFixedWidthBucketizer(opts, 5, clock.New())
+	tierId := ftypes.RealmID(1)
+	cs, err := NewCloset(p, tierId, aggId, rpc.AggCodec_V1, mr, b)
+	assert.NoError(t, err)
+	ctx := context.Background()
+
+	val, err := cs.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(0)}, val)
+
+	keys, vgs, err := cs.Update(ctx, []uint32{uint32(time.Now().Unix())}, []string{"mygk"}, []value.Value{value.Int(5)})
+	assert.NoError(t, err)
+	err = db.SetMany(keys, vgs)
+	assert.NoError(t, err)
+	val, err = cs.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(5)}, val)
+
+	keys, vgs, err = cs.Update(ctx, []uint32{uint32(time.Now().Unix())}, []string{"mygk"}, []value.Value{value.Int(7)})
+	assert.NoError(t, err)
+	err = db.SetMany(keys, vgs)
+	assert.NoError(t, err)
+	val, err = cs.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(12)}, val)
+}
+
+func TestProcess(t *testing.T) {
+	planeId := ftypes.RealmID(5)
+	// TODO: Create api to create a test plane.
+	db, err := db.NewHangar(planeId, t.TempDir(), 1<<10, encoders.Default())
+	assert.NoError(t, err)
+	p := plane.Plane{
+		ID:     planeId,
+		Logger: zap.NewNop(),
+		Store:  db,
+	}
+	opts := aggregate.Options{
+		AggType:   "max",
+		Durations: []uint32{24 * 3600},
+	}
+	aggId := ftypes.AggId(1)
+	mr, err := counter.ToMergeReduce(aggId, opts)
+	assert.NoError(t, err)
+	ck := clock.NewMock()
+	ck.Add(time.Since(time.Unix(0, 0)))
+	b := temporal.NewFixedWidthBucketizer(opts, 100, ck)
+	tierId := ftypes.RealmID(1)
+	cs, err := NewCloset(p, tierId, aggId, rpc.AggCodec_V1, mr, b)
+	assert.NoError(t, err)
+	ctx := context.Background()
+
+	// function for pushing an event to Closet.
+	pushEvent := func(cs Closet, tierId ftypes.RealmID, aggId ftypes.AggId, gk string, val value.Value) {
+		ev, err := value.ToProtoValue(val)
+		assert.NoError(t, err)
+		ks, vgs, err := cs.Process(ctx, []*rpc.NitrousOp{
+			{
+				TierId: uint32(tierId),
+				Type:   rpc.OpType_AGG_EVENT,
+				Op: &rpc.NitrousOp_AggEvent{
+					AggEvent: &rpc.AggEvent{
+						AggId:     uint32(aggId),
+						Groupkey:  gk,
+						Value:     &ev,
+						Timestamp: uint32(ck.Now().Unix()),
+					},
+				},
+			},
+		})
+		assert.NoError(t, err)
+		err = db.SetMany(ks, vgs)
+		assert.NoError(t, err)
+	}
+
+	// max is 0.
+	vals, err := cs.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Double(0)}, vals)
+
+	// max is the inserted value.
+	pushEvent(cs, tierId, aggId, "mygk", value.Int(42))
+	vals, err = cs.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(42)}, vals)
+
+	// max should remain as 42.
+	ck.Add(10 * time.Hour)
+	pushEvent(cs, tierId, aggId, "mygk", value.Int(29))
+	vals, err = cs.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(42)}, vals)
+
+	// Add an event for another agg in the same time window should not conflict
+	// and should not overwrite.
+	opts = aggregate.Options{
+		AggType:   "sum",
+		Durations: []uint32{24 * 3600},
+	}
+	aggId2 := ftypes.AggId(2)
+	mr2, err := counter.ToMergeReduce(aggId2, opts)
+	assert.NoError(t, err)
+	cs2, err := NewCloset(p, tierId, aggId2, rpc.AggCodec_V1, mr2, b)
+	assert.NoError(t, err)
+	pushEvent(cs2, tierId, aggId2, "mygk", value.Int(531))
+	vals, err = cs2.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(531)}, vals)
+	vals, err = cs.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(42)}, vals)
+
+	// "42" will expire and max should now be 29 for agg1.
+	// agg2 should remain unchanged.
+	ck.Add(16 * time.Hour)
+	pushEvent(cs, tierId, aggId, "mygk", value.Int(-10))
+	vals, err = cs.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(29)}, vals)
+	vals, err = cs2.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(531)}, vals)
+
+	// "29" will expire and max should now be -10.
+	// agg2 value should also expire and now return 0.
+	ck.Add(10 * time.Hour)
+	vals, err = cs.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(-10)}, vals)
+	vals, err = cs2.Get(ctx, 24*3600, []string{"mygk"})
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, []value.Value{value.Int(0)}, vals)
+}
