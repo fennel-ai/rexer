f3a525c9260de9cf7247d7c37c4a844b8587d4fa
Abhay Bothra <bothra90@users.noreply.github.com>
2022-06-23 23:43:57 -0700

    nitrous: add module to store aggregate definitions in hangar
    This enables us to run nitrous as a stand-alone service instead of
    relying on aggregate definitions stored in different tier dbs.

---
+++ go/fennel/nitrous/server/metadata/agg_defs_test.go
@@ -0,0 +1,118 @@
+package metadata
+
+import (
+	"context"
+	"fennel/lib/aggregate"
+	"fennel/lib/ftypes"
+	"fennel/lib/nitrous"
+	"fennel/lib/value"
+	rpc "fennel/nitrous/rpc/v2"
+	"fennel/nitrous/server"
+	"fennel/nitrous/server/tailer"
+	"fennel/plane"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitRestore(t *testing.T) {
+	tp := plane.NewTestPlane(t)
+	p := tp.Plane
+
+	tlr := tailer.NewTestTailer(p, nitrous.BINLOG_KAFKA_TOPIC)
+	go tlr.Tail()
+	svr := server.NewServer()
+	ctx := context.Background()
+	tierId := ftypes.RealmID(5)
+	aggId := ftypes.AggId(20)
+
+	req := &rpc.AggregateValuesRequest{
+		TierId:    uint32(tierId),
+		AggId:     uint32(aggId),
+		Codec:     rpc.AggCodec_V1,
+		Duration:  24 * 3600,
+		Groupkeys: []string{"mygk"},
+	}
+
+	_, err := svr.GetAggregateValues(ctx, req)
+	assert.Error(t, err)
+
+	adm := NewAggDefsMgr(p, tlr, svr)
+	err = adm.RestoreAggregates()
+	assert.NoError(t, err)
+	// Get fails since aggregate has not been defined yet.
+	_, err = svr.GetAggregateValues(ctx, req)
+	assert.Error(t, err)
+
+	// Define the aggregate.
+	ks, vgs, err := adm.Process(ctx, []*rpc.NitrousOp{
+		{
+			TierId: uint32(tierId),
+			Type:   rpc.OpType_CREATE_AGGREGATE,
+			Op: &rpc.NitrousOp_CreateAggregate{
+				CreateAggregate: &rpc.CreateAggregate{
+					AggId: uint32(aggId),
+					Options: &aggregate.AggOptions{
+						AggType:   "sum",
+						Durations: []uint32{24 * 3600},
+					},
+				},
+			},
+		},
+	})
+	assert.NoError(t, err)
+	err = p.Store.SetMany(ks, vgs)
+	assert.NoError(t, err)
+
+	resp, err := svr.GetAggregateValues(ctx, req)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(resp.Results))
+	val, err := value.FromProtoValue(resp.Results[0])
+	assert.NoError(t, err)
+	assert.Equal(t, value.Int(0), val)
+
+	// Create some aggregate events.
+	producer := tp.NewProducer(t, nitrous.BINLOG_KAFKA_TOPIC)
+	ev, err := value.ToProtoValue(value.Int(42))
+	assert.NoError(t, err)
+	op := &rpc.NitrousOp{
+		TierId: uint32(tierId),
+		Type:   rpc.OpType_AGG_EVENT,
+		Op: &rpc.NitrousOp_AggEvent{
+			AggEvent: &rpc.AggEvent{
+				AggId:     uint32(aggId),
+				Groupkey:  "mygk",
+				Value:     &ev,
+				Timestamp: uint32(time.Now().Unix()),
+			},
+		},
+	}
+	err = producer.LogProto(ctx, op, nil)
+	assert.NoError(t, err)
+	// Wait for the event to be processed.
+	for {
+		lag, err := tlr.GetLag()
+		assert.NoError(t, err)
+		if lag == 0 {
+			// Sleep a brief amount of time to allow the read from consumer too
+			// actually be processed.
+			time.Sleep(time.Millisecond * 100)
+			break
+		} else {
+			time.Sleep(time.Millisecond * 10)
+		}
+	}
+
+	// Restore on a new "server".
+	svr2 := server.NewServer()
+	adm = NewAggDefsMgr(p, tlr, svr2)
+	err = adm.RestoreAggregates()
+	assert.NoError(t, err)
+	resp, err = svr2.GetAggregateValues(ctx, req)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(resp.Results))
+	val, err = value.FromProtoValue(resp.Results[0])
+	assert.NoError(t, err)
+	assert.Equal(t, value.Int(42), val)
+}
