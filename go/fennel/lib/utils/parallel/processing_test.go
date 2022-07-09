df03ec03f086a11e12df1980f0dd058133dc40c7
Aditya Nambiar <adityanambiar@Adityas-MacBook-Pro.local>
2022-07-09 09:30:37 -0700

    parallel: some benchmarking code

--- go/fennel/lib/utils/parallel/processing_test.go
+++ go/fennel/lib/utils/parallel/processing_test.go
@@ -2,7 +2,11 @@ package parallel_test
 
 import (
 	"context"
+	"math"
+	"math/rand"
 	"runtime"
+	"sort"
+	"sync"
 	"testing"
 	"time"
 
@@ -10,6 +14,10 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func square(x int) (int, error) {
 	return x * x, nil
@@ -49,4 +57,100 @@ func TestWorkerPool(t *testing.T) {
 	expected := []int{1, 4, 9, 16, 25, 36, 49, 64, 81, 100}
 	assert.Equal(t, expected, results)
 	assert.LessOrEqual(t, elapsed, time.Duration(len(inputs)/maxWorkers+2)*time.Second)
+}
+
+func countPrimes(limit int) (int, error) {
+	primes := make([]int, limit)
+	count := 0
+	isPrimeDivisible := func(candidate int) bool {
+		for j := 0; j < count; j++ {
+			if math.Sqrt(float64(candidate)) < float64(primes[j]) {
+				return false
+			}
+			isDivisibe := isDivisible(candidate, primes[j])
+			if isDivisibe {
+				return true
+			}
+		}
+		return false
+	}
+	for candidate := 2; ; {
+		if count < limit {
+			if !isPrimeDivisible(candidate) {
+				primes[count] = candidate
+				count++
+			}
+			candidate++
+		} else {
+			break
+		}
+	}
+	return primes[limit-1], nil
+}
+
+func isDivisible(candidate, by int) bool {
+	return candidate%by == 0
+}
+
+func cpuIntensiveWork(sz int) {
+	arr := make([]int, sz)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = rand.Int()
+	}
+	// sort the array
+	sort.Ints(arr)
+	// shuffle the array
+	for i := len(arr) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+
+}
+
+var workerPool = parallel.NewWorkerPool[int, int](runtime.GOMAXPROCS(0))
+
+func featureThread(mode string) {
+	// Does some cpu intensive work
+	// 1000 sized array takes 120 microseconds
+	cpuIntensiveWork(rand.Intn(5000))
+	arr := make([]int, 1000)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = rand.Intn(1000) + 1
+	}
+	// Does some io work
+	sleepTime := time.Duration(rand.Intn(20)) * time.Millisecond
+	time.Sleep(sleepTime)
+	// Does a lot more cpu intensive work
+	switch mode {
+	case "iterative":
+		// Total takes around 12ms
+		for i := 0; i < len(arr); i++ {
+			// Each one takes around 120 microseconds
+			countPrimes(arr[i])
+		}
+	case "parallel":
+		parallel.Process(context.Background(), runtime.GOMAXPROCS(0), arr, countPrimes)
+	case "pool":
+		workerPool.Process(context.Background(), arr, countPrimes)
+	}
+}
+
+// go test -tags dynamic -bench Benchmark_Parallelization -v fennel/lib/utils/parallel -run ^$
+// iterative : 360ms
+// parallel : 301ms
+// worker pool : 424ms
+func Benchmark_Parallelization(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		for j := 0; j < 100; j++ {
+			wg.Add(1)
+			go func() {
+				// 1 call takes ~25ms
+				featureThread("pool")
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
 }
