0b702630ef5dd0a102a1c4bc4324eb9ec190e73d
Abhay Bothra <bothra90@users.noreply.github.com>
2022-07-09 13:31:09 -0700

    pool: add batching to worker pool jobs and move to separate file

---
+++ go/fennel/lib/utils/parallel/pool_test.go
@@ -0,0 +1,43 @@
+package parallel_test
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+
+	"fennel/lib/utils/parallel"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWorkerPool(t *testing.T) {
+	inputs := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	maxWorkers := runtime.GOMAXPROCS(0)
+	workerPool := parallel.NewWorkerPool[int, int](maxWorkers)
+	start := time.Now()
+	results, err := workerPool.Process(context.Background(), inputs, square)
+	elapsed := time.Since(start)
+	assert.NoError(t, err)
+	expected := []int{1, 4, 9, 16, 25, 36, 49, 64, 81, 100}
+	assert.Equal(t, expected, results)
+	assert.LessOrEqual(t, elapsed, time.Duration(len(inputs)/maxWorkers+2)*time.Second)
+}
+
+func TestWorkerPoolWithError(t *testing.T) {
+	var inputs []int
+	for i := 0; i < 1000; i++ {
+		inputs = append(inputs, i)
+	}
+	maxWorkers := runtime.GOMAXPROCS(0)
+	workerPool := parallel.NewWorkerPool[int, int](maxWorkers)
+	f := func(x int) (int, error) {
+		if x%4 == 0 {
+			return 0, fmt.Errorf("error")
+		}
+		return square(x)
+	}
+	_, err := workerPool.Process(context.Background(), inputs, f)
+	assert.Error(t, err)
+}
--- go/fennel/lib/utils/parallel/processing.go
+++ go/fennel/lib/utils/parallel/processing.go
@@ -2,18 +2,13 @@ package parallel
 
 import (
 	"context"
-	"github.com/samber/mo"
-	"golang.org/x/sync/errgroup"
 	"runtime"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type input[I any] struct {
 	inp   I
-	index int
-}
-
-type response[R any] struct {
-	resp  R
 	index int
 }
 
@@ -54,84 +49,4 @@ func Process[I, R any](ctx context.Context, parallelism int, inputs []I, f func(
 		})
 	}
 	return ret, g.Wait()
-}
-
-type WorkerPool[I, R any] struct {
-	jobQueue chan Job[input[I], response[R]]
-}
-
-// NewWorkerPool starts a pool of workers and returns a channel
-// which needs to be passed while calling ProcessUsingWorkerPool.
-func NewWorkerPool[I, R any](nWorkers int) WorkerPool[I, R] {
-	jobQueue := make(chan Job[input[I], response[R]])
-	for i := 0; i < nWorkers; i++ {
-		worker := NewWorker[input[I], response[R]](jobQueue)
-		worker.Start()
-	}
-	return WorkerPool[I, R]{jobQueue: jobQueue}
-}
-
-func (w *WorkerPool[I, R]) Process(ctx context.Context, inputs []I, f func(I) (R, error)) ([]R, error) {
-	ret := make([]R, len(inputs))
-	retChan := make(chan mo.Result[response[R]])
-	defer close(retChan)
-	wrappedF := func(i input[I]) (response[R], error) {
-		r, err := f(i.inp)
-		if err != nil {
-			return response[R]{}, err
-		}
-		return response[R]{r, i.index}, nil
-	}
-
-	// Needs to run in a separate go routine as else workers will try to write to retChan but no one is reading
-	// from retChan, hence blocking the workers.
-	go func() {
-		for i := range inputs {
-			w.jobQueue <- Job[input[I], response[R]]{input[I]{inputs[i], i}, wrappedF, retChan}
-		}
-	}()
-
-	for i := 0; i < len(inputs); i++ {
-		select {
-		case r := <-retChan:
-			output, err := r.Get()
-			if err != nil {
-				return nil, err
-			}
-			ret[output.index] = output.resp
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		}
-	}
-
-	return ret, nil
-}
-
-// Job represents the job to be run. It accepts a function F that needs to be run on
-// an input of type I and returns the response of type R in the return channel passed to it.
-type Job[I, R any] struct {
-	Input   I
-	F       func(I) (R, error)
-	RetChan chan mo.Result[R]
-}
-
-// Worker represents the worker that executes the job
-type Worker[I, R any] struct {
-	jobQueue chan Job[I, R]
-}
-
-func NewWorker[I, R any](jobQueue chan Job[I, R]) Worker[I, R] {
-	return Worker[I, R]{
-		jobQueue: jobQueue,
-	}
-}
-
-// Start method starts the run loop for the worker.
-func (w Worker[I, R]) Start() {
-	go func() {
-		for job := range w.jobQueue {
-			resp, err := job.F(job.Input)
-			job.RetChan <- mo.TupleToResult(resp, err)
-		}
-	}()
 }
