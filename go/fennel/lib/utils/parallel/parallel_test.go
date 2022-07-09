22324f3157702ba098f8c5b3edc971b1d9a4922b
Abhay Bothra <bothra90@users.noreply.github.com>
2022-07-09 14:28:17 -0700

    pool: add batching to worker pool jobs and move to separate file (#1114)
    Also
    - added a test for the WorkerPool where the given function returns an error
    - made worker and job private structs instead of exporting from the pkg

---
+++ go/fennel/lib/utils/parallel/parallel_test.go
@@ -0,0 +1,124 @@
+package parallel_test
+
+import (
+	"context"
+	"math"
+	"math/rand"
+	"runtime"
+	"sort"
+	"sync"
+	"testing"
+	"time"
+
+	"fennel/lib/utils/parallel"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func square(x int) (int, error) {
+	return x * x, nil
+}
+
+func squareSleep(x int) (int, error) {
+	time.Sleep(1 * time.Second)
+	return x * x, nil
+}
+
+func countPrimes(limit int) (int, error) {
+	primes := make([]int, limit)
+	count := 0
+	isPrimeDivisible := func(candidate int) bool {
+		for j := 0; j < count; j++ {
+			if math.Sqrt(float64(candidate)) < float64(primes[j]) {
+				return false
+			}
+			isDivisibe := isDivisible(candidate, primes[j])
+			if isDivisibe {
+				return true
+			}
+		}
+		return false
+	}
+	for candidate := 2; ; {
+		if count < limit {
+			if !isPrimeDivisible(candidate) {
+				primes[count] = candidate
+				count++
+			}
+			candidate++
+		} else {
+			break
+		}
+	}
+	return primes[limit-1], nil
+}
+
+func isDivisible(candidate, by int) bool {
+	return candidate%by == 0
+}
+
+func cpuIntensiveWork(sz int) {
+	arr := make([]int, sz)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = rand.Int()
+	}
+	// sort the array
+	sort.Ints(arr)
+	// shuffle the array
+	for i := len(arr) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+
+}
+
+var workerPool = parallel.NewWorkerPool[int, int](runtime.GOMAXPROCS(0))
+
+func featureThread(b *testing.B, mode string) {
+	// Does some cpu intensive work
+	// 1000 sized array takes 120 microseconds
+	cpuIntensiveWork(rand.Intn(5000))
+	arr := make([]int, 1000)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = rand.Intn(1000) + 1
+	}
+	// Does some io work
+	sleepTime := time.Duration(rand.Intn(20)) * time.Millisecond
+	time.Sleep(sleepTime)
+	// Does a lot more cpu intensive work
+	switch mode {
+	case "iterative":
+		// Total takes around 12ms
+		for i := 0; i < len(arr); i++ {
+			// Each one takes around 120 microseconds
+			_, err := countPrimes(arr[i])
+			assert.NoError(b, err)
+		}
+	case "parallel":
+		_, err := parallel.Process(context.Background(), runtime.GOMAXPROCS(0), arr, countPrimes)
+		assert.NoError(b, err)
+	case "pool":
+		_, err := workerPool.Process(context.Background(), arr, countPrimes)
+		assert.NoError(b, err)
+	}
+}
+
+// go test -tags dynamic -bench Benchmark_Parallelization -v fennel/lib/utils/parallel -run ^$
+// iterative : 332ms
+// parallel : 292ms
+// worker pool : 286ms
+func Benchmark_Parallelization(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		for j := 0; j < 100; j++ {
+			wg.Add(1)
+			go func() {
+				// 1 call takes ~25ms
+				featureThread(b, "pool")
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	}
+}
---
+++ go/fennel/lib/utils/parallel/pool.go
@@ -0,0 +1,102 @@
+package parallel
+
+import "context"
+
+// WorkerPool is a pool of workers that can be used to run jobs.
+// Jobs are submitted to the pool via the `Process` method.
+type WorkerPool[I, O any] struct {
+	// channel over which sub-tasks are dispatched to the workers.
+	jobQueue chan<- job[I, O]
+}
+
+func NewWorkerPool[I, O any](nWorkers int) WorkerPool[I, O] {
+	jobQueue := make(chan job[I, O])
+	for i := 0; i < nWorkers; i++ {
+		worker := newWorker(jobQueue)
+		worker.start()
+	}
+	return WorkerPool[I, O]{jobQueue: jobQueue}
+}
+
+func (w *WorkerPool[I, O]) Process(ctx context.Context, inputs []I, f func(I) (O, error)) ([]O, error) {
+	ret := make([]O, len(inputs))
+	// We use a batch size of 16 to dispatch jobs to the workers. This amortizes
+	// the synchronization cost of sending a job to a worker, but does not make
+	// the batch so large that we lose the benefit of parallelism.
+	// A batch size of 16 performs reasonably well in our benchmark.
+	const BATCH_SIZE = 16
+	numBatches := (len(inputs) + BATCH_SIZE - 1) / BATCH_SIZE
+	// Note: We don't close errCh because we early-return in case of an error,
+	// but don't want worker go-routines to panic if they try to write on
+	// a closed channel. This is also why we create a buffered channel, so that
+	// worker go-routines can write to it even if there is no consumer.
+	errCh := make(chan error, numBatches)
+	// Needs to run in a separate go routine as else workers will try to write to retChan but no one is reading
+	// from retChan, hence blocking the workers.
+	go func() {
+		for i := 0; i < len(inputs); {
+			end := i + BATCH_SIZE
+			if end > len(inputs) {
+				end = len(inputs)
+			}
+			// TODO: If there's already been an error, stop dispatching more
+			// jobs in this task to the worker.
+			w.jobQueue <- job[I, O]{
+				inputs:  inputs[i:end],
+				outputs: ret[i:end],
+				f:       f,
+				errChan: errCh,
+			}
+			i = end
+		}
+	}()
+	for i := 0; i < numBatches; i++ {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case err := <-errCh:
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return ret, nil
+}
+
+// job represents the job to be run. It accepts a function `f` that needs to be
+// run on inputs of type I and writes outputs of type O to the given `outputs``
+// slice. If an error is encountered, it is written to the `errChan` and the
+// remaining slice is not processed.
+type job[I, O any] struct {
+	inputs  []I
+	outputs []O
+	f       func(I) (O, error)
+	errChan chan<- error
+}
+
+// worker accepts a job from `jobQueue` and runs it.
+type worker[I, O any] struct {
+	jobQueue <-chan job[I, O]
+}
+
+func newWorker[I, O any](jobQueue chan job[I, O]) worker[I, O] {
+	return worker[I, O]{
+		jobQueue: jobQueue,
+	}
+}
+
+// start method starts the run loop for the worker.
+func (w worker[I, O]) start() {
+	go func() {
+		for job := range w.jobQueue {
+			var err error
+			for i, input := range job.inputs {
+				job.outputs[i], err = job.f(input)
+				if err != nil {
+					break
+				}
+			}
+			job.errChan <- err
+		}
+	}()
+}
