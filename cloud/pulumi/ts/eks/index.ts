6715b70a30614b651b0563a11e1717506cf010d8
Mohit Reddy <mohitreddy1996@users.noreply.github.com>
2022-07-11 21:16:20 -0700

    cloud: enable remotely storing emissary ingress logs and format it to show additional info (#1116)
    * cloud: enable remotely storing emissary ingress logs and format it to show additional info
    
    * cloud: Add documentation on why formatting is not working

--- cloud/pulumi/ts/eks/index.ts
+++ cloud/pulumi/ts/eks/index.ts
@@ -155,7 +155,11 @@ async function setupEmissaryIngressCrds(input: inputType, awsProvider: aws.Provi
                         // Specifies the maximum number of parallel outstanding requests to ALL hosts in a cluster at any given time.
                         "max_requests": 3072,
                         // default - "max_retries": 3,
-                    }
+                    },
+                    // NOTE: This does not seem to be working
+                    // See: https://github.com/emissary-ingress/emissary/issues/4329
+                    "envoy_log_type": "text",
+                    "envoy_log_format": "%REQ(:METHOD)% %RESPONSE_CODE% %RESPONSE_FLAGS% %RESPONSE_CODE_DETAILS% %CONNECTION_TERMINATION_DETAILS% %DURATION%"
                 }
             }
         }, { provider: cluster.provider, dependsOn: waiter })
