ecf5b982c4d875da85566cdbbefb95d6d5133550
Xiao Jiang <xiao@fennel.ai>
2022-07-13 14:18:48 -0700

    comment: tier

--- go/fennel/opdefs/model/predict_no_sm_test.go
+++ go/fennel/opdefs/model/predict_no_sm_test.go
@@ -10,6 +10,9 @@ import (
 )
 
 func TestPredictErrorNoModelParam(t *testing.T) {
+	tier := test.Tier(t)
+	defer test.Teardown(tier)
+
 	intable := []value.Value{
 		value.Nil,
 	}
@@ -19,12 +22,13 @@ func TestPredictErrorNoModelParam(t *testing.T) {
 		}),
 	}
 
-	tier := test.Tier(t)
-	defer test.Teardown(tier)
 	optest.AssertErrorIs(t, tier, &predictOperator{}, value.Dict{} /* no static kwargs */, [][]value.Value{intable}, contextKwargTable, &interpreter.RequiredKwargNotProvidedError{ParamName: "model", OpModule: "model", OpName: "predict"})
 }
 
 func TestPredictErrorNoInputParam(t *testing.T) {
+	tier := test.Tier(t)
+	defer test.Teardown(tier)
+
 	intable := []value.Value{
 		value.Nil,
 	}
@@ -35,12 +39,13 @@ func TestPredictErrorNoInputParam(t *testing.T) {
 		value.NewDict(map[string]value.Value{}),
 	}
 
-	tier := test.Tier(t)
-	defer test.Teardown(tier)
 	optest.AssertErrorContains(t, tier, &predictOperator{}, staticKwargsTable, [][]value.Value{intable}, contextKwargTable, "input is not a list, got 'null'")
 }
 
 func TestPredictErrorNoModelStore(t *testing.T) {
+	tier := test.Tier(t)
+	defer test.Teardown(tier)
+
 	intable := []value.Value{
 		value.NewList(value.String("1:1 9:1 19:1 21:1 24:1 34:1 36:1 39:1 42:1 53:1 56:1 65:1 69:1 77:1 86:1 88:1 92:1 95:1 102:1 106:1 117:1 122:1")),
 	}
@@ -51,7 +56,5 @@ func TestPredictErrorNoModelStore(t *testing.T) {
 		value.NewDict(map[string]value.Value{}),
 	}
 
-	tier := test.Tier(t)
-	defer test.Teardown(tier)
 	optest.AssertErrorContains(t, tier, &predictOperator{}, staticKwargsTable, [][]value.Value{intable}, contextKwargTable, "could not get framework from db")
 }
