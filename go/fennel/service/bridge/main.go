bbbcdade3197585e818c8b96ed225371206328b6
Xiao Jiang <xiao@fennel.ai>
2022-07-13 15:08:46 -0700

    fix lint

--- go/fennel/service/bridge/main.go
+++ go/fennel/service/bridge/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -70,5 +71,7 @@ func setupRouter() *gin.Engine {
 func main() {
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Error running the server: %s", err)
+	}
 }
