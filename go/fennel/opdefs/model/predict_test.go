559de0f4a2414dbd7a17ef4e2941cb8231582ac4
Xiao Jiang <xiao@fennel.ai>
2022-07-13 14:46:49 -0700

    rex-1276: remove input type constraint on model predictor op (#1099)
    * rex-1276: remove input type constraint on model predictor op
    
    * fix tests
    
    * comment: add RequiredKwargNotProvidedError
    
    * comment: tier

--- go/fennel/opdefs/model/predict_test.go
+++ go/fennel/opdefs/model/predict_test.go
@@ -74,20 +74,3 @@ func TestPredictError(t *testing.T) {
 		"model_version": value.String("v1"),
 	}), [][]value.Value{intable}, contextKwargTable)
 }
-
-func TestPredictErrorNoModel(t *testing.T) {
-	intable := []value.Value{
-		value.Nil,
-	}
-	contextKwargTable := []value.Dict{
-		value.NewDict(map[string]value.Value{
-			"input": value.NewList(value.String("1:1 9:1 19:1 21:1 24:1 34:1 36:1 39:1 42:1 53:1 56:1 65:1 69:1 77:1 86:1 88:1 92:1 95:1 102:1 106:1 117:1 122:1")),
-		}),
-	}
-
-	tier := test.Tier(t)
-	defer test.Teardown(tier)
-	err := test.AddSagemakerClientToTier(&tier)
-	assert.NoError(t, err)
-	optest.AssertError(t, tier, &predictOperator{}, value.Dict{} /* no static kwargs */, [][]value.Value{intable}, contextKwargTable)
-}
