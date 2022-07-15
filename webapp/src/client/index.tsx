899c4edfaee906533274c7c3cda2ad628839765c
Xiao Jiang <xiao@fennel.ai>
2022-07-15 11:38:22 -0700

    [webapp] add eslint

--- webapp/src/client/index.tsx
+++ webapp/src/client/index.tsx
@@ -2,5 +2,5 @@ import App from "./App";
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
-const root = createRoot(container!);
+const root = createRoot(container!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
 root.render(<App />);
