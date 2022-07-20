43c68dc3eab2145583c0b1909c89bf3d0f699bfd
Abhay Bothra <bothra90@users.noreply.github.com>
2022-07-20 00:06:16 -0700

    hangar: add in-memory implementation

---
+++ go/fennel/hangar/test/in_memory.go
@@ -0,0 +1,92 @@
+package test
+
+import (
+	"io"
+	"sync"
+
+	"fennel/hangar"
+	"fennel/lib/ftypes"
+)
+
+// In-memory implementation of the Hangar interface.
+// Note that this implementation does not perform key-expiry.
+type InMemoryHangar struct {
+	planeId ftypes.RealmID
+	m       map[string]map[string]string
+
+	mu sync.Mutex
+}
+
+func NewInMemoryHangar(planeId ftypes.RealmID) *InMemoryHangar {
+	return &InMemoryHangar{
+		planeId: planeId,
+		m:       make(map[string]map[string]string),
+	}
+}
+
+var _ hangar.Hangar = &InMemoryHangar{}
+
+func (h *InMemoryHangar) PlaneID() ftypes.RealmID {
+	return h.planeId
+}
+
+func (h *InMemoryHangar) GetMany(kgs []hangar.KeyGroup) ([]hangar.ValGroup, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	var vgs []hangar.ValGroup
+	for _, kg := range kgs {
+		vg := hangar.ValGroup{}
+		if val, ok := h.m[string(kg.Prefix.Data)]; ok {
+			if kg.Fields.IsAbsent() {
+				for k, v := range val {
+					vg.Fields = append(vg.Fields, []byte(k))
+					vg.Values = append(vg.Values, []byte(v))
+				}
+			} else {
+				for _, f := range kg.Fields.MustGet() {
+					if v, ok := val[string(f)]; ok {
+						vg.Fields = append(vg.Fields, f)
+						vg.Values = append(vg.Values, []byte(v))
+					}
+				}
+			}
+		}
+		vgs = append(vgs, vg)
+	}
+	return vgs, nil
+}
+
+func (h *InMemoryHangar) SetMany(keys []hangar.Key, vgs []hangar.ValGroup) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	for i, key := range keys {
+		vg := vgs[i]
+		if _, ok := h.m[string(key.Data)]; !ok {
+			h.m[string(key.Data)] = make(map[string]string)
+		}
+		for j, f := range vg.Fields {
+			h.m[string(key.Data)][string(f)] = string(vg.Values[j])
+		}
+	}
+	return nil
+}
+
+func (h *InMemoryHangar) DelMany(keys []hangar.KeyGroup) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	for _, key := range keys {
+		k := string(key.Prefix.Data)
+		if key.Fields.IsAbsent() {
+			delete(h.m, k)
+		} else {
+			for _, f := range key.Fields.MustGet() {
+				delete(h.m[k], string(f))
+			}
+		}
+	}
+	return nil
+}
+func (h *InMemoryHangar) Close() error                                        { return nil }
+func (h *InMemoryHangar) Teardown() error                                     { return nil }
+func (h *InMemoryHangar) Backup(sink io.Writer, since uint64) (uint64, error) { return 0, nil }
+func (h *InMemoryHangar) Restore(source io.Reader) error                      { return nil }
---
+++ go/fennel/hangar/test/in_memory_test.go
@@ -0,0 +1,104 @@
+package test
+
+import (
+	"testing"
+
+	"fennel/hangar"
+	"fennel/lib/ftypes"
+
+	"github.com/samber/mo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasic(t *testing.T) {
+	planeId := ftypes.RealmID(1)
+	h := NewInMemoryHangar(planeId)
+
+	// Get before set.
+	vgs, err := h.GetMany([]hangar.KeyGroup{
+		{
+			Prefix: hangar.Key{
+				Data: []byte("foo"),
+			},
+			Fields: mo.None[hangar.Fields](),
+		},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(vgs), "%+v", vgs)
+	assert.Equal(t, 0, len(vgs[0].Fields), "%+v", vgs[0])
+	assert.Equal(t, 0, len(vgs[0].Values), "%+v", vgs[0])
+
+	// Now set some fields.
+	fields := [][]byte{[]byte("a"), []byte("b")}
+	values := [][]byte{[]byte("x"), []byte("y")}
+	err = h.SetMany([]hangar.Key{
+		{
+			Data: []byte("foo"),
+		},
+	}, []hangar.ValGroup{
+		{
+			Fields: fields,
+			Values: values,
+		},
+	})
+	assert.NoError(t, err)
+
+	// Get all values for "foo" prefix.
+	vgs, err = h.GetMany([]hangar.KeyGroup{
+		{
+			Prefix: hangar.Key{
+				Data: []byte("foo"),
+			},
+			Fields: mo.None[hangar.Fields](),
+		},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(vgs), "%+v", vgs)
+	assert.Equal(t, 2, len(vgs[0].Fields), "%+v", vgs[0])
+	assert.Equal(t, 2, len(vgs[0].Values), "%+v", vgs[0])
+	assert.ElementsMatch(t, fields, vgs[0].Fields)
+	assert.ElementsMatch(t, values, vgs[0].Values)
+
+	// Get a specific field for "foo" prefix.
+	vgs, err = h.GetMany([]hangar.KeyGroup{
+		{
+			Prefix: hangar.Key{
+				Data: []byte("foo"),
+			},
+			Fields: mo.Some[hangar.Fields](fields[:1]),
+		},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(vgs), "%+v", vgs)
+	assert.Equal(t, 1, len(vgs[0].Fields), "%+v", vgs[0])
+	assert.Equal(t, 1, len(vgs[0].Values), "%+v", vgs[0])
+	assert.ElementsMatch(t, fields[:1], vgs[0].Fields)
+	assert.ElementsMatch(t, values[:1], vgs[0].Values)
+
+	// Delete a field for "foo" prefix.
+	err = h.DelMany([]hangar.KeyGroup{
+		{
+			Prefix: hangar.Key{
+				Data: []byte("foo"),
+			},
+			Fields: mo.Some[hangar.Fields](fields[:1]),
+		},
+	})
+	assert.NoError(t, err)
+
+	// Now get all fields for "foo".
+	vgs, err = h.GetMany([]hangar.KeyGroup{
+		{
+			Prefix: hangar.Key{
+				Data: []byte("foo"),
+			},
+			Fields: mo.None[hangar.Fields](),
+		},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(vgs), "%+v", vgs)
+	assert.Equal(t, 1, len(vgs[0].Fields), "%+v", vgs[0])
+	assert.Equal(t, 1, len(vgs[0].Values), "%+v", vgs[0])
+	assert.ElementsMatch(t, fields[1:], vgs[0].Fields)
+	assert.ElementsMatch(t, values[1:], vgs[0].Values)
+}
