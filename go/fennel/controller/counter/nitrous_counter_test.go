ed4a158fe1b29467850893dd708fc86617581dae
Abhay Bothra <bothra90@users.noreply.github.com>
2022-07-20 13:09:20 -0700

    counter: fix data race caused by accessing inputs to BatchValue in new go-routine (#1170)

--- go/fennel/controller/counter/nitrous_counter_test.go
+++ go/fennel/controller/counter/nitrous_counter_test.go
@@ -62,7 +62,6 @@ func TestNitrousBatchValue(t *testing.T) {
 	wait()
 
 	aggIds := []ftypes.AggId{aggs[0].Id, aggs[1].Id}
-	aggOptions := []libaggregate.Options{aggs[0].Options, aggs[1].Options}
 	key := value.Int(0)
 	keys := []value.Value{key, key}
 	kwargs := []value.Dict{
@@ -72,7 +71,7 @@ func TestNitrousBatchValue(t *testing.T) {
 
 	// initially should find nothing
 	exp1, exp2 := value.Int(0), value.Double(0)
-	found, err := NitrousBatchValue(ctx, tier, aggIds, aggOptions, keys, kwargs)
+	found, err := NitrousBatchValue(ctx, tier, aggIds, keys, kwargs)
 	assert.NoError(t, err)
 	assert.True(t, exp1.Equal(found[0]))
 	assert.True(t, exp2.Equal(found[1]))
@@ -99,11 +98,8 @@ func TestNitrousBatchValue(t *testing.T) {
 
 	// should find this time
 	exp1, exp2 = value.Int(60*48), value.Double(1.0)
-	found, err = NitrousBatchValue(ctx, tier, aggIds, aggOptions, keys, kwargs)
-	assert.NoError(t, err)
-	assert.Equal(t, 2, len(found))
 	for {
-		found, err = NitrousBatchValue(ctx, tier, aggIds, aggOptions, keys, kwargs)
+		found, err = NitrousBatchValue(ctx, tier, aggIds, keys, kwargs)
 		assert.NoError(t, err)
 		assert.Equal(t, 2, len(found))
 		if found[0].(value.Int) < exp1 {
@@ -123,18 +119,18 @@ func TestNitrousBatchValue(t *testing.T) {
 	kwargs[0] = value.NewDict(map[string]value.Value{"duration": value.Int(24 * 3600)})
 	kwargs[1] = value.NewDict(map[string]value.Value{"duration": value.Int(24 * 3600)})
 	exp1, exp2 = value.Int(60*24), value.Double(1.0)
-	found, err = NitrousBatchValue(ctx, tier, aggIds, aggOptions, keys, kwargs)
+	found, err = NitrousBatchValue(ctx, tier, aggIds, keys, kwargs)
 	assert.NoError(t, err)
 	assert.GreaterOrEqual(t, found[0], exp1)
 	assert.GreaterOrEqual(t, found[1], exp2)
 
 	// not specifying a duration in kwargs should return an error
 	kwargs[1] = value.NewDict(nil)
-	_, err = NitrousBatchValue(ctx, tier, aggIds, aggOptions, keys, kwargs)
+	_, err = NitrousBatchValue(ctx, tier, aggIds, keys, kwargs)
 	assert.Error(t, err)
 
 	// specifying a duration that wasn't registered should also return an error
 	kwargs[1] = value.NewDict(map[string]value.Value{"duration": value.Int(7 * 24 * 3600)})
-	_, err = NitrousBatchValue(ctx, tier, aggIds, aggOptions, keys, kwargs)
+	_, err = NitrousBatchValue(ctx, tier, aggIds, keys, kwargs)
 	assert.Error(t, err)
 }
