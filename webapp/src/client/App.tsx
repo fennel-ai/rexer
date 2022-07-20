b76fcfd05237778493102df0137bc37da2a33da3
Xiao Jiang <xiao@fennel.ai>
2022-07-20 10:52:42 -0700

    [webapp] layman FE route

--- webapp/src/client/App.tsx
+++ webapp/src/client/App.tsx
@@ -1,21 +1,39 @@
 import "./styles/App.css";
-import styles from "./styles/App.module.scss";
 import Navbar from "./Navbar";
-import { Button, DatePicker } from "antd";
 
-function App() {
+interface Props {
+    page: string | null;
+}
+
+const DASHBOARD_PAGE = "dashboard";
+const DATA_PAGE = "data";
+
+function App(props: Props) {
+    const page = props.page || DASHBOARD_PAGE
     return (
         <div>
-            <Navbar />
-            <h1 className={styles.foo}>
-                Hello world!
-            </h1>
-            <DatePicker />
-            <Button type="primary" style={{ marginLeft: 8 }}>
-                Primary Button
-            </Button>
+            <Navbar page={page} />
+            <Route page={page} />
         </div>
     );
+}
+
+function Route(props: Props) {
+    switch (props.page) {
+        case DATA_PAGE:
+            return <DataPage />;
+        case DASHBOARD_PAGE:
+            return <DashboardPage />;
+    }
+    return <DashboardPage />;
+}
+
+function DataPage() {
+    return (<h1> Data </h1>);
+}
+
+function DashboardPage() {
+    return (<h1> Dashboard </h1>);
 }
 
 export default App;
