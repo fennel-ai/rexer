ea0506af5ad9dd9206006b55f2218fccfb265be6
Xiao Jiang <xiao@fennel.ai>
2022-07-22 17:23:46 -0700

    [webapp] sign up page skeleton (#1188)
    * [Savepoint] simple route
    
    * [webapp] signup page skeleton

--- webapp/src/client/signon/SignOnApp.tsx
+++ webapp/src/client/signon/SignOnApp.tsx
@@ -1,9 +1,33 @@
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+
 interface Props {
     page: string,
 }
 
+const SIGNUP_PAGE = "signup";
+const SIGNIN_PAGE = "signin";
+
 function SignOnApp(props: Props) {
-    return <div>Sign On</div>;
+    const {page} = props;
+
+    return (
+        <div>
+            <Route page={page} />
+        </div>
+    )
+}
+
+function Route(props: Props) {
+    const {page} = props;
+
+    switch (page) {
+        case SIGNUP_PAGE:
+            return <SignUp />
+        case SIGNIN_PAGE:
+            return <SignIn />
+    }
+    return <SignIn />;
 }
 
 export default SignOnApp;
