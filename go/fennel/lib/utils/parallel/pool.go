c2a8208c311ccef7cb634b5afb07a3959ae3063d
Abhay Bothra <bothra90@users.noreply.github.com>
2022-08-05 17:31:51 -0700

    pool: add some capacity to job queue and submit jobs synchronously

--- go/fennel/lib/utils/parallel/pool.go
+++ go/fennel/lib/utils/parallel/pool.go
@@ -9,20 +9,37 @@ import (
 // Jobs are submitted to the pool via the `Process` method.
 type WorkerPool[I, O any] struct {
 	// channel over which sub-tasks are dispatched to the workers.
-	jobQueue chan<- job[I, O]
+	jobQueue chan job[I, O]
 	// WaitGroup to wait for all goroutines to finish.
 	wg *sync.WaitGroup
 }
 
+// job represents the job to be run. It accepts a function `f` that needs to be
+// run on inputs of type I and writes outputs of type O to the given `outputs``
+// slice. If an error is encountered, it is written to the `errChan` and the
+// remaining slice is not processed.
+type job[I, O any] struct {
+	ctx     context.Context
+	inputs  []I
+	outputs []O
+	f       func([]I, []O) error
+	errChan chan<- error
+}
+
 func NewWorkerPool[I, O any](nWorkers int) *WorkerPool[I, O] {
-	jobQueue := make(chan job[I, O])
+	// We provide a lot of buffer in the job queue to avoid tail latency problems
+	// that are caused by the overhead of goroutine scheduling delay.
+	jobQueue := make(chan job[I, O], 100*nWorkers)
 	wg := &sync.WaitGroup{}
+	pool := WorkerPool[I, O]{
+		jobQueue: jobQueue,
+		wg:       wg,
+	}
 	wg.Add(nWorkers)
 	for i := 0; i < nWorkers; i++ {
-		worker := worker[I, O]{jobQueue, wg}
-		worker.start()
+		go pool.work()
 	}
-	return &WorkerPool[I, O]{jobQueue, wg}
+	return &pool
 }
 
 func (w *WorkerPool[I, O]) Process(ctx context.Context, inputs []I, f func([]I, []O) error, batchSize int) ([]O, error) {
@@ -31,27 +48,24 @@ func (w *WorkerPool[I, O]) Process(ctx context.Context, inputs []I, f func([]I, 
 	errCh := make(chan error, numBatches)
 	ctx, cancelFn := context.WithCancel(ctx)
 	defer cancelFn()
-	// Run the job submission in a go-routine, so that we can start consuming
-	// results/errors from errCh and cancel any outstanding jobs.
-	go func() {
-		start := 0
-		for i := 0; i < numBatches; i++ {
-			end := start + batchSize
-			if end > len(inputs) {
-				end = len(inputs)
-			}
-			w.jobQueue <- job[I, O]{
-				ctx:     ctx,
-				inputs:  inputs[start:end],
-				outputs: ret[start:end],
-				f:       f,
-				errChan: errCh,
-			}
-			start = end
+	// Break the input into batches and submit them to the job queue.
+	// After submission, we wait for the results to be available in the errCh
+	// channel. If an error occurs, we cancel the context and wait for the
+	// remaining jobs to return.
+	for start := 0; start < len(inputs); start += batchSize {
+		end := start + batchSize
+		if end > len(inputs) {
+			end = len(inputs)
 		}
-	}()
+		w.jobQueue <- job[I, O]{
+			ctx:     ctx,
+			inputs:  inputs[start:end],
+			outputs: ret[start:end],
+			f:       f,
+			errChan: errCh,
+		}
+	}
 	var err error
-	// We wait for all the jobs to complete.
 	for i := 0; i < numBatches; i++ {
 		e := <-errCh
 		if e != nil {
@@ -70,37 +84,16 @@ func (w *WorkerPool[I, O]) Close() {
 	w.wg.Wait()
 }
 
-// job represents the job to be run. It accepts a function `f` that needs to be
-// run on inputs of type I and writes outputs of type O to the given `outputs``
-// slice. If an error is encountered, it is written to the `errChan` and the
-// remaining slice is not processed.
-type job[I, O any] struct {
-	ctx     context.Context
-	inputs  []I
-	outputs []O
-	f       func([]I, []O) error
-	errChan chan<- error
-}
-
-// worker accepts a job from `jobQueue` and runs it.
-type worker[I, O any] struct {
-	jobQueue <-chan job[I, O]
-	wg       *sync.WaitGroup
-}
-
-// start method starts the run loop for the worker.
-func (w worker[I, O]) start() {
-	go func() {
-		defer w.wg.Done()
-		for job := range w.jobQueue {
-			var err error
-			select {
-			case <-job.ctx.Done():
-				err = job.ctx.Err()
-			default:
-				err = job.f(job.inputs, job.outputs)
-			}
-			job.errChan <- err
+func (w *WorkerPool[I, O]) work() {
+	defer w.wg.Done()
+	for job := range w.jobQueue {
+		var err error
+		select {
+		case <-job.ctx.Done():
+			err = job.ctx.Err()
+		default:
+			err = job.f(job.inputs, job.outputs)
 		}
-	}()
+		job.errChan <- err
+	}
 }
