6968127d2e81f9293483f55e9d983a50e2d06917
Xiao Jiang <xiao@fennel.ai>
2022-08-09 17:18:54 -0700

    [rex-1526][webapp] actions UI

--- webapp/src/client/DataPage.tsx
+++ webapp/src/client/DataPage.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from "antd";
 import styles from "./styles/DataPage.module.scss";
 import ProfilesTab from "./ProfilesTab";
+import ActionsTab from "./ActionsTab";
 
 function DataPage(){
     return (
@@ -12,7 +13,11 @@ function DataPage(){
                         <ProfilesTab />
                     </div>
                 </Tabs.TabPane>
-                <Tabs.TabPane tab="Actions" disabled key="actions" />
+                <Tabs.TabPane tab="Actions" key="actions">
+                    <div className={styles.tabContent}>
+                        <ActionsTab />
+                    </div>
+                </Tabs.TabPane>
                 <Tabs.TabPane tab="Features" disabled key="features" />
             </Tabs>
         </div>
