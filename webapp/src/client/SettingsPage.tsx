bbf4e2647a6ca9aca87236260038a5b4b6d61b96
Xiao Jiang <xiao@fennel.ai>
2022-08-23 16:49:33 -0700

    [Savepoint] url hash to locate the active tab

--- webapp/src/client/SettingsPage.tsx
+++ webapp/src/client/SettingsPage.tsx
@@ -1,20 +1,36 @@
 import { Tabs } from "antd";
+import { useState, useEffect } from "react";
 
 import AccountTab from "./AccountTab";
 import OrgTab from "./OrgTab";
 import styles from "./styles/Page.module.scss";
 
 function SettingsPage() {
+    const tabFromHash = () => (window.location.hash && window.location.hash.substring(1));
+    const [activeTab, setActiveTab] = useState<string>(tabFromHash() || "account");
+    const onTabClick = (key: string) => {
+        window.location.hash = key;
+    };
+    useEffect(() => {
+        const onHashChange = () => {
+            setActiveTab(tabFromHash);
+        };
+        window.addEventListener("hashchange", onHashChange);
+        return () => {
+            window.addEventListener("hashchange", onHashChange);
+        };
+    }, []);
+
     return (
         <div className={styles.container}>
             <h4 className={styles.title}>Settings</h4>
-            <Tabs defaultActiveKey="account" className={styles.tabs}>
+            <Tabs className={styles.tabs} activeKey={activeTab} onTabClick={onTabClick}>
                 <Tabs.TabPane tab="Account" key="account">
                     <div className={styles.tabContent}>
                         <AccountTab />
                     </div>
                 </Tabs.TabPane>
-                <Tabs.TabPane tab="Organization" key="org">
+                <Tabs.TabPane tab="Organization" key="organization">
                     <div className={styles.tabContent}>
                         <OrgTab />
                     </div>
