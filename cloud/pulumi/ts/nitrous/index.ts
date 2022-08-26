c19464ea396f9ceb5859e9012e7a2c15d0993a09
Mohit Reddy <mohitreddy1996@gmail.com>
2022-08-25 20:43:39 -0700

    cloud: document replication factor in nitrous

--- cloud/pulumi/ts/nitrous/index.ts
+++ cloud/pulumi/ts/nitrous/index.ts
@@ -112,7 +112,10 @@ function setupBinLogInMsk(input: inputType, awsProvider: aws.Provider) {
         // We set a default replication factor of 2 (has to be > 1 since this could block producers during a
         // rolling update a broker could be brought down). For production workloads we expect this value to be >= 3
         //
-        // since we configure 2 AZs, setting replication factor as 2 is fine, but it comes potential partial data loss
+        // since we configure 2 AZs, setting replication factor as 2 is fine for non-production workloads,
+        // but it could cause potential partial data loss - this is possible when the "leader" replica for a partition
+        // is down and one of the AZ is unreachable, kafka control plane is unable to assign a broker as the leader,
+        // which causes the new messages to be dropped
         replicationFactor: input.binlog.replicationFactor || 2,
         config: config,
     }, { provider: kafkaProvider, protect: input.protect })
