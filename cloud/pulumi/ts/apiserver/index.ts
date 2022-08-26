ab5e54e3e297ac53a7b73295daa76a581f50d7a9
Amit Arya <amit@fennel.ai>
2022-08-26 16:14:39 -0700

    launchpad: readonly api server for listing demo tiers and get tier config

---
+++ cloud/pulumi/ts/apiserver/index.ts
@@ -0,0 +1,17 @@
+import * as http from "http"
+import * as process from "process"
+import { router } from "./router"
+
+const port = process.env.PORT || 3000
+
+// Currently we expose only read-only APIs to the following.
+// 1. List stacks with a filter.
+// 2. Select and summarize a stack.
+
+const server = http.createServer((req, res) => {
+    router(req, res)
+})
+
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`)
+})
