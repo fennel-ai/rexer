c28aebac3b02d4791a8d1b37d97202aeacd75cad
Mohit Reddy <mohitreddy1996@gmail.com>
2022-09-15 08:40:49 -0700

    msk: Change broker type for plane 2 since running e2e and integration tests against it can hit partitions limit

--- cloud/pulumi/ts/launchpad/index.ts
+++ cloud/pulumi/ts/launchpad/index.ts
@@ -452,7 +452,7 @@ const dataPlaneConfs: Record<number, DataPlaneConf> = {
         // set up MSK cluster for integration tests
         mskConf: {
             // compute cost = 0.0456 ($/hr) x 2 (#brokers) x 720 = $65.6
-            brokerType: "kafka.t3.small",
+            brokerType: "kafka.m5.large",
             // this will place 1 broker node in each of the AZs
             numberOfBrokerNodes: 2,
             // storage cost = 0.10 ($/GB-month) x 64 = 6.4$
