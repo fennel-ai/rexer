62d470f79391f9d478305a3aa918b43f2d53dd10
Mohit Reddy <mohitreddy1996@gmail.com>
2022-09-29 16:29:48 -0700

    proto: captain proto using packed marshal and unmarshal

--- go/fennel/nitrous/rpc/unmarshal_bench_test.go
+++ go/fennel/nitrous/rpc/unmarshal_bench_test.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
-
 	"testing"
 )
 
@@ -35,6 +34,15 @@ func nitrousSimpleOpUnmarshal(ops [][]byte) error {
 func captainUnmarshal(ops [][]byte) error {
 	for _, op := range ops {
 		if _, err := capnp.Unmarshal(op); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func captainUnmarshalPacked(ops [][]byte) error {
+	for _, op := range ops {
+		if _, err := capnp.UnmarshalPacked(op); err != nil {
 			return err
 		}
 	}
@@ -160,19 +168,68 @@ func benchmarkCaptain(b *testing.B) {
 	}
 }
 
+
+func benchmarkCaptainPacked(b *testing.B) {
+	b.ReportAllocs()
+	ops := make([][]byte, 10_000)
+	actual := make([]*NitrousBinlogEventCap, 10_000)
+	l := 0
+	for i := 0; i < 10_000; i++ {
+		msg, seg, err := capnp.NewMessage(capnp.SingleSegment(nil))
+		assert.NoError(b, err)
+		cv, err := NewRootNitrousBinlogEventCap(seg)
+		cv.SetTierId(uint32(i))
+
+		_, s, err := capnp.NewMessage(capnp.SingleSegment(nil))
+		aggEvent, err := NewAggEventCap(s)
+		aggEvent.SetAggId(uint32(21))
+		err = aggEvent.SetGroupkey(utils.RandString(10))
+		assert.NoError(b, err)
+		aggEvent.SetTimestamp(uint32(i * 100))
+		v := value.Int(10)
+		capv, _, err := value.ToCapnValue(v)
+		assert.NoError(b, err)
+		err = aggEvent.SetValue(capv)
+		assert.NoError(b, err)
+
+		err = cv.SetAggEvent(aggEvent)
+		assert.NoError(b, err)
+
+		data, err := msg.MarshalPacked()
+		assert.NoError(b, err)
+		ops[i] = data
+		l += len(data)
+		actual[i] = &cv
+	}
+
+	fmt.Printf("len: %s %d\n", b.Name(), l)
+
+	// reset to not report setup time
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := captainUnmarshalPacked(ops); err != nil {
+			panic(err)
+		}
+	}
+}
+
 /*
 BenchmarkUnmarshal/oneof-10
-     315	   3764591 ns/op	 2400025 B/op	   60000 allocs/op
 len: BenchmarkUnmarshal/oneof: 269702
+     325	   3659325 ns/op	 2400020 B/op	   60000 allocs/op
 BenchmarkUnmarshal/simple-10
-     405	   2959862 ns/op	 2320016 B/op	   50000 allocs/op
 len: BenchmarkUnmarshal/simple: 269702
+     392	   3171480 ns/op	 2320019 B/op	   50000 allocs/op
 BenchmarkUnmarshal/captain-simple-10
-    1227	    984746 ns/op	 1920005 B/op	   30000 allocs/op
 len: BenchmarkUnmarshal/captain-simple 960000
+    1050	   1053201 ns/op	 1920013 B/op	   30000 allocs/op
+BenchmarkUnmarshal/captain-packed-10
+len: BenchmarkUnmarshal/captain-packed 398852
+     410	   3117862 ns/op	 4400032 B/op	   80000 allocs/op
 */
 func BenchmarkUnmarshal(b *testing.B) {
 	b.Run("oneof", benchmarkOneof)
 	b.Run("simple", benchmarkSimple)
 	b.Run("captain-simple", benchmarkCaptain)
+	b.Run("captain-packed", benchmarkCaptainPacked)
 }
