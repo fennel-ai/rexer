953524e718dbcd24526e5fcb4f3fdf8ad7f87c49
Xiao Jiang <xiao@fennel.ai>
2022-10-12 09:27:59 -0700

    [feature-app] [rex-1738] python code viewer (#1535)
    * [Savepoint] install react-syntax-highlighter
    
    * [feature-app] [rex-1738] python code viewer

--- webapp/src/client/feature_app/FeatureDetailPage.tsx
+++ webapp/src/client/feature_app/FeatureDetailPage.tsx
@@ -1,8 +1,36 @@
 import { PicLeftOutlined, BranchesOutlined, DownOutlined } from "@ant-design/icons";
-import { Dropdown, Menu, Space } from "antd";
+import { Dropdown, Menu, Space, Collapse } from "antd";
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import python from 'react-syntax-highlighter/dist/esm/languages/hljs/python';
 
 import styles from "./styles/FeatureDetailPage.module.scss";
 import commonStyles from "./styles/Page.module.scss";
+
+SyntaxHighlighter.registerLanguage("python", python);
+
+const CODE = `
+    # Given a movie, users who rated the movie 5 stars
+    @rex.aggregate(
+        name='users_who_liked_movie', aggregate_type='list',
+        action_types=['rating'], config={'durations': [14*DAY, 7*DAY, 1*DAY]},
+    )
+    def users_who_liked_movie(actions):
+        top_rated_events = op.std.filter(actions, var='e', where=var('e').metadata == 5.0)
+        with_key = op.std.set(top_rated_events, var='e', name='groupkey', value=var('e').target_id)
+        return op.std.set(with_key, var='e', name='value', value=var('e').actor_id)
+
+
+    # Given a user, all the movies they have rated with 5 stars
+    @rex.aggregate(
+        name='top_rated_movies_by_user', aggregate_type='list',
+        action_types=['rating'], config={'durations': [14*DAY, 7*DAY, 1*DAY]},
+    )
+    def top_rated_movies_by_user(actions):
+        top_rated_events = op.std.filter(actions, var='e', where=var('e').metadata == 5.0)
+        with_key = op.std.set(top_rated_events, var='e', name='groupkey', value=var('e').actor_id)
+        return op.std.set(with_key, var='e', name='value', value=var('e').target_id)
+`;
 
 function FeatureDetailPage(): JSX.Element {
     // TODO(xiao): change the icon and tags, real version
@@ -39,6 +67,13 @@ function FeatureDetailPage(): JSX.Element {
                     </tr>
                 </tbody>
             </table>
+            <Collapse>
+                <Collapse.Panel header="Code" key="code">
+                    <SyntaxHighlighter language="python" style={atomOneLight}>
+                        {CODE}
+                    </SyntaxHighlighter>
+                </Collapse.Panel>
+            </Collapse>
         </div>
     );
 }
