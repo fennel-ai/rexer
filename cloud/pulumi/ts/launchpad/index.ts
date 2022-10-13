3c223c9cfa072ba7c0dc2ac0fad801456c1302bb
Mohit Reddy <mohitreddy1996@users.noreply.github.com>
2022-10-13 11:19:23 -0700

    memorydb: scale up to 30 shards, with new huge batch of 170M actions,… (#1540)
    * memorydb: scale up to 30 shards, with new huge batch of 170M actions, we started getting bottlenecked on memory
    
    * add in airbyte required changes as well

--- cloud/pulumi/ts/launchpad/index.ts
+++ cloud/pulumi/ts/launchpad/index.ts
@@ -606,7 +606,7 @@ const dataPlaneConfs: Record<number, DataPlaneConf> = {
         },
         controlPlaneConf: controlPlane,
         redisConf: {
-            numShards: 20,
+            numShards: 30,
             nodeType: "db.r6g.large",
             numReplicasPerShard: 1,
         },
@@ -712,6 +712,18 @@ const dataPlaneConfs: Record<number, DataPlaneConf> = {
                     expansionPriority: 1,
                     labels: {
                         "node-group": "p-5-common-ng-arm",
+                    },
+                },
+                {
+                    name: "p-5-common-ng-x86-c6i2xlarge",
+                    instanceTypes: ["c6i.2xlarge"],
+                    minSize: 1,
+                    maxSize: 10,
+                    amiType: DEFAULT_X86_AMI_TYPE,
+                    capacityType: ON_DEMAND_INSTANCE_TYPE,
+                    expansionPriority: 1,
+                    labels: {
+                        "node-group": "p-5-common-ng-x86",
                     },
                 }
             ],
