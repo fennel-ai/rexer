07760cccbc59fe61c8bda8bc809db71e85f1b1bf
Abhay Bothra <bothra90@users.noreply.github.com>
2023-03-15 17:34:35 -0700

    server: support request body being gzip encoded
    This should not be needed if envoy can be configured to transparently
    decompress the body, but doesn't seem to be working with emissary.

--- go/fennel/service/http/server.go
+++ go/fennel/service/http/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"compress/gzip"
 	"context"
 	"encoding/json"
 	"errors"
@@ -89,7 +90,16 @@ var totalUnleashQueryRequestsDropped = promauto.NewCounter(
 
 func readRequest(req *http.Request) ([]byte, error) {
 	defer req.Body.Close()
-	return ioutil.ReadAll(req.Body)
+	if req.Header.Get("Content-Encoding") == "gzip" {
+		reader, err := gzip.NewReader(req.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read request body: %s", err)
+		}
+		defer reader.Close()
+		return ioutil.ReadAll(reader)
+	} else {
+		return ioutil.ReadAll(req.Body)
+	}
 }
 
 type server struct {
