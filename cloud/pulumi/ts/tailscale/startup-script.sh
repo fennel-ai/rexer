80577bd9e9a9b2faaccf2638ce9bc8c2cd945585
Abhay Bothra <bothra90@users.noreply.github.com>
2023-03-20 17:16:45 -0700

    tailscale: add demo plane cidr to vpn (#1625)

--- cloud/pulumi/ts/tailscale/startup-script.sh
+++ cloud/pulumi/ts/tailscale/startup-script.sh
@@ -30,6 +30,7 @@ sudo systemctl enable --now tailscaled
 # 10.114.0.0/16: Data plane for yext in their account in us-east-1
 # 10.121.0.0/16: Data plane for oslash in their account
 # 10.122.0.0/16: Data plane for oslash in their account in us-east-1
+# 10.123.0.0/16: Demo data plane for feature engineering
 sudo tailscale up \
-  --advertise-routes=172.31.0.0/16,10.101.0.0/16,10.102.0.0/16,10.103.0.0/16,10.104.0.0/16,10.105.0.0/16,10.109.0.0/16,10.111.0.0/16,10.112.0.0/16,10.121.0.0/16,10.122.0.0/16,10.113.0.0/16,10.114.0.0/16 \
-  --authkey tskey-auth-kFD8zZ1CNTRL-a3My8Ng7k43gh4FUBVw4533AX9xdATX4B
+  --advertise-routes=172.31.0.0/16,10.101.0.0/16,10.102.0.0/16,10.103.0.0/16,10.104.0.0/16,10.105.0.0/16,10.109.0.0/16,10.111.0.0/16,10.112.0.0/16,10.121.0.0/16,10.122.0.0/16,10.113.0.0/16,10.114.0.0/16,10.123.0.0/16 \
+  --authkey tskey-auth-kkQazx3CNTRL-kXaAE3zD1oiJdv89QJVWoiEfQiqtLuAz
